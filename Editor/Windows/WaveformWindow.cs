a99d35feeb87e479f0d1b4ff0303fe9c2e39ecaf
Gregory Labute <gregoryl@unity3d.com>
2018-02-06 17:31:02 -0500

    Re-added OverlayImage and WaveformMonitor

---
+++ Editor/Windows/WaveformWindow.cs
@@ -0,0 +1,226 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine.Rendering;
+
+namespace Cinemachine.Editor
+{
+    public class WaveformWindow : EditorWindow
+    {
+        WaveformGenerator mWaveformGenerator;
+        Texture2D mScreenshot;
+        string mScreenshotFilename;
+
+        [MenuItem("Window/Waveform Monitor")]
+        private static void OpenWindow()
+        {
+            WaveformWindow window = EditorWindow.GetWindow<WaveformWindow>(false);
+            window.autoRepaintOnSceneChange = true;
+            //window.position = new Rect(100, 100, 400, 400);
+            window.Show(true);
+        }
+        
+        private void OnEnable()
+        {
+            titleContent = new GUIContent("Waveform", CinemachineSettings.CinemachineLogoTexture);
+            mWaveformGenerator = new WaveformGenerator();
+
+            mScreenshotFilename = Path.GetFullPath(FileUtil.GetUniqueTempPathInProject() + ".png");
+            ScreenCapture.CaptureScreenshot(mScreenshotFilename);
+            EditorApplication.update += UpdateScreenshot;
+        }
+
+        private void OnDisable()
+        {
+            EditorApplication.update -= UpdateScreenshot;
+            if (!string.IsNullOrEmpty(mScreenshotFilename) && File.Exists(mScreenshotFilename))
+                File.Delete(mScreenshotFilename);
+            mScreenshotFilename = null;
+            mWaveformGenerator.DestroyBuffers();
+            if (mScreenshot != null)
+                DestroyImmediate(mScreenshot);
+            mScreenshot = null;
+        }
+
+        private void OnGUI()
+        {
+            Rect rect = EditorGUILayout.GetControlRect(true);
+            EditorGUIUtility.labelWidth /= 2;
+            EditorGUI.BeginChangeCheck();
+            mWaveformGenerator.m_Exposure = EditorGUI.Slider(
+                rect, "Exposure", mWaveformGenerator.m_Exposure, 0.01f, 2);
+            if (EditorGUI.EndChangeCheck())
+                UnityEditorInternal.InternalEditorUtility.RepaintAllViews();
+            EditorGUIUtility.labelWidth *= 2;
+            rect.y += rect.height;
+            rect.height = position.height - rect.height;
+            var tex = mWaveformGenerator.Result;
+            if (tex != null)
+                GUI.DrawTexture(rect, tex);
+        }
+
+        float mLastUpdateTime = 0;
+        private void UpdateScreenshot()
+        {
+            // Don't do this too often
+            float now = Time.time;
+            if (mScreenshot != null && now - mLastUpdateTime < 0.1f)
+                return;
+
+            mLastUpdateTime = now;
+            if (!string.IsNullOrEmpty(mScreenshotFilename) && File.Exists(mScreenshotFilename))
+            {
+                byte[] fileData = File.ReadAllBytes(mScreenshotFilename);
+                if (mScreenshot == null)
+                    mScreenshot = new Texture2D(2, 2);
+                mScreenshot.LoadImage(fileData); // this will auto-resize the texture dimensions.
+                mWaveformGenerator.RenderWaveform(mScreenshot);
+
+                // Capture the next one
+                ScreenCapture.CaptureScreenshot(mScreenshotFilename);
+            }
+        }
+
+        class WaveformGenerator
+        {
+            public float m_Exposure = 0.2f;
+
+            RenderTexture mOutput;
+            ComputeBuffer mData;
+
+            int mThreadGroupSize;
+            int mThreadGroupSizeX;
+            int mThreadGroupSizeY;
+
+            ComputeShader mWaveformCompute;
+            MaterialPropertyBlock mWaveformProperties;
+            Material mWaveformMaterial;
+            CommandBuffer mCmd;
+
+            static Mesh sFullscreenTriangle;
+            static Mesh FullscreenTriangle 
+            { 
+                get 
+                {
+                    if (sFullscreenTriangle == null)
+                    {
+                        sFullscreenTriangle = new Mesh { name = "Fullscreen Triangle" };
+                        sFullscreenTriangle.SetVertices(new List<Vector3>
+                        {
+                            new Vector3(-1f, -1f, 0f),
+                            new Vector3(-1f,  3f, 0f),
+                            new Vector3( 3f, -1f, 0f)
+                        });
+                        sFullscreenTriangle.SetIndices(
+                            new [] { 0, 1, 2 }, MeshTopology.Triangles, 0, false);
+                        sFullscreenTriangle.UploadMeshData(false);
+                    }
+                    return sFullscreenTriangle;
+                } 
+            }
+          
+            public WaveformGenerator()
+            {
+                mWaveformCompute = Resources.Load<ComputeShader>("CMWaveform");
+                mWaveformProperties = new MaterialPropertyBlock();
+                mWaveformMaterial = new Material(Resources.Load<Shader>("CMWaveform"))
+                {
+                    name = "CMWaveformMaterial",
+                    hideFlags = HideFlags.DontSave
+                };
+                mCmd = new CommandBuffer();
+            }
+
+            void CreateBuffers(int width, int height)
+            {
+                if (mOutput == null || !mOutput.IsCreated() 
+                    || mOutput.width != width || mOutput.height != height)
+                {
+                    DestroyImmediate(mOutput);
+                    mOutput = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32)
+                    {
+                        anisoLevel = 0,
+                        filterMode = FilterMode.Bilinear,
+                        wrapMode = TextureWrapMode.Clamp,
+                        useMipMap = false
+                    };
+                    mOutput.Create();
+                }
+
+                int count = Mathf.CeilToInt(width / (float)mThreadGroupSizeX) * mThreadGroupSizeX * height;
+                if (mData == null)
+                    mData = new ComputeBuffer(count, sizeof(uint) << 2);
+                else if (mData.count < count)
+                {
+                    mData.Release();
+                    mData = new ComputeBuffer(count, sizeof(uint) << 2);
+                }
+            }
+
+            public void DestroyBuffers()
+            {
+                if (mData != null)
+                    mData.Release();
+                mData = null;
+                DestroyImmediate(mOutput);
+                mOutput = null;
+            }
+
+            public RenderTexture Result { get { return mOutput; } }
+
+            public void RenderWaveform(Texture2D source)
+            {
+                if (mWaveformMaterial == null)
+                    return;
+
+                int width = source.width;
+                int height = source.height;
+                int histogramResolution = 256;
+
+                mThreadGroupSize = 256;
+                mThreadGroupSizeX = 16;
+                mThreadGroupSizeY = 16;
+                CreateBuffers(width, histogramResolution);
+
+                mCmd.Clear();
+                mCmd.BeginSample("CMWaveform");
+
+                var parameters = new Vector4(
+                    width, height, 
+                    QualitySettings.activeColorSpace == ColorSpace.Linear ? 1 : 0, 
+                    histogramResolution);
+
+                // Clear the buffer on every frame
+                int kernel = mWaveformCompute.FindKernel("KCMWaveformClear");
+                mCmd.SetComputeBufferParam(mWaveformCompute, kernel, "_WaveformBuffer", mData);
+                mCmd.SetComputeVectorParam(mWaveformCompute, "_Params", parameters);
+                mCmd.DispatchCompute(mWaveformCompute, kernel, 
+                    Mathf.CeilToInt(width / (float)mThreadGroupSizeX), 
+                    Mathf.CeilToInt(histogramResolution / (float)mThreadGroupSizeY), 1);
+
+                // Gather all pixels and fill in our waveform
+                kernel = mWaveformCompute.FindKernel("KCMWaveformGather");
+                mCmd.SetComputeBufferParam(mWaveformCompute, kernel, "_WaveformBuffer", mData);
+                mCmd.SetComputeTextureParam(mWaveformCompute, kernel, "_Source", source);
+                mCmd.SetComputeVectorParam(mWaveformCompute, "_Params", parameters);
+                mCmd.DispatchCompute(mWaveformCompute, kernel, width, 
+                    Mathf.CeilToInt(height / (float)mThreadGroupSize), 1);
+
+                // Generate the waveform texture
+                float exposure = Mathf.Max(0f, m_Exposure);
+                exposure *= (float)histogramResolution / height;
+                mWaveformProperties.SetVector(Shader.PropertyToID("_Params"), 
+                    new Vector4(width, histogramResolution, exposure, 0f));
+                mWaveformProperties.SetBuffer(Shader.PropertyToID("_WaveformBuffer"), mData);
+                mCmd.SetRenderTarget(mOutput);
+                mCmd.DrawMesh(
+                    FullscreenTriangle, Matrix4x4.identity, 
+                    mWaveformMaterial, 0, 0, mWaveformProperties);
+                mCmd.EndSample("CMWaveform");
+
+                Graphics.ExecuteCommandBuffer(mCmd);
+            }
+        }
+    }
+}
---
+++ Runtime/Behaviours/CinemachineOverlayImage.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using UnityEngine;
+
+namespace Cinemachine
+{
+    /// <summary>
+    /// An add-on module for Cinemachine Virtual Camera that replaces the camera's
+    /// output with a storyboard image
+    /// </summary>
+    [ExecuteInEditMode]
+    [SaveDuringPlay]
+    [AddComponentMenu("")] // Hide in menu
+    public class CinemachineOverlayImage : CinemachineExtension
+    {
+        [Tooltip("If checked, the specified image will be displayed as an overlay over the virtual camera's output")]
+        public bool m_ShowImage = true;
+
+        [Tooltip("The image to display")]
+        public Texture m_Image;
+
+        public enum FillStrategy
+        {
+            /// <summary>Image will be as large as possible on the screen, without being cropped</summary>
+            BestFit,
+            /// <summary>Image will be cropped if necessary so that the screen is entirely filled</summary>
+            CropImageToFit,
+            /// <summary>Image will be stretched to cover any aspect mismatch with the screen</summary>
+            StretchToFit
+        };
+        [Tooltip("How to handle differences between image aspect and screen aspect")]
+        public FillStrategy m_Aspect = FillStrategy.BestFit;
+
+        [Tooltip("The opacity of the image.  0 is transparent, 1 is opaque")]
+        [Range(0, 1)]
+        public float m_Alpha = 1;
+
+        [Tooltip("The screen-space position at which to display the image.  Zero is center")]
+        public Vector2 m_Center = Vector2.zero;
+
+        [Tooltip("The screen-space rotation to apply to the image")]
+        public Vector3 m_Rotation = Vector3.zero;
+
+        [Tooltip("The screen-space scaling to apply to the image")]
+        public Vector2 m_Scale = Vector3.one;
+
+        [Tooltip("If checked, X and Y scale are synchronized")]
+        public bool m_SyncScale = true;
+
+        [Tooltip("If checked, Camera transform will not be controlled by this virtual camera")]
+        public bool m_MuteCamera;
+
+        [Range(-1, 1)]
+        [Tooltip("Wipe the image on and off horizontally")]
+        public float m_Wipe = 0f;
+
+        GameObject mCanvas;
+        CinemachineBrain mCanvasParent;
+        RectTransform mViewport; // for mViewport clipping
+        UnityEngine.UI.RawImage mRawImage;
+
+        protected override void PostPipelineStageCallback(
+            CinemachineVirtualCameraBase vcam,
+            CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+        {
+            // Apply to this vcam only, not the children
+            if (vcam != VirtualCamera || stage != CinemachineCore.Stage.Finalize)
+                return;
+
+            if (m_ShowImage)
+            {
+                state.AddCustomBlendable(new CameraState.CustomBlendable(this, 1));
+                if (m_MuteCamera)
+                    state.BlendHint |= CameraState.BlendHintValue.NoTransform;
+            }
+        }
+
+        void CameraUpdatedCallback(CinemachineBrain brain)
+        {
+            bool isLive = CinemachineCore.Instance.IsLive(VirtualCamera);
+            LocateMyCanvas(brain, isLive);
+            if (mCanvas != null)
+                mCanvas.SetActive(isLive && m_ShowImage);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            int numBrains = CinemachineCore.Instance.BrainCount;
+            for (int i = 0; i < numBrains; ++i)
+            {
+                LocateMyCanvas(CinemachineCore.Instance.GetActiveBrain(i), false);
+                if (mCanvas != null)
+                    DestroyImmediate(mCanvas);
+                mCanvas = null;
+            }
+        }
+
+        protected override void ConnectToVcam(bool connect)
+        {
+            base.ConnectToVcam(connect);
+            CinemachineCore.CameraUpdatedEvent.RemoveListener(CameraUpdatedCallback);
+            if (connect)
+                CinemachineCore.CameraUpdatedEvent.AddListener(CameraUpdatedCallback);
+        }
+        
+        string CanvasName { get { return "_CM_canvas" + gameObject.GetInstanceID().ToString(); } }
+
+        void LocateMyCanvas(CinemachineBrain parent, bool createIfNotFound)
+        {
+            if (mCanvas == null || mCanvasParent != parent)
+            {
+                mCanvas = null;
+                mRawImage = null;
+                mViewport = null;
+                mCanvasParent = parent;
+                string canvasName = CanvasName;
+                int numChildren = parent.transform.childCount;
+                for (int i = 0; mCanvas == null && i < numChildren; ++i)
+                {
+                    RectTransform child = parent.transform.GetChild(i) as RectTransform;
+                    if (child != null && child.name == canvasName)
+                        mCanvas = child.gameObject;
+                }
+            }
+            if (mCanvas == null && createIfNotFound)
+                CreateCanvas(parent);
+            if (mCanvas != null && (mRawImage == null || mViewport == null))
+            {
+                mViewport = mCanvas.GetComponentInChildren<RectTransform>();
+                mRawImage = mCanvas.GetComponentInChildren<UnityEngine.UI.RawImage>();
+            }
+        }
+
+        void CreateCanvas(CinemachineBrain parent)
+        {
+            mCanvas = new GameObject(CanvasName, typeof(RectTransform));
+            mCanvas.hideFlags = HideFlags.HideAndDontSave;
+            mCanvas.transform.SetParent(parent.transform);
+            mCanvasParent = parent;
+
+            var c = mCanvas.AddComponent<Canvas>();
+            c.renderMode = RenderMode.ScreenSpaceOverlay;
+
+            var go = new GameObject("Viewport", typeof(RectTransform));
+            go.transform.SetParent(mCanvas.transform);
+            mViewport = (RectTransform)go.transform;
+            go.AddComponent<UnityEngine.UI.RectMask2D>();
+
+            go = new GameObject("RawImage", typeof(RectTransform));
+            go.transform.SetParent(mViewport.transform);
+            mRawImage = go.AddComponent<UnityEngine.UI.RawImage>();
+        }
+
+        void PlaceImage(float alpha)
+        {
+            if (mRawImage != null && mViewport != null)
+            {
+                Rect screen = new Rect(0, 0, Screen.width, Screen.height);
+                if (mCanvasParent.OutputCamera != null)
+                    screen = mCanvasParent.OutputCamera.pixelRect;
+                screen.x -= (float)Screen.width/2;
+                screen.y -= (float)Screen.height/2;
+
+                mViewport.localPosition = screen.center;
+                mViewport.localRotation = Quaternion.identity;
+                mViewport.localScale = Vector3.one;;
+                mViewport.sizeDelta = screen.size;
+
+                Vector2 scale = Vector2.one;
+                if (m_Image != null
+                    && m_Image.width > 0 && m_Image.width > 0 
+                    && screen.width > 0 && screen.height > 0)
+                {
+                    float f = (screen.height * m_Image.width) / (screen.width * m_Image.height);
+                    switch (m_Aspect)
+                    {
+                        case FillStrategy.BestFit:
+                            if (f >= 1)
+                                scale.y /= f;
+                            else
+                                scale.x *= f;
+                            break;
+                        case FillStrategy.CropImageToFit:
+                            if (f >= 1)
+                                scale.x *= f;
+                            else
+                                scale.y /= f;
+                            break;
+                        case FillStrategy.StretchToFit:
+                            break;
+                    }
+                }
+                scale.x *= m_Scale.x;
+                scale.y *= m_SyncScale ? m_Scale.x : m_Scale.y;
+
+                mRawImage.texture = m_Image;
+                Color tintColor = Color.white;
+                tintColor.a = m_Alpha * alpha;
+                mRawImage.color = tintColor;
+                mRawImage.rectTransform.localPosition 
+                    = new Vector2(screen.width * m_Center.x, screen.height * m_Center.y);
+                mRawImage.rectTransform.localRotation = Quaternion.Euler(m_Rotation);
+                mRawImage.rectTransform.localScale = scale;
+                mRawImage.rectTransform.sizeDelta = screen.size;
+
+                // Apply wipe
+                float delta = -Mathf.Clamp(m_Wipe, -1, 1) * screen.width;
+                var p = mViewport.localPosition; p.x -= delta/2; mViewport.localPosition = p;
+                p = mRawImage.rectTransform.localPosition; p.x += delta/2; mRawImage.rectTransform.localPosition = p;
+                mViewport.sizeDelta = new Vector2(screen.width - Mathf.Abs(delta), screen.height);
+            }        
+        }
+
+        static void StaticBlendingHandler(CinemachineBrain brain)
+        {
+            //UnityEngine.Profiling.Profiler.BeginSample("CinemachineOverlayImage.StaticBlendingHandler");
+            CameraState state = brain.CurrentCameraState;
+            int numBlendables = state.NumCustomBlendables;
+            for (int i = 0; i < numBlendables; ++i)
+            {
+                var b = state.GetCustomBlendable(i);
+                CinemachineOverlayImage src = b.m_Custom as CinemachineOverlayImage;
+                if (!(src == null)) // in case it was deleted
+                {
+                    src.LocateMyCanvas(brain, true);
+                    src.PlaceImage(b.m_Weight);
+                }
+            }
+            //UnityEngine.Profiling.Profiler.EndSample();
+        }
+
+#if UNITY_EDITOR
+        [UnityEditor.InitializeOnLoad]
+        class EditorInitialize { static EditorInitialize() { InitializeModule(); } }
+#endif
+        [RuntimeInitializeOnLoadMethod]
+        static void InitializeModule()
+        {
+            CinemachineCore.CameraUpdatedEvent.RemoveListener(StaticBlendingHandler);
+            CinemachineCore.CameraUpdatedEvent.AddListener(StaticBlendingHandler);
+        }
+    }
+}
