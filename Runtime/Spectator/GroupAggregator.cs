201c85c560d6dcb3aa04fd4e7c183826435d9609
Gregory Labute <gregoryl@unity3d.com>
2018-08-07 11:11:18 -0400

    wip

---
+++ Runtime/Spectator/GroupAggregator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+namespace Spectator
+{
+    /// Group Aggregator
+    /// 
+    /// - Dynamically creates and destroys groups
+    /// - Each story thread has a target group, containing the item of interest.  
+    ///     By default the target group will contain a single object - the item of interest 
+    ///     that created the thread
+    /// - Group aggregator dynamically creates and destroys groups based on 
+    ///     proximity (player + enemy, groups of players, groups of enemies, etc).
+    /// - New story threads are then created for the new groups.  
+    ///     Interest level of the new threads is an aggregate of the interest levels of the members
+    /// - Polls periodically
+    /// 
+    public class GroupAggregator : MonoBehaviour
+    {   
+        public float m_proximityThreshold = 5;
+        public float m_updateTimeStep = 2;
+        public Transform m_Root;
+
+        public class Group
+        {
+            public CinemachineTargetGroup m_TargetGroup;
+            public List<Group> m_Parents;
+            public List<Group> m_Children;
+        }
+        Dictionary<StoryManager.Subject, Group> mGroupLookup;
+        
+        public Group GetGroup(StoryManager.Subject subject)
+        {
+            Group g;
+            if (mGroupLookup == null)
+                mGroupLookup = new Dictionary<StoryManager.Subject, Group>();
+            mGroupLookup.TryGetValue(subject, out g);
+            return g;
+        }
+        /*
+        public Group CreateGroup(StoryManager.Subject subject)
+        {
+            GameObject go = new GameObject(subject.m_Transform.name);
+            var cmTargetGroup = go.AddComponent<CinemachineFreeLook>();
+
+        }
+        */
+    }
+}
---
+++ Runtime/Spectator/StoryThreadSubjectBase.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spectator
+{
+    /// Objects of Interest:
+    ///
+    /// - Component on an individual object that maintains an ‘interest level’
+    /// - Registers itself with story manager on creation
+    ///   - Notifies story manager when it becomes active; a story thread is created for it
+    /// - Players, enemies, doors, interactive elements etc
+    /// - Exposes an "interesting" value for the story manager to order.  
+    ///   Pushes this value every frame to the corresponding story thread
+    /// - Subscribes to actions of the specific object, ie:
+    ///   - Taking damage
+    ///   - Causing damage
+    ///   - Opening / being interacted with (doors / terminals / health packs etc)
+    /// - "Interesting" value is a function of actions and other things such as
+    ///   - Range to player (for non-player objects)
+    ///   - Novelty (new things are often more interesting for a while)
+    /// - Can maintain a separate list of ‘object-specific’ cameras that get activated 
+    ///     when the object wakes up
+    /// 
+    public abstract class StoryThreadSubjectBase : MonoBehaviour 
+    {
+        StoryManager.Subject mSubject;
+
+	    protected void Start() 
+        {
+            mSubject = new StoryManager.Subject(transform);
+	    }
+
+        protected void OnEnable()
+        {
+            StoryManager.Instance.CreateStoryThread(mSubject);
+        }
+
+        protected void OnDisable()
+        {
+            StoryManager.Instance.DestroyStoryThread(mSubject);
+        }
+
+        protected void Update() 
+        {
+            // Calculate interest level and push it to story thread
+            var th = StoryManager.Instance.GetStoryThread(mSubject);
+            if (th != null)
+            {
+                th.InterestLevel = ComputeInterest(th.InterestLevel);
+            }
+	    }
+
+        // Object-specific interest level.
+        // How interesting is this object right now?
+        // Story thread will be sorted by interest level.
+        // Value is arbitrary, and can be a function of anything you want.
+        abstract protected float ComputeInterest(float currentInterestLevel);
+    }
+
+}
