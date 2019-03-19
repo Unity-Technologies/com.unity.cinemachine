e12a7fb026adb29252a0a4dbe75a82a141501ba1
Greg Labute <gregoryl@unity3d.com>
2019-03-19 09:49:37 -0400

    Bugfix: cameras sometimes snapped when interrupting blends

--- Runtime/Behaviours/CinemachineStateDrivenCamera.cs
+++ Runtime/Behaviours/CinemachineStateDrivenCamera.cs
@@ -140,8 +140,7 @@ namespace Cinemachine
         /// <returns>True if the vcam is currently actively influencing the state of this vcam</returns>
         public override bool IsLiveChild(ICinemachineCamera vcam)
         {
-            return vcam == LiveChild
-                || (mActiveBlend != null && (vcam == mActiveBlend.CamA || vcam == mActiveBlend.CamB));
+            return vcam == LiveChild || (mActiveBlend != null && mActiveBlend.Uses(vcam));
         }
 
         /// <summary>The State of the current live child</summary>
