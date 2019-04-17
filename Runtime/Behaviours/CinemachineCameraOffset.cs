d728dd8eb52fa2f0cc399f66f9f4cda17158e973
Greg Labute <gregoryl@unity3d.com>
2019-04-17 14:25:10 -0400

    CinemachineCameraOffset: added customazable stage for when to apply the offset

--- Runtime/Behaviours/CinemachineCameraOffset.cs
+++ Runtime/Behaviours/CinemachineCameraOffset.cs
@@ -5,18 +5,19 @@ using Cinemachine;
 /// An add-on module for Cinemachine Virtual Camera that adds a final offset to the camera
 /// </summary>
 [AddComponentMenu("")] // Hide in menu
-public class CinemachineCameraOffset : CinemachineExtension 
+public class CinemachineCameraOffset : CinemachineExtension
 {
     [Tooltip("Offset the camera's position by this much (camera space)")]
     public Vector3 m_Offset = Vector3.zero;
+
+    [Tooltip("When to apply the offset")]
+    public CinemachineCore.Stage m_ApplyAfter = CinemachineCore.Stage.Aim;
 
     protected override void PostPipelineStageCallback(
         CinemachineVirtualCameraBase vcam,
         CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
     {
-        // Apply after the camera has been aimed.
-        // To apply offset before the camera aims, change this to Body
-        if (stage == CinemachineCore.Stage.Aim)
+        if (stage == m_ApplyAfter)
         {
             Vector3 offset = state.FinalOrientation * m_Offset;
             state.ReferenceLookAt += offset;
