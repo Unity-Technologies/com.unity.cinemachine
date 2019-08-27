5a17e980d47fc63d761ee46afe455cafa816fbe4
Greg Labute <gregoryl@unity3d.com>
2019-08-27 13:49:47 -0400

    Fix that last fix

--- Editor/EventHelpers/CinemachineTriggerActionEditor.cs
+++ Editor/EventHelpers/CinemachineTriggerActionEditor.cs
@@ -1,3 +1,8 @@
+#if !UNITY_2019_3_OR_NEWER
+#define CINEMACHINE_PHYSICS
+#define CINEMACHINE_PHYSICS_2D
+#endif
+
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
--- Editor/Impulse/CinemachineCollisionImpulseSourceEditor.cs
+++ Editor/Impulse/CinemachineCollisionImpulseSourceEditor.cs
@@ -1,4 +1,9 @@
-﻿using UnityEditor;
+﻿#if !UNITY_2019_3_OR_NEWER
+#define CINEMACHINE_PHYSICS
+#define CINEMACHINE_PHYSICS_2D
+#endif
+
+using UnityEditor;
 using UnityEngine;
 
 namespace Cinemachine.Editor
