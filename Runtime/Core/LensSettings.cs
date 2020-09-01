9dfd4668600741cd8f51daa95186e0b8d6d2e173
Greg Labute <gregoryl@unity3d.com>
2020-09-01 16:13:22 -0400

    cosmetic

--- Runtime/Core/LensSettings.cs
+++ Runtime/Core/LensSettings.cs
@@ -31,26 +31,31 @@ namespace Cinemachine
         /// on a super-35mm sensor would equal a 19.6 degree FOV
         /// </summary>
         [Range(1f, 179f)]
-        [Tooltip("This is the camera view in vertical degrees. For cinematic people, a 50mm lens on a super-35mm sensor would equal a 19.6 degree FOV")]
+        [Tooltip("This is the camera view in vertical degrees. For cinematic people, a 50mm lens "
+           + "on a super-35mm sensor would equal a 19.6 degree FOV")]
         public float FieldOfView;
 
         /// <summary>
-        /// When using an orthographic camera, this defines the height, in world
+        /// When using an orthographic camera, this defines the half-height, in world
         /// co-ordinates, of the camera view.
         /// </summary>
-        [Tooltip("When using an orthographic camera, this defines the half-height, in world coordinates, of the camera view.")]
+        [Tooltip("When using an orthographic camera, this defines the half-height, in world "
+            + "coordinates, of the camera view.")]
         public float OrthographicSize;
 
         /// <summary>
         /// The near clip plane for this LensSettings
         /// </summary>
-        [Tooltip("This defines the near region in the renderable range of the camera frustum. Raising this value will stop the game from drawing things near the camera, which can sometimes come in handy.  Larger values will also increase your shadow resolution.")]
+        [Tooltip("This defines the near region in the renderable range of the camera frustum. "
+            + "Raising this value will stop the game from drawing things near the camera, which "
+            + "can sometimes come in handy.  Larger values will also increase your shadow resolution.")]
         public float NearClipPlane;
 
         /// <summary>
         /// The far clip plane for this LensSettings
         /// </summary>
-        [Tooltip("This defines the far region of the renderable range of the camera frustum. Typically you want to set this value as low as possible without cutting off desired distant objects")]
+        [Tooltip("This defines the far region of the renderable range of the camera frustum. Typically "
+            + "you want to set this value as low as possible without cutting off desired distant objects")]
         public float FarClipPlane;
 
         /// <summary>
