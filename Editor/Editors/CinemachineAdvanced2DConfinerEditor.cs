1d8493c4410a033a9125b44f990abd7629d6c49b
Krisztian G. Birkas <gaborkb@unity3d.com>
2020-09-28 15:40:12 -0400

    Tooltip text cleanup + long line breaking cleanup

--- Editor/Editors/CinemachineAdvanced2DConfinerEditor.cs
+++ Editor/Editors/CinemachineAdvanced2DConfinerEditor.cs
@@ -27,15 +27,14 @@ namespace Cinemachine.Editor
         {
             m_maxOrthoSizeProperty = FindProperty(x => x.m_MaxOrthoSize);
             m_maxOrthoSizeGUIContent = new GUIContent("Max Camera Window Size", 
-                "Defines a maximum camera window size for the precalculation. Use this to optimize memory usage. " +
-                "If 0, then this parameter is ignored.  " +
-                "Can be also used to allow the camera to look outside the map a bit. For example, if you are using a " +
-                "camera with orthographic size of 2, then you could set this value to 1.8. This will allow the camera " +
-                "to look outside by some amount depending on your window ratio.");
+                "Defines a maximum camera window size for baking. Use this to optimize memory usage.  If 0, then " +
+                "this parameter is ignored.  Can be also used to allow the camera to look outside the map a bit. " +
+                "For example, if you are using a camera with orthographic size of 2, then you could set this value " +
+                "to 1.8. This will allow the camera to look outside by some amount depending on your window ratio.");
             m_shrinkToPointsExperimentalProperty = FindProperty(x => x.m_ShrinkToPointsExperimental);
             m_shrinkToPointsExperimentalGUIContent = new GUIContent("Shrink Sub-Polygons To Point Experimental", 
-                "By default, the confiner is reduced to its skeleton. If this property is enabled, then the confiner will " +
-                "continue reducing the skeletons by reducing bones (line segments) to points.");
+                "By default, the confiner is reduced to its skeleton. If this property is enabled, then the confiner " +
+                "will continue reducing the skeletons by reducing bones (line segments) to points.");
             m_autoBakeProperty = FindProperty(x => x.m_AutoBake);
             m_autoBakeGUIContent = new GUIContent("Automatic Baking",
                 "Automatically rebakes the confiner, if input parameters (InputCollider, Resolution, " +
