5d74e4066ca26b51b19b004088ce8968b213e342
Krisztian G. Birkas <gaborkb@unity3d.com>
2020-09-28 11:31:19 -0400

    Bugfix for 1174993 + release notes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - Bugfix (1276343) - CM Brain inspector custom blends misaligned dropdown arrow
 - Bugfix (1256530) - disallow multiple components where appropriate
 - Bugfix: BlendList camera was incorrectly holding 0-length camera cuts
+- Bugfix (1174993) - CM Brain logo was not added to Hierarchy next to Main Camera after adding vcam for the first time after importing CM. 
 
 
 ## [2.6.2] - 2020-09-02
--- Editor/Windows/CinemachineSettings.cs
+++ Editor/Windows/CinemachineSettings.cs
@@ -232,6 +232,22 @@ namespace Cinemachine.Editor
 
         internal static event Action AdditionalCategories = null;
 
+        [InitializeOnLoadMethod]
+        /// Ensures that CM Brain logo is added to the Main Camera
+        /// after adding a virtual camera to the project for the first time
+        static void OnPackageLoadedInEditor()
+        {
+            if (CinemachineLogoTexture == null) {
+				// After adding the CM to a project, we need to wait for one update cycle for the assets to load
+                EditorApplication.update += OnPackageLoadedInEditor; 
+			}
+            else
+            {
+                EditorApplication.update -= OnPackageLoadedInEditor;
+                EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyGUI; // Update hierarchy with CM Brain logo
+            }
+        }
+
         static CinemachineSettings()
         {
             if (CinemachineLogoTexture != null)
