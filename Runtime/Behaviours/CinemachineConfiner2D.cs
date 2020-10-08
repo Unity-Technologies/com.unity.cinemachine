e69bc32e39073a74334bbf160a068217bf314bb0
Krisztian G. Birkas <gaborkb@unity3d.com>
2020-10-08 10:24:33 -0400

    Sidedamping removed + cleanup

--- Runtime/Behaviours/CinemachineConfiner2D.cs
+++ Runtime/Behaviours/CinemachineConfiner2D.cs
@@ -27,25 +27,12 @@ namespace Cinemachine
         public Collider2D m_BoundingShape2D;
 
         /// <summary>Damping applied automatically around corners to avoid jumps.</summary>
-        [Tooltip("Damping applied automatically around corners to avoid jumps.  "
-                 + "Higher numbers are more gradual.")]
+        [Tooltip("Damping applied around corners to avoid jumps.  Higher numbers are more gradual.")]
         [Range(0, 5)]
-        public float m_CornerDamping = 0;
+        public float m_Damping = 0;
         private bool m_CornerDampingIsOn = false;
         private float m_CornerDampingSpeedup = 1f;
         private float m_CornerAngleTreshold = 10f;
-        /// <summary>Damping applied when getting within the specified proximity to the sides.</summary>
-        [Tooltip("Damping applied when getting within the specified proximity to the sides.  When side damping is " +
-                 "enabled, then corner damping must be enabled with equal or greater value.")]
-        [Range(0, 5)]
-        public float m_SideDamping = 0;
-        
-        /// <summary>User specified proximity used by Side Damping</summary>
-        [Tooltip("Proximity used by Side Damping")]
-        [Range(0, 100)]
-        public float m_SideDampingProximity = 10;
-        private float m_SideDampingTime;
-        private bool m_SideDampingOn = false;
 
         // advanced features
         public bool m_DrawGizmos = false;
@@ -75,6 +62,13 @@ namespace Cinemachine
         private List<ConfinerOven.ConfinerState> m_confinerStates;
         private ConfinerOven m_confinerBaker = null;
 
+        /// <summary>Force rebake manually. This function invalidates the cache and rebakes the confiner.</summary>
+        public void ForceBake()
+        {
+            InvalidatePathCache();
+            Bake();
+        }
+        
         /// <summary>
         /// Trigger rebake process manually.
         /// The confiner rebakes iff an input parameters affecting the outcome of the baked result change.
@@ -84,22 +78,6 @@ namespace Cinemachine
             m_TriggerBake = true;
         }
 
-        /// <summary>Force rebake manually. This function invalidates the cache and rebakes the confiner.</summary>
-        public void ForceBake()
-        {
-            InvalidatePathCache();
-            Bake();
-        }
-
-        private void OnValidate()
-        {
-            m_CornerDamping = Mathf.Clamp(m_CornerDamping, m_SideDamping, float.MaxValue);
-        }
-
-        private Vector3 prevPosition = Vector3.zero;
-        private Vector2 prevDampVector = Vector2.zero;
-        private float sideDampingCatchupSpeed = 0;
-        private float catchupTimer = 0;
         protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, 
             CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
         {
@@ -118,11 +96,11 @@ namespace Cinemachine
                 if (VirtualCamera.PreviousStateIsValid && deltaTime >= 0)
                 { 
                     float displacementAngle = Vector2.Angle(extra.m_previousDisplacement, displacement);
-                    if (m_CornerDampingIsOn || m_CornerDamping > 0 && displacementAngle > m_CornerAngleTreshold)
+                    if (m_CornerDampingIsOn || m_Damping > 0 && displacementAngle > m_CornerAngleTreshold)
                     {
                         Vector3 delta = displacement - extra.m_previousDisplacement;
                         var deltaDamped = 
-                            Damper.Damp(delta, m_CornerDamping / m_CornerDampingSpeedup, deltaTime);
+                            Damper.Damp(delta, m_Damping / m_CornerDampingSpeedup, deltaTime);
                         displacement = extra.m_previousDisplacement + deltaDamped;
 
                         m_CornerDampingSpeedup = displacementAngle < 1f ? 2f : 1f;
@@ -133,94 +111,6 @@ namespace Cinemachine
                 extra.m_previousDisplacement = displacement;
                 state.PositionCorrection += displacement;
                 extra.confinerDisplacement = displacement.magnitude;
-                
-                if (!m_CornerDampingIsOn && m_SideDamping > 0)
-                {
-                    Vector3 delta = state.CorrectedPosition - prevPosition;
-
-                    state.PositionCorrection -= delta;
-                    
-                    GetDampVectorBasedOnDirection(state.CorrectedPosition, delta,
-                        in m_currentPathCache, in m_SideDampingProximity,
-                        out Vector2 dampVector);
-
-                    dampVector = dampVector.Abs();
-                    if (dampVector == Vector2.zero)
-                    {
-                        sideDampingCatchupSpeed = Mathf.Max(1, delta.sqrMagnitude) * 2f;
-                        sideDampingCatchupSpeed = Mathf.Lerp(1, sideDampingCatchupSpeed, catchupTimer);
-                        var multiplier = 1f / m_SideDamping; // TODO: test
-                        catchupTimer += deltaTime * multiplier;
-                    }
-                    else
-                    {
-                        catchupTimer = 0;
-                        prevDampVector = dampVector;
-                    }
-
-                    bool zeroDampVector = dampVector.sqrMagnitude > UnityVectorExtensions.Epsilon;
-                    if (m_SideDampingOn || zeroDampVector)
-                    {
-                        if (zeroDampVector)
-                        {
-                            m_SideDampingTime += deltaTime;
-                            m_SideDampingOn = true;
-                            if (m_SideDampingTime >= 1)
-                            {
-                                m_SideDampingTime = 1;
-                            }
-                        }
-                        else
-                        {
-                            m_SideDampingTime -= deltaTime;
-                            if (m_SideDampingTime <= 0)
-                            {
-                                m_SideDampingTime = 0;
-                                m_SideDampingOn = false;
-                            }
-                        }
-                        float sideSmoothingValue =
-                            Mathf.Lerp(0, m_SideDamping, m_SideDampingTime);
-                        if (dampVector.x > UnityVectorExtensions.Epsilon)
-                        {
-                            dampVector.x = Mathf.Max(dampVector.x, 1f);
-                            delta.x = Damper.Damp(delta.x, sideSmoothingValue, deltaTime * dampVector.x);
-                        }
-                        else
-                        {
-                            if (dampVector.x < UnityVectorExtensions.Epsilon && 
-                                prevDampVector.x > UnityVectorExtensions.Epsilon)
-                            {
-                                delta.x = Damper.Damp(delta.x, sideSmoothingValue, deltaTime * sideDampingCatchupSpeed);
-                            }
-                            else 
-                            {
-                                delta.x = Damper.Damp(delta.x, 0, deltaTime);
-                            }
-                        }
-
-                        if (dampVector.y > UnityVectorExtensions.Epsilon)
-                        {
-                            dampVector.y = Mathf.Max(dampVector.y, 1f);
-                            delta.y = Damper.Damp(delta.y, sideSmoothingValue, deltaTime * dampVector.y);
-                        }
-                        else
-                        {
-                            if (dampVector.y < UnityVectorExtensions.Epsilon && 
-                                prevDampVector.y > UnityVectorExtensions.Epsilon)
-                            {
-                                delta.y = Damper.Damp(delta.y, sideSmoothingValue, deltaTime * sideDampingCatchupSpeed);
-                            }
-                            else
-                            {
-                                delta.y = Damper.Damp(delta.y, 0, deltaTime);
-                            }
-                        }
-                    }
-                    state.PositionCorrection += delta;
-                }
-                
-                prevPosition = state.CorrectedPosition;
             }
         }
 
@@ -250,132 +140,7 @@ namespace Cinemachine
             }
             return frustumHeight;
         }
-
-
-        /// <summary>
-        /// Returns a 2D damping vector, that defines a component-wise damping. If a component is 0, that means no
-        /// damping in that directions. For example, (0,0) means no damping in either X or Y directions, and (1,1) means
-        /// damping in both.
-        /// Damping Vector is based on the closest edges in the direction the player is moving. The direction is
-        /// decomposed into an X and Y component. These are used to find the closest two edges.
-        /// </summary>
-        /// <param name="position">Position of the player</param>
-        /// <param name="direction">Direction of the player's velocity</param>
-        /// <param name="polygons">Polygons that define the confiner area</param>
-        /// <param name="proximity">How far to check for edges</param>
-        /// <param name="dampVector">2D Vector defining damping to be applied on player</param>
-        private void GetDampVectorBasedOnDirection(
-            Vector2 position, Vector2 direction, in List<List<Vector2>> polygons, in float proximity,
-            out Vector2 dampVector)
-        {
-            dampVector = Vector2.zero;
-            
-            var horizontalSearchVector = new Vector2(Math.Abs(direction.x) < UnityVectorExtensions.Epsilon ? 
-                0 : 
-                Mathf.Sign(direction.x) * proximity, 0); 
-            var verticalSearchVector = new Vector2(0, Math.Abs(direction.y) < UnityVectorExtensions.Epsilon ? 
-                0 : 
-                Mathf.Sign(direction.y) * proximity);
-            
-            if (direction.sqrMagnitude < UnityVectorExtensions.Epsilon)
-            {
-                return;
-            }
-            direction.Normalize();
-            
-            var normalH = Vector2.zero;
-            var normalV = Vector2.zero;
-            float minHorizontalDistance = proximity;
-            float minVerticalDistance = proximity;
-            for (var i = 0; i < polygons.Count; i++)
-            {
-                for (var p = 0; p < polygons[i].Count; p++)
-                {
-                    int nextP = (p + 1) % polygons[i].Count;
-                    UnityVectorExtensions.FindIntersection(
-                        position, position + horizontalSearchVector,
-                        polygons[i][p], polygons[i][nextP],
-                        out _, out bool segmentsIntersect, out _);
-
-                    if (segmentsIntersect)
-                    {
-                        var horizontalDistance = UnityVectorExtensions.DistanceBetweenPointAndLineSegment(position, 
-                            polygons[i][p],
-                            polygons[i][nextP],
-                            out float onSegment);
-
-                        if (horizontalDistance < minHorizontalDistance)
-                        {
-                            minHorizontalDistance = horizontalDistance;
-                            var edge = polygons[i][p] - polygons[i][nextP];
-                            if (onSegment < 0)
-                            {
-                                normalH = position - polygons[i][p];
-                            }
-                            else if (onSegment > 1)
-                            {
-                                normalH = position - polygons[i][nextP];
-                            }
-                            else
-                            {
-                                normalH = new Vector2(edge.y, -edge.x);
-                            }
-                        }
-                    }
-                    UnityVectorExtensions.FindIntersection(
-                        position, position + verticalSearchVector, 
-                        polygons[i][p], polygons[i][nextP],
-                        out _, out segmentsIntersect, out _);
-
-                    if (segmentsIntersect)
-                    {
-                        var verticalDistance = UnityVectorExtensions.DistanceBetweenPointAndLineSegment(position, 
-                            polygons[i][p],
-                            polygons[i][nextP],
-                            out float onSegment);
-
-                        if (verticalDistance < minVerticalDistance)
-                        {
-                            minVerticalDistance = verticalDistance;
-                            var edge = polygons[i][p] - polygons[i][nextP];
-                            if (onSegment < 0)
-                            {
-                                normalV = position - polygons[i][p];
-                            }
-                            else if (onSegment > 1)
-                            {
-                                normalV = position - polygons[i][nextP];
-                            }
-                            else
-                            {
-                                normalV = new Vector2(edge.y, -edge.x);
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (minHorizontalDistance >= proximity)
-            {
-                normalH = Vector2.zero;
-            }
-            else
-            {
-                normalH = normalH.normalized * minHorizontalDistance;
-            }
-
-            if (minVerticalDistance >= proximity)
-            {
-                normalV = Vector2.zero;
-            }
-            else
-            {
-                normalV = normalV.normalized * minVerticalDistance;
-            }
-
-            dampVector = (normalH + normalV).normalized;
-        }
-
+        
         /// <summary>
         /// Confines input 2D point within the confined area.
         /// </summary>
@@ -628,9 +393,6 @@ namespace Cinemachine
                         path[(index + 1) % path.Count]);
                 }
             }
-            
-            Handles.color = Color.green;
-            Handles.DrawWireDisc(prevPosition, Vector3.back, m_SideDampingProximity);
         }
     }
 #endif
