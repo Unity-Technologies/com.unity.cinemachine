82a61642d40816c99dc5bfcfe1c955130c4a1cd0
Krisztian G. Birkas <57672095+gaborkb@users.noreply.github.com>
2020-12-02 15:40:52 -0500

    Update Cinemachine3rdPersonFollow.cs
    Revert previous commit

--- Runtime/Components/Cinemachine3rdPersonFollow.cs
+++ Runtime/Components/Cinemachine3rdPersonFollow.cs
@@ -1,7 +1,7 @@
-#if !UNITY_2019_3_OR_NEWER
+//#if !UNITY_2019_3_OR_NEWER
 #define CINEMACHINE_PHYSICS
 #define CINEMACHINE_PHYSICS_2D
-#endif
+//#endif
 
 using UnityEngine;
 using Cinemachine.Utility;
