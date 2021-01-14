12d1dbb6dda4fdad4e86f687aec3c0cfa7835112
Krisztian G. Birkas <gaborkb@unity3d.com>
2021-01-14 13:14:22 -0500

    Moved to Editor/Utility

--- Editor/Utility/CinemachineEditorAnalytics.cs
+++ Editor/Utility/CinemachineEditorAnalytics.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-namespace Cinemachine
+namespace Cinemachine.Editor
 {
     [InitializeOnLoad]
     static class CinemachineEditorAnalytics
