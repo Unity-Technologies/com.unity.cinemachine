626c40d047a4297972a544ef4d0e65de824bb111
Krisztian G. Birkas <gaborkb@unity3d.com>
2021-03-12 01:51:58 -0500

    Moved Editable class into CinemachineScreenComposerGuides

--- Editor/Editors/CinemachineScreenComposerGuides.cs
+++ Editor/Editors/CinemachineScreenComposerGuides.cs
@@ -8,6 +8,29 @@ using UnityEngine.UIElements;
 
 namespace Cinemachine.Editor
 {
+    [InitializeOnLoad]
+    static class CinemachineScreenComposerGuidesGlobalEditable
+    {
+        static CinemachineScreenComposerGuidesGlobalEditable()
+        {
+            CinemachineScreenComposerGuides.sEditableGameWindowGuides = Enabled;
+        }
+
+        public static string kEnabledKey = "EditableScreenComposerGuides_Enabled";
+        public static bool Enabled
+        {
+            get => EditorPrefs.GetBool(kEnabledKey, true);
+            set
+            {
+                if (value != CinemachineScreenComposerGuides.sEditableGameWindowGuides)
+                {
+                    EditorPrefs.SetBool(kEnabledKey, value);
+                    CinemachineScreenComposerGuides.sEditableGameWindowGuides = value;
+                }
+            }
+        }
+    }
+    
 #if !UNITY_2019_2_OR_NEWER
     internal class GameViewEventCatcher
     {
