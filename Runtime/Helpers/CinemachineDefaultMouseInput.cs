3090153db6502f491e13488bdf31577eccd799e8
Krisztian G. Birkas <gaborkb@unity3d.com>
2021-03-23 16:42:02 -0400

    Using Input System package's default inputs

--- Runtime/Helpers/CinemachineDefaultMouseInput.cs
+++ Runtime/Helpers/CinemachineDefaultMouseInput.cs
@@ -3,172 +3,42 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
 namespace Cinemachine
 {
+    /// <summary>
+    /// Finds InputSystem package's default mouse control look input asset, and returns a reference to it.
+    /// </summary>
     static class CinemachineDefaultMouseInput
     {
-        static DefaultMouseInput s_DefaultMouseInput = new DefaultMouseInput();
+        static InputActionReference s_InputActionReference = null;
+        static CinemachineDefaultMouseInput()
+        {
+            var inputActionAsset = AssetDatabase.LoadAssetAtPath<InputActionAsset>("Packages/com.unity.inputsystem/" +
+                "InputSystem/Plugins/PlayerInput/DefaultInputActions.inputactions");
+
+            InputAction look;
+            var enumerator = inputActionAsset.GetEnumerator();
+            for (int i = 0; i < 40; ++i)
+            {
+                if (enumerator.Current != null && 
+                    enumerator.Current.ToString() == "Player/Look[/Mouse/delta,/Pen/delta]")
+                {
+                    look = enumerator.Current;
+                    s_InputActionReference = InputActionReference.Create(look);
+                    s_InputActionReference.name = "PlayerLook";
+                    break;
+                }
+                enumerator.MoveNext();
+            }
+        }
         public static InputActionReference GetInputActionReference()
         {
-            return InputActionReference.Create(s_DefaultMouseInput.CMInputProvider.MouseXY);
-        }
-        
-        // GENERATED AUTOMATICALLY FROM 'Assets/DefaultMouseInput.inputactions'
-        class @DefaultMouseInput : IInputActionCollection, IDisposable
-        {
-            public InputActionAsset asset { get; }
-
-            public @DefaultMouseInput()
-            {
-                asset = InputActionAsset.FromJson(@"{
-                    ""name"": ""DefaultMouseInput"",
-                    ""maps"": [
-                        {
-                            ""name"": ""CMInputProvider"",
-                            ""id"": ""58fd55e8-1d7c-4e1c-8b41-c5b0b9b084e1"",
-                            ""actions"": [
-                                {
-                                    ""name"": ""MouseXY"",
-                                    ""type"": ""Value"",
-                                    ""id"": ""0fb909e5-6fae-4b52-9c6a-42e266a91c63"",
-                                    ""expectedControlType"": ""Vector2"",
-                                    ""processors"": """",
-                                    ""interactions"": """"
-                                }
-                            ],
-                            ""bindings"": [
-                                {
-                                    ""name"": """",
-                                    ""id"": ""49707a4f-ce91-4104-80ee-405a8a9b4cfe"",
-                                    ""path"": ""<Pointer>/delta"",
-                                    ""interactions"": """",
-                                    ""processors"": ""ScaleVector2"",
-                                    ""groups"": ""Mouse"",
-                                    ""action"": ""MouseXY"",
-                                    ""isComposite"": false,
-                                    ""isPartOfComposite"": false
-                                }
-                            ]
-                        }
-                    ],
-                    ""controlSchemes"": []
-                }");
-
-                // CMInputProvider
-                m_CMInputProvider = asset.FindActionMap("CMInputProvider", throwIfNotFound: true);
-                m_CMInputProvider_MouseXY = m_CMInputProvider.FindAction("MouseXY", throwIfNotFound: true);
-            }
-
-            public void Dispose()
-            {
-                UnityEngine.Object.Destroy(asset);
-            }
-
-            public InputBinding? bindingMask
-            {
-                get => asset.bindingMask;
-                set => asset.bindingMask = value;
-            }
-
-            public ReadOnlyArray<InputDevice>? devices
-            {
-                get => asset.devices;
-                set => asset.devices = value;
-            }
-
-            public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
-
-            public bool Contains(InputAction action)
-            {
-                return asset.Contains(action);
-            }
-
-            public IEnumerator<InputAction> GetEnumerator()
-            {
-                return asset.GetEnumerator();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
-
-            public void Enable()
-            {
-                asset.Enable();
-            }
-
-            public void Disable()
-            {
-                asset.Disable();
-            }
-
-            // CMInputProvider
-            private readonly InputActionMap m_CMInputProvider;
-            private ICMInputProviderActions m_CMInputProviderActionsCallbackInterface;
-            private readonly InputAction m_CMInputProvider_MouseXY;
-
-            public struct CMInputProviderActions
-            {
-                private @DefaultMouseInput m_Wrapper;
-
-                public CMInputProviderActions(@DefaultMouseInput wrapper)
-                {
-                    m_Wrapper = wrapper;
-                }
-
-                public InputAction @MouseXY => m_Wrapper.m_CMInputProvider_MouseXY;
-
-                public InputActionMap Get()
-                {
-                    return m_Wrapper.m_CMInputProvider;
-                }
-
-                public void Enable()
-                {
-                    Get().Enable();
-                }
-
-                public void Disable()
-                {
-                    Get().Disable();
-                }
-
-                public bool enabled => Get().enabled;
-
-                public static implicit operator InputActionMap(CMInputProviderActions set)
-                {
-                    return set.Get();
-                }
-
-                public void SetCallbacks(ICMInputProviderActions instance)
-                {
-                    if (m_Wrapper.m_CMInputProviderActionsCallbackInterface != null)
-                    {
-                        @MouseXY.started -= m_Wrapper.m_CMInputProviderActionsCallbackInterface.OnMouseXY;
-                        @MouseXY.performed -= m_Wrapper.m_CMInputProviderActionsCallbackInterface.OnMouseXY;
-                        @MouseXY.canceled -= m_Wrapper.m_CMInputProviderActionsCallbackInterface.OnMouseXY;
-                    }
-
-                    m_Wrapper.m_CMInputProviderActionsCallbackInterface = instance;
-                    if (instance != null)
-                    {
-                        @MouseXY.started += instance.OnMouseXY;
-                        @MouseXY.performed += instance.OnMouseXY;
-                        @MouseXY.canceled += instance.OnMouseXY;
-                    }
-                }
-            }
-
-            public CMInputProviderActions @CMInputProvider => new CMInputProviderActions(this);
-
-            public interface ICMInputProviderActions
-            {
-                void OnMouseXY(InputAction.CallbackContext context);
-            }
+            return s_InputActionReference;
         }
     }
 }
