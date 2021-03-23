2742fa22fe2cc1af62742393e62c7cdd8da3096d
Krisztian G. Birkas <gaborkb@unity3d.com>
2021-03-23 12:29:14 -0400

    Prep for input adding

--- Editor/Editors/CinemachinePOVEditor.cs
+++ Editor/Editors/CinemachinePOVEditor.cs
@@ -1,15 +1,38 @@
 using UnityEditor;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Cinemachine.Editor
 {
     [CustomEditor(typeof(CinemachinePOV))]
-    internal sealed class CinemachinePOVEditor : BaseEditor<CinemachinePOV>
+    sealed class CinemachinePOVEditor : BaseEditor<CinemachinePOV>
     {
-        private void OnEnable()
+        GUIContent m_InputProviderAddLabel = new GUIContent(
+            "Add cm input provider", "Adds CinemachineInputProvider to this vcam, enabling it to read input from" +
+            "input actions using the UnityEngine.Input package API.");
+
+        public override void OnInspectorGUI()
+        {
+            BeginInspector();
+            DrawRemainingPropertiesInInspector();
+            
+            var rect = EditorGUILayout.GetControlRect(true);
+            if (GUI.Button(rect, m_InputProviderAddLabel))
+            {
+                var myGO = ((CinemachinePOV)target).gameObject;
+                var inputProvider = myGO.GetComponent<CinemachineInputProvider>();
+                if (inputProvider == null)
+                {
+                    inputProvider = myGO.AddComponent<CinemachineInputProvider>();
+                    //inputProvider.XYAxis = 
+                }
+            }
+        }
+        
+        void OnEnable()
         {
             Target.UpdateInputAxisProvider();
         }
-
     }
 }
