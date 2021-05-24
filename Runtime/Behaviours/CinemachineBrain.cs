caaad5d7dd24d24768203602e19a9a3bf3c4cd4b
Krisztian G. Birkas <gaborkb@unity3d.com>
2021-05-24 05:09:34 -0400

    Blend condition bug

--- Runtime/Behaviours/CinemachineBrain.cs
+++ Runtime/Behaviours/CinemachineBrain.cs
@@ -679,9 +679,9 @@ namespace Cinemachine
                         {
                             // Special case: if backing out of a blend-in-progress
                             // with the same blend in reverse, adjust the blend time
-                            if (frame.blend.CamA == activeCamera
-                                && frame.blend.CamB == outGoingCamera
-                                && frame.blend.Duration <= blendDef.BlendTime)
+                            if (//frame.blend.CamA == activeCamera && 
+                                frame.blend.CamB == outGoingCamera && 
+                                frame.blend.Duration <= blendDef.BlendTime)
                             {
                                 blendDef.m_Time = 
                                     (frame.blend.TimeInBlend / frame.blend.Duration) * blendDef.BlendTime;
