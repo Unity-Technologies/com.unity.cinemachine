e056b51525b707cc84449d62b44c3a60efbfc8a4
Mark Visser <markv@unity3d.com>
2021-06-26 00:06:18 -0400

    cleanup * use Assert.That instead of boolean expressions * use parameterized tests where possible to avoid copypasta * enclose in Tests.Editor namespace

--- Tests/Editor/AxisStateTests.cs
+++ Tests/Editor/AxisStateTests.cs
@@ -1,95 +1,90 @@
-using UnityEngine;
-using UnityEngine.TestTools;
-using NUnit.Framework;
 using System.Collections;
+using NUnit.Framework;
 using Cinemachine;
 
-public class AxisStateTests
+namespace Tests.Editor
 {
-    struct TestAxisProvider : AxisState.IInputAxisProvider
+    public class AxisStateTests
     {
-        public float value;
-
-        public TestAxisProvider(float value)
+        struct TestAxisProvider : AxisState.IInputAxisProvider
         {
-            this.value = value;
+            public float value;
+
+            public TestAxisProvider(float value)
+            {
+                this.value = value;
+            }
+
+            public float GetAxisValue(int axis)
+            {
+                return value;
+            }
         }
-        
-        public float GetAxisValue(int axis)
+
+        static IEnumerable AxisStateTestCases
         {
-            return value;
+            get
+            {
+                yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f).Returns(1.0f);
+                yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.5f).Returns(5.0f);
+                yield return new TestCaseData(-100f, 100f, false, 1f, 0.5f, 0.5f, false, 1.0f).Returns(1.0f);
+                yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 100.0f).Returns(10.0f);
+                yield return new TestCaseData(-13f, 5f, false, 10f, 0.5f, 0.5f, false, 100.0f).Returns(5.0f);
+                yield return new TestCaseData(-13f, 5f, true, 10f, 0.5f, 0.5f, false, 100.0f).Returns(-12.0f);
+
+                yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, true, 0.1f).Returns(-1.0f);
+                yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, true, 0.5f).Returns(-5.0f);
+                yield return new TestCaseData(-100f, 100f, false, 1f, 0.5f, 0.5f, true, 1.0f).Returns(-1.0f);
+                yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, true, 100.0f).Returns(-10.0f);
+                yield return new TestCaseData(-13f, 5f, false, 10f, 0.5f, 0.5f, true, 100.0f).Returns(-13.0f);
+                yield return new TestCaseData(-13f, 5f, true, 10f, 0.5f, 0.5f, true, 100.0f).Returns(4.0f);
+            }
         }
-    }
-    
-    static IEnumerable AxisStateTestCases
-    {
-        get
+
+        [Test, TestCaseSource(nameof(AxisStateTestCases))]
+        public float TestAxisState(float minValue, float maxValue, bool wrap,
+            float maxSpeed, float accelTime, float decelTime,
+            bool invert, float axisValue)
         {
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f).Returns(1.0f);
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.5f).Returns(5.0f);
-            yield return new TestCaseData(-100f, 100f, false, 1f, 0.5f, 0.5f, false, 1.0f).Returns(1.0f);
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 100.0f).Returns(10.0f);
-            yield return new TestCaseData(-13f, 5f, false, 10f, 0.5f, 0.5f, false, 100.0f).Returns(5.0f);
-            yield return new TestCaseData(-13f, 5f, true, 10f, 0.5f, 0.5f, false, 100.0f).Returns(-12.0f);
+            var axisState = new AxisState(minValue, maxValue, wrap, false, maxSpeed, accelTime, decelTime, null, invert);
+            axisState.SetInputAxisProvider(0, new TestAxisProvider(axisValue));
+            axisState.Validate();
 
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, true, 0.1f).Returns(-1.0f);
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, true, 0.5f).Returns(-5.0f);
-            yield return new TestCaseData(-100f, 100f, false, 1f, 0.5f, 0.5f, true, 1.0f).Returns(-1.0f);
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, true, 100.0f).Returns(-10.0f);
-            yield return new TestCaseData(-13f, 5f, false, 10f, 0.5f, 0.5f, true, 100.0f).Returns(-13.0f);
-            yield return new TestCaseData(-13f, 5f, true, 10f, 0.5f, 0.5f, true, 100.0f).Returns(4.0f);
+            var success = axisState.Update(1.0f);
+            Assert.IsTrue(success, "Update had no effect");
+
+            return axisState.Value;
         }
-    }
-    
-    [Test, TestCaseSource(nameof(AxisStateTestCases))]
-    public float TestAxisState(float minValue, float maxValue, bool wrap, 
-        float maxSpeed, float accelTime, float decelTime,
-        bool invert, float axisValue)
-    {
-        var axisState = new AxisState(minValue, maxValue, wrap, false, maxSpeed, accelTime, decelTime, null, invert);
-        axisState.SetInputAxisProvider(0, new TestAxisProvider(axisValue));
-        axisState.Validate();
-        
-        var success = axisState.Update(1.0f);
-        Assert.IsTrue(success, "Update had no effect");
 
-        return axisState.Value;
-    }
-
-    static IEnumerable RecenteringTestCases
-    {
-        get
+        static IEnumerable RecenteringTestCases
         {
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, true, 0.01f).Returns(0.0f);
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, true, 5f).Returns(0.180375189f);
+            get
+            {
+                yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, true, 0.01f, 0.0f);
+                yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, true, 5f, 0.180375189f);
 
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, false, 0.01f).Returns(1.0f);
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, false, 5f).Returns(1.0f);
+                yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, false, 0.01f, 1.0f);
+                yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, false, 5f, 1.0f);
 
+            }
         }
-    }
 
-    [Test, TestCaseSource(nameof(RecenteringTestCases))]
-    public float TestRecentering(float minValue, float maxValue, bool wrap,
-        float maxSpeed, float accelTime, float decelTime, bool invert, float axisValue,
-        bool enabled, float recenteringTime)
-    {
-        var axisState = new AxisState(minValue, maxValue, false, false, maxSpeed, accelTime, decelTime, null, invert);
-        axisState.SetInputAxisProvider(0, new TestAxisProvider(axisValue));
-        axisState.Validate();
+        [Test, TestCaseSource(nameof(RecenteringTestCases))]
+        public void TestRecentering(float minValue, float maxValue, bool wrap,
+            float maxSpeed, float accelTime, float decelTime, bool invert, float axisValue,
+            bool enabled, float recenteringTime, float expectedValue)
+        {
+            var axisState = new AxisState(minValue, maxValue, false, false, maxSpeed, accelTime, decelTime, null, invert);
+            axisState.SetInputAxisProvider(0, new TestAxisProvider(axisValue));
+            axisState.Validate();
 
-        var success = axisState.Update(1.0f);
-        Assert.IsTrue(success, "Update had no effect");
+            var success = axisState.Update(1.0f);
+            Assert.IsTrue(success, "Update had no effect");
 
-        var recentering = new AxisState.Recentering(enabled, 0.0f, recenteringTime);
-        recentering.DoRecentering(ref axisState, 10.0f, 0.0f);
+            var recentering = new AxisState.Recentering(enabled, 0.0f, recenteringTime);
+            recentering.DoRecentering(ref axisState, 10.0f, 0.0f);
 
-        return axisState.Value < Cinemachine.Utility.UnityVectorExtensions.Epsilon ? 0f : axisState.Value;
-    }
-
-    [TearDown]
-    public static void TearDown()
-    {
-        Cinemachine.CinemachineCore.CurrentTimeOverride = -1;
+            Assert.That(axisState.Value, Is.EqualTo(expectedValue).Within(Cinemachine.Utility.UnityVectorExtensions.Epsilon));
+        }
     }
 }
