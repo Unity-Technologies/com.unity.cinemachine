d5d7f12c8e3cf775aeab0dc97ca046698d9e58b1
Krisztian G. Birkas <gaborkb@unity3d.com>
2021-10-22 09:19:52 -0400

    Lambda on callback instead of function

--- Editor/Editors/CinemachineVirtualCameraBaseEditor.cs
+++ Editor/Editors/CinemachineVirtualCameraBaseEditor.cs
@@ -90,16 +90,12 @@ namespace Cinemachine.Editor
                 excluded.AddRange(Target.m_ExcludedPropertiesInInspector);
         }
 
-        void InternalUpdateStateOnUndoRedo()
-        {
-            Target.InternalUpdateCameraState(Vector3.up, 0); // update state on undo, and do nothing else
-        }
-
         /// <summary>Inspector panel is being enabled.  
         /// Implementation should call the base class implementation</summary>
         protected virtual void OnEnable()
         {
-            Undo.undoRedoPerformed += InternalUpdateStateOnUndoRedo;
+            // register to update state information on undo/redo
+            Undo.undoRedoPerformed += () => Target.InternalUpdateCameraState(Vector3.up, 0);
             
             IsPrefabBase = Target.gameObject.scene.name == null; // causes a small GC alloc
             if (sExtensionTypes == null)
