9a150e009028c505154c88679b2ebb758f62da4d
Krisztian G. Birkas <gaborkb@unity3d.com>
2021-11-08 15:52:31 -0500

    Text update

--- Samples~/Cinemachine Example Scenes/Shared/Scripts/InputSystemHelper.cs
+++ Samples~/Cinemachine Example Scenes/Shared/Scripts/InputSystemHelper.cs
@@ -11,11 +11,11 @@ namespace Cinemachine.Examples
             if (Time.realtimeSinceStartup - s_LastMessageTime > 5)
             {
                 Debug.Log(
-                    "Old input backends are disabled. Cinemachine examples use UnityEngine.Input. " +
-                    "To enable input in our examples: Edit > Project Settings > Player, " +
+                    "Old input backends are disabled. Cinemachine examples use Unity’s classic input system. " +
+                    "To enable classic input: Edit > Project Settings > Player, " +
                     "set Active Input Handling to Both!\n" +
                     "Note: This is probably because the Input System Package has been added to the project. To use " +
-                    "Cinemachine Virtual Cameras with Input System Package, add CinemachineInputProvider to them.");
+                    "Cinemachine Virtual Cameras with the Input System Package, add CinemachineInputProvider component to them.");
                 s_LastMessageTime = Time.realtimeSinceStartup;
             }
         }
