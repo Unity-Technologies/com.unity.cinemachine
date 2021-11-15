76dae2e166db4c1da51f777143e22999f7c5027d
Krisztian G. Birkas <gaborkb@unity3d.com>
2021-11-15 15:43:25 -0500

    Cleanup, all tools, todo for freelook selection

--- Editor/Overlays/CinemachineToolbarOverlay.cs
+++ Editor/Overlays/CinemachineToolbarOverlay.cs
@@ -2,7 +2,6 @@
 using System;
 using UnityEditor;
 using UnityEditor.EditorTools;
-using UnityEditor.Overlays;
 using UnityEditor.Toolbars;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -47,6 +46,44 @@ namespace Cinemachine.Editor
         }
     }
     
+    [EditorTool("Field of View Tool", typeof(CinemachineVirtualCameraBase))]
+    class FoVTool : CinemachineTool
+    {
+        FoVTool()
+        {
+            m_Type = typeof(FoVTool);
+        }
+        void OnEnable()
+        {
+            m_IconContent = new GUIContent()
+            {
+                image = AssetDatabase.LoadAssetAtPath<Texture2D>(ScriptableObjectUtility.CinemachineRealativeInstallPath 
+                    + "/Editor/EditorResources/FOV.png"),
+                text = "Field of View Tool",
+                tooltip = "Field of View Tool",
+            };
+        }
+    }
+    
+    [EditorTool("Far/Near Clip Tool", typeof(CinemachineVirtualCameraBase))]
+    class FarNearClipTool : CinemachineTool
+    {
+        FarNearClipTool()
+        {
+            m_Type = typeof(FarNearClipTool);
+        }
+        void OnEnable()
+        {
+            m_IconContent = new GUIContent()
+            {
+                image = AssetDatabase.LoadAssetAtPath<Texture2D>(ScriptableObjectUtility.CinemachineRealativeInstallPath 
+                    + "/Editor/EditorResources/FarNearClip.png"),
+                text = "Far/Near Clip Tool",
+                tooltip = "Far/Near Clip Tool",
+            };
+        }
+    }
+    
     [EditorTool("Follow Offset Tool", typeof(CinemachineVirtualCameraBase))]
     class FollowOffsetTool : CinemachineTool
     {
@@ -66,119 +103,27 @@ namespace Cinemachine.Editor
         }
     }
 
-    
-    /// <summary>
-    /// To display a CinemachineExclusiveEditorToolbarToggle in the Cinemachine Toolbar,
-    /// TODO: Make this extendable when another PR lands (see: CMCL-501),
-    /// </summary>
-    // [Overlay(typeof(SceneView), "Cinemachine")]
-    // [Icon("Packages/com.unity.cinemachine/Gizmos/cm_logo.png")]
-    // class CinemachineToolbarOverlay : ToolbarOverlay
-    // {
-    //     public CinemachineToolbarOverlay()
-    //         : base(
-    //             FreelookRigSelection.id,
-    //             FoVTool.id,
-    //             FarNearClipTool.id,
-    //             //FollowOffsetTool.id,
-    //             TrackedObjectOffsetTool.id
-    //         )
-    //     {
-    //         CinemachineSceneToolUtility.RegisterToolbarIsDisplayedHandler(() => displayed);
-    //         CinemachineSceneToolUtility.RegisterToolbarDisplayHandler(v =>
-    //         {
-    //             if (displayed == v)
-    //             {
-    //                 return false;
-    //             }
-    //             displayed = v;
-    //             return true;
-    //         });
-    //     }
-    // }
-
-    /// <summary>
-    /// Creates a toggle tool on the Cinemachine toolbar that is exclusive with other
-    /// CinemachineExclusiveEditorToolbarToggles. Meaning, that maximum one
-    /// CinemachineExclusiveEditorToolbarToggle can be active at any time.
-    /// </summary>
-    abstract class CinemachineExclusiveEditorToolbarToggle : EditorToolbarToggle
+    [EditorTool("Tracked Object Offset Tool", typeof(CinemachineVirtualCameraBase))]
+    class TrackedObjectOffsetTool : CinemachineTool
     {
-        protected CinemachineExclusiveEditorToolbarToggle()
+        TrackedObjectOffsetTool()
         {
-            var type = GetType();
-            this.RegisterValueChangedCallback(
-                v => CinemachineSceneToolUtility.SetTool(v.newValue, type));
-            CinemachineSceneToolUtility.RegisterExclusiveToolHandlers(type, isOn => value = isOn, 
-                display => style.display = display ? DisplayStyle.Flex : DisplayStyle.None);
+            m_Type = typeof(TrackedObjectOffsetTool);
         }
-    }
-    
-    [EditorToolbarElement(id, typeof(SceneView))]
-    class FoVTool : CinemachineExclusiveEditorToolbarToggle
-    {
-        public const string id = "FoVTool/Toggle";
 
-        public FoVTool()
+        void OnEnable()
         {
-            icon = AssetDatabase.LoadAssetAtPath<Texture2D>(ScriptableObjectUtility.CinemachineRealativeInstallPath 
-                + "/Editor/EditorResources/FOV.png");
-            tooltip = "Field of View Tool";
+            m_IconContent = new GUIContent()
+            {
+                image = AssetDatabase.LoadAssetAtPath<Texture2D>(ScriptableObjectUtility.CinemachineRealativeInstallPath
+                    + "/Editor/EditorResources/TrackedObjectOffset.png"),
+                text = "Tracked Object Offset Tool",
+                tooltip = "Tracked Object Offset Tool",
+            };
         }
     }
 
-    [EditorToolbarElement(id, typeof(SceneView))]
-    class FarNearClipTool : CinemachineExclusiveEditorToolbarToggle
-    {
-        public const string id = "FarNearClipTool/Toggle";
-
-        public FarNearClipTool()
-        {
-            icon = AssetDatabase.LoadAssetAtPath<Texture2D>(ScriptableObjectUtility.CinemachineRealativeInstallPath 
-                + "/Editor/EditorResources/FarNearClip.png");
-            tooltip = "Far/Near Clip Tool";
-        }
-    }
-
-    // [EditorToolbarElement(id, typeof(SceneView))]
-    // class FollowOffsetTool : CinemachineExclusiveEditorToolbarToggle
-    // {
-    //     public const string id = "FollowOffsetTool/Toggle";
-    //
-    //     public FollowOffsetTool()
-    //     {
-    //         icon = AssetDatabase.LoadAssetAtPath<Texture2D>(ScriptableObjectUtility.CinemachineRealativeInstallPath 
-    //             + "/Editor/EditorResources/FollowOffset.png");
-    //         tooltip = "Follow Offset Tool";
-    //     }
-    // }
-
-    [EditorToolbarElement(id, typeof(SceneView))]
-    class TrackedObjectOffsetTool : CinemachineExclusiveEditorToolbarToggle
-    {
-        public const string id = "TrackedObjectOffsetTool/Toggle";
-
-        public TrackedObjectOffsetTool()
-        {
-            icon = AssetDatabase.LoadAssetAtPath<Texture2D>(ScriptableObjectUtility.CinemachineRealativeInstallPath 
-                + "/Editor/EditorResources/TrackedObjectOffset.png");
-            tooltip = "Tracked Object Offset Tool";
-        }
-    }
-    
-    /// <summary>
-    /// Creates a toggle tool on the Cinemachine toolbar.
-    /// </summary>
-    abstract class CinemachineEditorToolbarToggle : EditorToolbarToggle
-    {
-        protected CinemachineEditorToolbarToggle()
-        {
-            CinemachineSceneToolUtility.RegisterToolHandlers(GetType(), isOn => value = isOn, 
-                display => style.display = display ? DisplayStyle.Flex : DisplayStyle.None);
-        }
-    }
-    
-    
+    // TODO: FreelookRigSelection dropdown needs to be in the Tools Settings toolbar
     [EditorToolbarElement(id, typeof(SceneView))]
     class FreelookRigSelection : EditorToolbarDropdown
     {
