7efc9826e1a7f88419e722f432a3b407eb6f5d5e
cp <cp>
2022-01-28 09:53:00 +0100

    Refactor out abstract Track base class and move Brain- and Camera-related functionality into their own base classes
    Note: .meta GUID for the old Track moves to BrainTrack. This way, serialization of existing timeline assets does not break.

---
+++ Runtime/Timeline/CinemachineTrackBase.cs
@@ -0,0 +1,38 @@
+﻿#if !UNITY_2019_1_OR_NEWER
+#define CINEMACHINE_TIMELINE
+#endif
+#if CINEMACHINE_TIMELINE
+
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+internal abstract class CinemachineTrackBase<T> : TrackAsset
+    where T : class, IPlayableBehaviour, new()
+{
+    /// <summary>
+    /// TrackAsset implementation
+    /// </summary>
+    /// <param name="graph"></param>
+    /// <param name="go"></param>
+    /// <param name="inputCount"></param>
+    /// <returns></returns>
+    public override Playable CreateTrackMixer(
+        PlayableGraph graph, GameObject go, int inputCount)
+    {
+#if !UNITY_2019_2_OR_NEWER
+            // Hack to set the display name of the clip to match the vcam
+            foreach (var c in GetClips())
+            {
+                CinemachineShot shot = (CinemachineShot)c.asset;
+                CinemachineVirtualCameraBase vcam = shot.VirtualCamera.Resolve(graph.GetResolver());
+                if (vcam != null)
+                    c.displayName = vcam.Name;
+            }
+#endif
+        var mixer = ScriptPlayable<T>.Create(graph);
+        mixer.SetInputCount(inputCount);
+        return mixer;
+    }
+}
+#endif
