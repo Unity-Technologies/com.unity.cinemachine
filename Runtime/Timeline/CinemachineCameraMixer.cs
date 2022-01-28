9cf126f0e400576d2a4ba94408888bc1d9dcdd13
krisrok <3404365+krisrok@users.noreply.github.com>
2022-01-28 13:56:03 +0100

    Clear parameters just in case.
    But: Does not work if multiple Timelines were to write to the TimelineCamera at the same time. Maybe adapting ICameraOverrideStack isn't that bad after all?

--- Runtime/Timeline/CinemachineCameraMixer.cs
+++ Runtime/Timeline/CinemachineCameraMixer.cs
@@ -8,9 +8,20 @@ using Cinemachine;
 
 internal sealed class CinemachineCameraMixer : CinemachineMixerBase<CinemachineTimelineCamera>
 {
+    private CinemachineTimelineCamera m_TimelineCamera;
+
     protected override void ProcessBlendFrame(FrameData info, CinemachineTimelineCamera target, ICinemachineCamera camA, ICinemachineCamera camB, float blendWeight)
     {
+        m_TimelineCamera = target;
         target.SetBlendParameters(camA, camB, blendWeight);
+    }
+
+    public override void OnPlayableDestroy(Playable playable)
+    {
+        base.OnPlayableDestroy(playable);
+
+        if (m_TimelineCamera != null)
+            m_TimelineCamera.SetBlendParameters(null, null, 0);
     }
 }
 #endif
