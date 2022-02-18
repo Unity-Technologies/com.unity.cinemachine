ca63cbcc3579d799872714e186784cec2db85084
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-02-18 14:48:41 -0500

    Don't use baseeditor

--- Editor/Editors/CinemachineSplineDollyEditor.cs
+++ Editor/Editors/CinemachineSplineDollyEditor.cs
@@ -7,29 +7,18 @@ namespace Cinemachine.Editor
 {
     [CustomEditor(typeof(CinemachineSplineDolly))]
     [CanEditMultipleObjects]
-    sealed class CinemachineSplineDollyEditor : BaseEditor<CinemachineSplineDolly>
+    sealed class CinemachineSplineDollyEditor : UnityEditor.Editor
     {
-        protected override void GetExcludedPropertiesInInspector(List<string> excluded)
-        {
-            base.GetExcludedPropertiesInInspector(excluded);
-            excluded.Add(FieldPath(x => x.m_CameraPosition));
-            excluded.Add(FieldPath(x => x.m_PositionUnits));
-            excluded.Add(FieldPath(x => x.m_SplineOffset));
-            excluded.Add(FieldPath(x => x.m_CameraUp));
-            excluded.Add(FieldPath(x => x.m_DampingEnabled));
-            excluded.Add(FieldPath(x => x.m_Damping));
-            excluded.Add(FieldPath(x => x.m_AngularDamping));
-            excluded.Add(FieldPath(x => x.m_AutoDolly));
-        }
-
-        SerializedProperty m_CameraPosition, m_PositionUnits, m_SplineOffset, 
+        SerializedProperty m_Spline, m_CameraPosition, m_PositionUnits, m_SplineOffset, 
             m_CameraUp, m_DampingEnabled, m_Damping, 
             m_AngularDamping, m_AutoDollyEnabled, m_AutoDollyPositionOffset;
-        GUIContent m_CameraPositionGUIContent, m_PositionUnitsGUIContent, m_SplineOffsetGUIContent, 
+        GUIContent m_SplineGUIContent, m_CameraPositionGUIContent, m_PositionUnitsGUIContent, m_SplineOffsetGUIContent, 
             m_CameraUpGUIContent, m_DampingEnabledGUIContent, m_DampingGUIContent, 
             m_AngularDampingGUIContent, m_AutoDollyEnabledGUIContent, m_AutoDollyPositionOffsetGUIContent;
         void OnEnable()
         {
+            m_SplineGUIContent = new GUIContent("Spline", 
+                (m_Spline = serializedObject.FindProperty("m_Spline")).tooltip);
             m_CameraPositionGUIContent = new GUIContent("Camera Position",
                 (m_CameraPosition = serializedObject.FindProperty("m_CameraPosition")).tooltip);
             m_PositionUnitsGUIContent = new GUIContent("",
@@ -52,7 +41,8 @@ namespace Cinemachine.Editor
 
         public override void OnInspectorGUI()
         {
-            BeginInspector();
+            serializedObject.Update();
+            
             bool needWarning = false;
             for (int i = 0; !needWarning && i < targets.Length; ++i)
                 needWarning = (targets[i] as CinemachineSplineDolly).m_Spline == null;
@@ -66,32 +56,31 @@ namespace Cinemachine.Editor
             if (needWarning)
                 EditorGUILayout.HelpBox("AutoDolly requires a Follow Target", MessageType.Warning);
 
-            DrawRemainingPropertiesInInspector();
-
+            EditorGUILayout.PropertyField(m_Spline, m_SplineGUIContent);
             EditorGUILayout.BeginHorizontal();
             {
                 EditorGUILayout.PropertyField(m_CameraPosition, m_CameraPositionGUIContent);
                 EditorGUILayout.PropertyField(m_PositionUnits, m_PositionUnitsGUIContent, GUILayout.MaxWidth(100));
             } 
             EditorGUILayout.EndHorizontal();
-            
+
             EditorGUILayout.PropertyField(m_SplineOffset, m_SplineOffsetGUIContent);
             EditorGUILayout.PropertyField(m_CameraUp, m_CameraUpGUIContent);
-            
+
             EditorGUILayout.PropertyField(m_AutoDollyEnabled, m_AutoDollyEnabledGUIContent);
             if (m_AutoDollyEnabled.boolValue)
             {
-                EditorGUI.indentLevel++;
-                EditorGUILayout.PropertyField(m_AutoDollyPositionOffset, m_AutoDollyPositionOffsetGUIContent);
+                EditorGUI.indentLevel++; 
+                EditorGUILayout.PropertyField(m_AutoDollyPositionOffset, m_AutoDollyPositionOffsetGUIContent); 
                 EditorGUI.indentLevel--;
             }
-            
+
             EditorGUILayout.PropertyField(m_DampingEnabled, m_DampingEnabledGUIContent);
             if (m_DampingEnabled.boolValue)
-            {
-                EditorGUI.indentLevel++;
-                EditorGUILayout.PropertyField(m_Damping, m_DampingGUIContent);
-                EditorGUILayout.PropertyField(m_AngularDamping, m_AngularDampingGUIContent);
+            { 
+                EditorGUI.indentLevel++; 
+                EditorGUILayout.PropertyField(m_Damping, m_DampingGUIContent); 
+                EditorGUILayout.PropertyField(m_AngularDamping, m_AngularDampingGUIContent); 
                 EditorGUI.indentLevel--;
             }
             
