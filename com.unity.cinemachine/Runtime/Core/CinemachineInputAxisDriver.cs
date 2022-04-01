7532f8e49bc63e6638a33083cfa4956d16f4953e
Antoine Charton <antoine.charton@gmail.com>
2022-04-01 15:46:12 -0400

    CMCL-400: Add test for hdrp and urp (#408)
    * Add test for hdrp and urp
    
    - Add test projects for URP, HDRP and standelone
    - Add test package
    - Update Yamato file to support the new layout
    
    * Fix pack not finding manifest
    
    * Fix version null on tests
    
    * Fix path issue
    
    * Fix tests not finding editor version
    
    * Pack the tests package
    
    * Fix typo in pack command
    
    * Removed test package
    
    * Remove test package from project manifest
    
    * Fixed invalid arguments
    
    * Fix project tests arguments
    
    * Request a GPU machine for tests
    
    * Fix path issue with package in project manifests
    
    * Remove GPU requests
    
    * Readded package test
    
    * add project pack
    
    * Fix path issue
    
    * Fix dependancy
    
    * Revert changes
    
    * Fix project test invalid command
    
    * Fix corevage comman
    
    * Fix coverage not executing
    
    * Fix package test error
    
    * Remove package test
    
    * Revert project test removed
    
    * Remove unecessary test
    
    * Add standelone tests
    
    * Fix typo in projects path
    
    * Fix trigger
    
    * Add test projects list
    
    * Add project name in the test name
    
    * Add path to test project
    
    * Add test arguments
    
    * Downgrade HDRP version
    
    * Remove unity 2019 from tested editors
    
    * Add hdrp projects
    
    * Update meta file
    
    * Enable hdrp testing
    
    * Simplify tests names
    
    * Add windows tests to ci configuration
    
    * Update promotion, publish and test coverage
    
    * Fix incorect hdrp version
    
    * Update HDRP project to make it compatible with 2020
    
    * Revert "Update HDRP project to make it compatible with 2020"
    
    This reverts commit e4adce70043d552137f0abc674974234f6e7edb2. Commit fixed HDRP issues on 2020 but broke the other versions
    
    * Ignore blending test on HDRP unity 2020
    
    * Fix naming typos and unecessary tests
    
    Fix naming issues from Standelone to Standalone
    Remove unecessary test for vetting and promotion tests
    
    * Test promotion tests
    
    * Remove srps project from isolation tests
    
    * Add pacakge args to the config
    
    * Fix test running on projects instead of package
    
    - Add project test
    - Edit package test to avoid project test
    
    * Fix pacakge test running multiple time
    
    * add pack to package pack
    
    * Add package artifacts
    
    * Revert pacakage tests
    
    * Add project pack job
    
    * Add project pack dependancy on projects tests
    
    * Add project pack
    
    * Fix coverage job dependancy
    
    * Remove dependancy of pack for package CI
    
    Add package test
    
    * Fix missing dependancy
    
    * Fix duplicates job names
    
    * Remove isolation tests from config
    
    * Reverted changes in packages and added project
    
    * Fix project pack
    
    * Fix package test path
    
    * Add multiple editor and versions
    
    * Fix path
    
    * Fix test projects typo
    
    * Remove unecessary 2019 tests
    
    * Remove integration test from package tests
    
    * Add projects test for windows and mac
    
    * Revert package ci
    
    * Add windows to package ci
    
    * Add macos to package ci
    
    * Remove mac os tests
    
    * Clean up project job names
    
    * Run macos tests on ci
    
    * Add 2019.4 to pacakge tests
    
    * Remove unecessary dependencies
    
    * Remove unecessary for loops
    
    * Fix 2019 standalone project issue
    
    * Remove 2019.4 for projects tests
    
    Remove 2019.4 tests for projects
    Add integration tests for 2019.4
    
    * Test promotion tests
    
    * Run on windows to test promotion tests
    
    * Fix isolation test running on projects
    
    * Fix typo in project test
    
    * Fix project test arguments
    
    * Fix type to array
    
    * Fix type command issue
    
    * Update URP project to 2019.4
    
    * Remove promotion tests
    
    * Bump version and changelog
    
    * Added date field to make tests pass
    
    * Add 2019.4 project and fix images
    
    * Add testable to manifest

---
+++ com.unity.cinemachine/Runtime/Core/CinemachineInputAxisDriver.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using UnityEngine;
+using Cinemachine.Utility;
+
+namespace Cinemachine
+{
+    /// <summary>
+    /// Alternative to AxisState - a simplified structure to hold the definition of an input axis
+    /// </summary>
+    [Serializable]
+    public struct AxisBase
+    {
+        /// <summary>The current value of the axis</summary>
+        [NoSaveDuringPlay]
+        [Tooltip("The current value of the axis.")]
+        public float m_Value;
+
+        /// <summary>The minimum value for the axis</summary>
+        [Tooltip("The minimum value for the axis")]
+        public float m_MinValue;
+
+        /// <summary>The maximum value for the axis</summary>
+        [Tooltip("The maximum value for the axis")]
+        public float m_MaxValue;
+
+        /// <summary>If checked, then the axis will wrap around at the min/max values, forming a loop</summary>
+        [Tooltip("If checked, then the axis will wrap around at the min/max values, forming a loop")]
+        public bool m_Wrap;
+
+        /// <summary>
+        /// Call this from OnValidate() to validate the fields of this structure (applies clamps, etc).
+        /// </summary>
+        public void Validate()
+        {
+            m_MaxValue = Mathf.Clamp(m_MaxValue, m_MinValue, m_MaxValue);
+        }
+    }
+
+    /// <summary>
+    /// A helper class to drive an input axis, as an alternative to the standard Cinemachine.AxisState.
+    /// Behaviour is a simple direct scaling of the input channel, with no max speed.
+    /// </summary>
+    [Serializable]
+    public struct CinemachineInputAxisDriver
+    {
+        /// <summary>Multiply the input by this amount prior to processing.  Controls the input power</summary>
+        [Tooltip("Multiply the input by this amount prior to processing.  Controls the input power.")]
+        public float multiplier;
+
+        /// <summary>The amount of time in seconds it takes to accelerate to a higher speed</summary>
+        [Tooltip("The amount of time in seconds it takes to accelerate to a higher speed")]
+        public float accelTime;
+
+        /// <summary>The amount of time in seconds it takes to decelerate to a lower speed</summary>
+        [Tooltip("The amount of time in seconds it takes to decelerate to a lower speed")]
+        public float decelTime;
+
+        /// <summary>The name of this axis as specified in Unity Input manager. 
+        /// Setting to an empty string will disable the automatic updating of this axis</summary>
+        [Tooltip("The name of this axis as specified in Unity Input manager. "
+            + "Setting to an empty string will disable the automatic updating of this axis")]
+        public string name;
+
+        /// <summary>The value of the input axis.  A value of 0 means no input.  You can drive 
+        /// "this directly from a custom input system, or you can set the Axis Name and 
+        /// have the value driven by the internal Input Manager</summary>
+        [NoSaveDuringPlay]
+        [Tooltip("The value of the input axis.  A value of 0 means no input.  You can drive "
+            + "this directly from a custom input system, or you can set the Axis Name and "
+            + "have the value driven by the internal Input Manager")]
+        public float inputValue;
+
+        /// Internal state
+        private float mCurrentSpeed;
+        const float Epsilon =  UnityVectorExtensions.Epsilon;
+
+        /// <summary>Call from OnValidate: Make sure the fields are sensible</summary>
+        public void Validate()
+        {
+            accelTime = Mathf.Max(0, accelTime);
+            decelTime = Mathf.Max(0, decelTime);
+        }
+
+        /// <summary>Update the axis</summary>
+        /// <param name="deltaTime">current deltaTime</param>
+        /// <param name="axis">The AxisState to update</param>
+        /// <returns>True if the axis value changed due to user input, false otherwise</returns>
+        public bool Update(float deltaTime, ref AxisBase axis)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                try { inputValue = CinemachineCore.GetInputAxis(name); }
+                catch (ArgumentException) {}
+                //catch (ArgumentException e) { Debug.LogError(e.ToString()); }
+            }
+
+            float input = inputValue * multiplier;
+            if (deltaTime < Epsilon)
+                mCurrentSpeed = 0;
+            else
+            {
+                float speed = input / deltaTime;
+                float dampTime = Mathf.Abs(speed) < Mathf.Abs(mCurrentSpeed) ? decelTime : accelTime;
+                speed = mCurrentSpeed + Damper.Damp(speed - mCurrentSpeed, dampTime, deltaTime);
+                mCurrentSpeed = speed;
+
+                // Decelerate to the end points of the range if not wrapping
+                float range = axis.m_MaxValue - axis.m_MinValue;
+                if (!axis.m_Wrap && decelTime > Epsilon && range > Epsilon)
+                {
+                    float v0 = ClampValue(ref axis, axis.m_Value);
+                    float v = ClampValue(ref axis, v0 + speed * deltaTime);
+                    float d = (speed > 0) ? axis.m_MaxValue - v : v - axis.m_MinValue;
+                    if (d < (0.1f * range) && Mathf.Abs(speed) > Epsilon)
+                        speed = Damper.Damp(v - v0, decelTime, deltaTime) / deltaTime;
+                }
+                input = speed * deltaTime;
+            }
+
+            axis.m_Value = ClampValue(ref axis, axis.m_Value + input);
+            return Mathf.Abs(inputValue) > Epsilon;
+        }
+
+
+        /// <summary>Support for legacy AxisState struct: update the axis</summary>
+        /// <param name="deltaTime">current deltaTime</param>
+        /// <param name="axis">The AxisState to update</param>
+        /// <returns>True if the axis value changed due to user input, false otherwise</returns>
+        public bool Update(float deltaTime, ref AxisState axis)
+        {
+            var a = new AxisBase
+            {
+                m_Value = axis.Value,
+                m_MinValue = axis.m_MinValue,
+                m_MaxValue = axis.m_MaxValue,
+                m_Wrap = axis.m_Wrap
+            };
+            bool changed = Update(deltaTime, ref a);
+            axis.Value = a.m_Value;
+            return changed;
+        }
+        
+        float ClampValue(ref AxisBase axis, float v)
+        {
+            float r = axis.m_MaxValue - axis.m_MinValue;
+            if (axis.m_Wrap && r > Epsilon)
+            {
+                v = (v - axis.m_MinValue) % r;
+                v += axis.m_MinValue + ((v < 0) ? r : 0);
+            }
+            return Mathf.Clamp(v, axis.m_MinValue, axis.m_MaxValue);
+        }
+    }
+}
