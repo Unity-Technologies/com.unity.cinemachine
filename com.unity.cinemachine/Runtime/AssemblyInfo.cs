b692a591002d94004a9ffa8b447016a3074ac2a4
Krisztian G. Birkas <57672095+gaborkb@users.noreply.github.com>
2022-04-19 13:38:26 -0400

    Update AssemblyInfo.cs

--- com.unity.cinemachine/Runtime/AssemblyInfo.cs
+++ com.unity.cinemachine/Runtime/AssemblyInfo.cs
@@ -1,3 +1,4 @@
-﻿using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("com.unity.cinemachine.editor")]
 [assembly: InternalsVisibleTo("Cinemachine.Tests")]
+[assembly: InternalsVisibleTo("Cinemachine.Editor.Tests")]
