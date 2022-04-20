ab9609d241161e97ad9005ddb0061853bb625c67
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-04-20 14:44:40 -0400

    Rename shared tests

--- com.unity.cinemachine/Runtime/AssemblyInfo.cs
+++ com.unity.cinemachine/Runtime/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("com.unity.cinemachine.editor")]
-[assembly: InternalsVisibleTo("Cinemachine.SharedTests")]
+[assembly: InternalsVisibleTo("Cinemachine.Shared.Tests")]
 [assembly: InternalsVisibleTo("Cinemachine.Editor.Tests")]
 [assembly: InternalsVisibleTo("Cinemachine.Runtime.Tests")]
