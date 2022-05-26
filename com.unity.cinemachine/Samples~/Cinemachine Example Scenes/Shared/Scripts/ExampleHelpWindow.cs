aa4fbf4c901f769dc38f6f5cff2d35402762c001
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-05-26 12:26:26 -0400

    ExampleHelpWindow change reverted

--- com.unity.cinemachine/Samples~/Cinemachine Example Scenes/Shared/Scripts/ExampleHelpWindow.cs
+++ com.unity.cinemachine/Samples~/Cinemachine Example Scenes/Shared/Scripts/ExampleHelpWindow.cs
@@ -2,40 +2,43 @@ using UnityEngine;
 
 namespace Cinemachine.Examples
 {
-    public class ExampleHelpWindow : MonoBehaviour
+
+[AddComponentMenu("")] // Don't display in add component menu
+public class ExampleHelpWindow : MonoBehaviour
+{
+    public string m_Title;
+    [TextArea(minLines: 10, maxLines: 50)]
+    public string m_Description;
+
+    private bool mShowingHelpWindow = true;
+    private const float kPadding = 40f;
+
+    private void OnGUI()
     {
-        public string title;
-        [TextArea(minLines: 10, maxLines: 50)]
-        public string description;
-
-        bool m_MShowingHelpWindow = true;
-        const float k_Padding = 40f;
-
-        void OnGUI()
+        if (mShowingHelpWindow)
         {
-            if (m_MShowingHelpWindow)
-            {
-                Vector2 size = GUI.skin.label.CalcSize(new GUIContent(description));
-                var halfSize = size * 0.5f;
+            Vector2 size = GUI.skin.label.CalcSize(new GUIContent(m_Description));
+            Vector2 halfSize = size * 0.5f;
 
-                float maxWidth = Mathf.Min(Screen.width - k_Padding, size.x);
-                float left = Screen.width * 0.5f - maxWidth * 0.5f;
-                float top = Screen.height * 0.4f - halfSize.y;
+            float maxWidth = Mathf.Min(Screen.width - kPadding, size.x);
+            float left = Screen.width * 0.5f - maxWidth * 0.5f;
+            float top = Screen.height * 0.4f - halfSize.y;
 
-                var windowRect = new Rect(left, top, maxWidth, size.y);
-                GUILayout.Window(400, windowRect, _ => DrawWindow(), title);
-            }
-        }
-
-        void DrawWindow()
-        {
-            GUILayout.BeginVertical(GUI.skin.box);
-            GUILayout.Label(description);
-            GUILayout.EndVertical();
-            if (GUILayout.Button("Got it!"))
-            {
-                m_MShowingHelpWindow = false;
-            }
+            Rect windowRect = new Rect(left, top, maxWidth, size.y);
+            GUILayout.Window(400, windowRect, (id) => DrawWindow(id, maxWidth), m_Title);
         }
     }
+
+    private void DrawWindow(int id, float maxWidth)
+    {
+        GUILayout.BeginVertical(GUI.skin.box);
+        GUILayout.Label(m_Description);
+        GUILayout.EndVertical();
+        if (GUILayout.Button("Got it!"))
+        {
+            mShowingHelpWindow = false;
+        }
+    }
+}
+
 }
