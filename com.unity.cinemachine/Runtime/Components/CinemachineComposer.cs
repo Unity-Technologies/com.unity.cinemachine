c450717357783f2d3332957daffc09c26aae0a2b
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-06-01 13:07:09 -0400

    CMCL-976: fix rounding error

--- com.unity.cinemachine/Runtime/Components/CinemachineComposer.cs
+++ com.unity.cinemachine/Runtime/Components/CinemachineComposer.cs
@@ -341,13 +341,6 @@ namespace Cinemachine
                 m_SoftZoneHeight = Mathf.Clamp(value.height, 0, 2f);
                 m_DeadZoneWidth = Mathf.Min(m_DeadZoneWidth, m_SoftZoneWidth);
                 m_DeadZoneHeight = Mathf.Min(m_DeadZoneHeight, m_SoftZoneHeight);
-
-                var center = new Vector2((float)Math.Round(value.center.x, 6), (float)Math.Round(value.center.y, 6));
-                Vector2 bias = center - new Vector2(m_ScreenX, m_ScreenY);
-                float biasWidth = Mathf.Max(0, m_SoftZoneWidth - m_DeadZoneWidth);
-                float biasHeight = Mathf.Max(0, m_SoftZoneHeight - m_DeadZoneHeight);
-                m_BiasX = biasWidth < Epsilon ? 0 : Mathf.Clamp(bias.x / biasWidth, -0.5f, 0.5f);
-                m_BiasY = biasHeight < Epsilon ? 0 : Mathf.Clamp(bias.y / biasHeight, -0.5f, 0.5f);
             }
         }
 
