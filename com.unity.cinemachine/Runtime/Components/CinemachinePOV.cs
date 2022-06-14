b3a490d546ef4b68ad68bb68f10c1b3a25ca5d72
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-06-14 15:01:06 -0400

    rotation fix

--- com.unity.cinemachine/Runtime/Components/CinemachinePOV.cs
+++ com.unity.cinemachine/Runtime/Components/CinemachinePOV.cs
@@ -138,7 +138,7 @@ namespace Cinemachine
                 rot = parent.rotation * rot;
                 up = parent.up;
             }
-            rot = Quaternion.FromToRotation(up, curState.ReferenceUp) * rot;
+            rot = Quaternion.FromToRotation(curState.ReferenceUp, up) * rot;
             curState.RawOrientation = rot;
         }
 
