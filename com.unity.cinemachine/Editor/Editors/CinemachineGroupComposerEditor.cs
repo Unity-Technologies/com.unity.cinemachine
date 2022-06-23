941ab43bb2db528124870e203a4a840f68d98798
Greg Labute <gregoryl@unity3d.com>
2022-06-23 19:23:58 -0400

    cosmetic

--- com.unity.cinemachine/Editor/Editors/CinemachineGroupComposerEditor.cs
+++ com.unity.cinemachine/Editor/Editors/CinemachineGroupComposerEditor.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
 using UnityEditor;
 using UnityEngine;
-using Cinemachine.Utility;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 
