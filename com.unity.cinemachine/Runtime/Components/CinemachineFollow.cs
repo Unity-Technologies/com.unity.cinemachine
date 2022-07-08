ded0fdc9dd14774aaa165b909a44fd7b3cfcdfb8
Greg Labute <gregoryl@unity3d.com>
2022-07-08 14:48:14 -0400

    Add CinemachineFollow

---
+++ com.unity.cinemachine/Runtime/Components/CinemachineFollow.cs
@@ -0,0 +1,135 @@
+using UnityEngine;
+using Cinemachine.TargetTracking;
+
+namespace Cinemachine
+{
+    /// <summary>
+    /// This is a CinemachineComponent in the Body section of the component pipeline.
+    /// Its job is to position the camera in a fixed relationship to the vcam's Tracking
+    /// Target object, with offsets and damping.
+    ///
+    /// This component will only change the camera's position in space.  It will not
+    /// re-orient or otherwise aim the camera.  To to that, you need to instruct
+    /// the camera in the Aim section of its pipeline.
+    /// </summary>
+    [AddComponentMenu("")] // Don't display in add component menu
+    [SaveDuringPlay]
+    [CameraPipeline(CinemachineCore.Stage.Body)]
+    public class CinemachineFollow : CinemachineComponentBase
+    {
+        /// <summary>The distance which the camera will attempt to maintain from the tracking target</summary>
+        [Tooltip("The distance vector that the camera will attempt to maintain from the tracking target")]
+        public Vector3 FollowOffset = Vector3.back * 10f;
+
+        /// <summary>Settings to control damping for target tracking.</summary>
+        public TrackerSettings TrackerSettings = TrackerSettings.Default;
+
+        Tracker m_TargetTracker;
+
+        /// <summary>Derived classes should call this from their OnValidate() implementation</summary>
+        void OnValidate()
+        {
+            FollowOffset = EffectiveOffset;
+            TrackerSettings.Validate();
+        }
+
+        private void Reset()
+        {
+            FollowOffset = Vector3.back * 10f;
+            TrackerSettings = TrackerSettings.Default;
+        }
+
+        /// <summary>Get the target offset, with sanitization</summary>
+        internal Vector3 EffectiveOffset
+        {
+            get
+            {
+                Vector3 offset = FollowOffset;
+                if (TrackerSettings.BindingMode == BindingMode.SimpleFollowWithWorldUp)
+                {
+                    offset.x = 0;
+                    offset.z = -Mathf.Abs(offset.z);
+                }
+                return offset;
+            }
+        }
+
+        /// <summary>True if component is enabled and has a valid Follow target</summary>
+        public override bool IsValid => enabled && FollowTarget != null;
+
+        /// <summary>Get the Cinemachine Pipeline stage that this component implements.
+        /// Always returns the Body stage</summary>
+        public override CinemachineCore.Stage Stage { get { return CinemachineCore.Stage.Body; } }
+
+        /// <summary>
+        /// Report maximum damping time needed for this component.
+        /// </summary>
+        /// <returns>Highest damping setting in this component</returns>
+        public override float GetMaxDampTime() => TrackerSettings.GetMaxDampTime();
+
+        /// <summary>Positions the virtual camera according to the transposer rules.</summary>
+        /// <param name="curState">The current camera state</param>
+        /// <param name="deltaTime">Used for damping.  If less than 0, no damping is done.</param>
+        public override void MutateCameraState(ref CameraState curState, float deltaTime)
+        {
+            m_TargetTracker.InitStateInfo(
+                this, deltaTime, TrackerSettings.BindingMode, curState.ReferenceUp);
+            if (IsValid)
+            {
+                Vector3 offset = EffectiveOffset;
+                m_TargetTracker.TrackTarget(
+                    this, deltaTime, curState.ReferenceUp, offset, TrackerSettings,
+                    out Vector3 pos, out Quaternion orient);
+                offset = orient * offset;
+
+                // Respect minimum target distance on XZ plane
+                var targetPosition = FollowTargetPosition;
+                pos += m_TargetTracker.GetOffsetForMinimumTargetDistance(
+                    this, pos, offset, curState.RawOrientation * Vector3.forward,
+                    curState.ReferenceUp, targetPosition);
+                    
+                curState.RawPosition = pos + offset;
+                curState.ReferenceUp = orient * Vector3.up;
+            }
+        }
+
+        /// <summary>This is called to notify the us that a target got warped,
+        /// so that we can update its internal state to make the camera
+        /// also warp seamlessly.</summary>
+        /// <param name="target">The object that was warped</param>
+        /// <param name="positionDelta">The amount the target's position changed</param>
+        public override void OnTargetObjectWarped(Transform target, Vector3 positionDelta)
+        {
+            base.OnTargetObjectWarped(target, positionDelta);
+            if (target == FollowTarget)
+                m_TargetTracker.OnTargetObjectWarped(positionDelta);
+        }
+
+        /// <summary>
+        /// Force the virtual camera to assume a given position and orientation
+        /// </summary>
+        /// <param name="pos">Worldspace position to take</param>
+        /// <param name="rot">Worldspace orientation to take</param>
+        public override void ForceCameraPosition(Vector3 pos, Quaternion rot)
+        {
+            base.ForceCameraPosition(pos, rot);
+            m_TargetTracker.ForceCameraPosition(this, TrackerSettings.BindingMode, pos, rot, EffectiveOffset);
+        }
+        
+        internal Quaternion GetReferenceOrientation(Vector3 up)
+        {
+            return m_TargetTracker.GetReferenceOrientation(this, TrackerSettings.BindingMode, up);
+        }
+
+        /// <summary>Internal API for the Inspector Editor, so it can draw a marker at the target</summary>
+        /// <param name="worldUp">Current effective world up</param>
+        /// <returns>The position of the Follow target</returns>
+        internal Vector3 GetDesiredCameraPosition(Vector3 worldUp)
+        {
+            if (!IsValid)
+                return Vector3.zero;
+            return FollowTargetPosition + m_TargetTracker.GetReferenceOrientation(
+                this, TrackerSettings.BindingMode, worldUp) * EffectiveOffset;
+        }
+    }
+}
