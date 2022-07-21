a1657f002e22d42e03ca1425a070de619a448c10
Gregory Labute <gregoryl@unity3d.com>
2022-07-21 06:41:13 -0400

    refactor target group, cleaner API (#499)

---
+++ com.unity.cinemachine/Editor/PropertyDrawers/GroupTargetPropertyDrawer.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+namespace Cinemachine.Editor
+{
+    [CustomPropertyDrawer(typeof(CinemachineTargetGroup.Target))]
+    internal sealed class GroupTargetPropertyDrawer : PropertyDrawer
+    {
+        CinemachineTargetGroup.Target def = new();
+
+        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            float floatFieldWidth = EditorGUIUtility.singleLineHeight * 3.5f;
+
+            EditorGUI.BeginProperty(rect, GUIContent.none, property);
+
+            float oldWidth = EditorGUIUtility.labelWidth;
+            EditorGUIUtility.labelWidth = 1;
+
+            rect.width -= 2 * (floatFieldWidth + EditorGUIUtility.singleLineHeight);
+            var p = property.FindPropertyRelative(() => def.Object);
+            EditorGUI.PropertyField(rect, p, new GUIContent(" ", p.tooltip));
+
+            EditorGUIUtility.labelWidth = EditorGUIUtility.singleLineHeight;
+            rect.x += rect.width; rect.width = floatFieldWidth + EditorGUIUtility.singleLineHeight;
+            p = property.FindPropertyRelative(() => def.Weight);
+            EditorGUI.PropertyField(rect, p, new GUIContent(" ", p.tooltip));
+
+            rect.x += rect.width;
+            p = property.FindPropertyRelative(() => def.Radius);
+            EditorGUI.PropertyField(rect, p, new GUIContent(" ", p.tooltip));
+
+            EditorGUIUtility.labelWidth = oldWidth;
+            EditorGUI.EndProperty();
+        }
+
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            float floatFieldWidth = EditorGUIUtility.singleLineHeight * 3.5f;
+            var ux = new VisualElement() { style = { flexDirection = FlexDirection.Row }};
+
+            ux.Add(new PropertyField(property.FindPropertyRelative(() => def.Object), string.Empty) 
+                { style = { flexGrow = 1, flexBasis = floatFieldWidth }});
+            ux.Add(new InspectorUtility.CompactPropertyField(
+                property.FindPropertyRelative(() => def.Radius), "R")
+                { style = { flexGrow = 0, flexBasis = floatFieldWidth, marginLeft = 5 }});
+            ux.Add(new InspectorUtility.CompactPropertyField(
+                property.FindPropertyRelative(() => def.Weight), "W")
+                { style = { flexGrow = 0, flexBasis = floatFieldWidth, marginLeft = 5 }});
+
+            return ux;
+        }
+    }
+}
--- com.unity.cinemachine/Samples~/Cinemachine Example Scenes/Scenes/CameraMagnets/CameraMagnetTargetController.cs
+++ com.unity.cinemachine/Samples~/Cinemachine Example Scenes/Scenes/CameraMagnets/CameraMagnetTargetController.cs
@@ -19,18 +19,18 @@ namespace Cinemachine.Examples
         // Update is called once per frame
         void Update()
         {
-            for (int i = 1; i < targetGroup.m_Targets.Length; ++i)
+            for (int i = 1; i < targetGroup.Targets.Count; ++i)
             {
-                float distance = (targetGroup.m_Targets[playerIndex].target.position -
-                    targetGroup.m_Targets[i].target.position).magnitude;
+                float distance = (targetGroup.Targets[playerIndex].Object.position -
+                    targetGroup.Targets[i].Object.position).magnitude;
                 if (distance < cameraMagnets[i - 1].Proximity)
                 {
-                    targetGroup.m_Targets[i].weight = cameraMagnets[i - 1].MagnetStrength *
+                    targetGroup.Targets[i].Weight = cameraMagnets[i - 1].MagnetStrength *
                         (1 - (distance / cameraMagnets[i - 1].Proximity));
                 }
                 else
                 {
-                    targetGroup.m_Targets[i].weight = 0;
+                    targetGroup.Targets[i].Weight = 0;
                 }
             }
         }
