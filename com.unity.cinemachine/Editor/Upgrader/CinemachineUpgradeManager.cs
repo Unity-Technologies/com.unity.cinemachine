e4f62e028cfc79252c23030ddb001a23a1d60a89
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-09-05 00:56:57 -0400

    manager camera references

--- com.unity.cinemachine/Editor/Upgrader/CinemachineUpgradeManager.cs
+++ com.unity.cinemachine/Editor/Upgrader/CinemachineUpgradeManager.cs
@@ -133,6 +133,16 @@ namespace Cinemachine.Editor
                     }
                     return oldValue;
                 });
+                
+                // manager cameras need to be recorded to keep references after saving scenes if they are part of a prefab
+                if (PrefabUtility.IsPartOfAnyPrefab(go))
+                {
+                    var managers = go.GetComponents<CinemachineCameraManagerBase>();
+                    foreach (var manager in managers)
+                    {
+                        PrefabUtility.RecordPrefabInstancePropertyModifications(manager);
+                    }
+                }
             }
         }
         
