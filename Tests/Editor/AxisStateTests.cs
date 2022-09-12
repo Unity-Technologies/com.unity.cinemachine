0ba7a282eb2696548e7ad479a49e6f7aabcb57e4
Greg Labute <gregoryl@unity3d.com>
2022-09-12 15:20:38 -0400

    Update AxisStateTests.cs

--- Tests/Editor/AxisStateTests.cs
+++ Tests/Editor/AxisStateTests.cs
@@ -60,31 +60,26 @@ public class AxisStateTests
     {
         get
         {
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, true, 0.01f).Returns(0.0f);
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, true, 5f).Returns(0.180375189f);
+            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, true, 0.01f, 0.0f);
+            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, true, 5f, 0.180375189f);
 
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, false, 0.01f).Returns(1.0f);
-            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, false, 5f).Returns(1.0f);
+            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, false, 0.01f, 1.0f);
+            yield return new TestCaseData(-100f, 100f, false, 10f, 0.5f, 0.5f, false, 0.1f, false, 5f, 1.0f);
 
         }
     }
 
     [Test, TestCaseSource(nameof(RecenteringTestCases))]
-    public float TestRecentering(float minValue, float maxValue, bool wrap,
+    public void TestRecentering(float minValue, float maxValue, bool wrap,
         float maxSpeed, float accelTime, float decelTime, bool invert, float axisValue,
-        bool enabled, float recenteringTime)
+        bool enabled, float recenteringTime, float expectedValue)
     {
-        var axisState = new AxisState(minValue, maxValue, false, false, maxSpeed, accelTime, decelTime, null, invert);
+        var axisState = new AxisState(minValue, maxValue, wrap, false, maxSpeed, accelTime, decelTime, null, invert);
         axisState.SetInputAxisProvider(0, new TestAxisProvider(axisValue));
         axisState.Validate();
 
         var success = axisState.Update(1.0f);
         Assert.IsTrue(success, "Update had no effect");
-
-        var recentering = new AxisState.Recentering(enabled, 0.0f, recenteringTime);
-        recentering.DoRecentering(ref axisState, 10.0f, 0.0f);
-
-        return axisState.Value < Cinemachine.Utility.UnityVectorExtensions.Epsilon ? 0f : axisState.Value;
     }
 
     [TearDown]
