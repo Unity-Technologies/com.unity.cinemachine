317c06d756a22187c348524673a11e4578b97bd5
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-09-20 12:32:30 -0400

    IconUtility added

---
+++ com.unity.cinemachine/Editor/Utility/IconUtility.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Cinemachine.Editor
+{
+    /// <summary>
+    /// Useful class for setting script icons in one go, instead of setting them by hand one by one
+    /// </summary>
+    static class IconUtility
+    {
+        static Dictionary<string, Texture2D> s_IconCache = new();
+        static Texture2D LoadAssetAtPathCached(string path)
+        {
+            if (!s_IconCache.ContainsKey(path)) 
+                s_IconCache.Add(path, AssetDatabase.LoadAssetAtPath<Texture2D>(path));
+            return s_IconCache[path];
+        }
+
+        /// <summary>Checks if CmCamera script has the correct icon or not.</summary>
+        /// <returns>True, when icons don't match. False, otherwise.</returns>
+        public static bool DoIconsNeedToBeUpdated()
+        {
+            var cmCameraPath = ScriptableObjectUtility.CinemachineRelativeInstallPath + 
+                "/Runtime/Behaviours/CmCamera.cs";
+            var monoImporter = AssetImporter.GetAtPath(cmCameraPath) as MonoImporter;
+            if (monoImporter == null)
+                return false;
+
+            var iconPath = GetIconPathForScript(monoImporter.GetScript());
+            if (iconPath != string.Empty)
+            {
+                var icon = LoadAssetAtPathCached(iconPath);
+                var scriptIcon = monoImporter.GetIcon();
+                return icon != scriptIcon;
+            }
+
+            return false;
+        }
+
+        /// <summary>Updates all icons of cinemachine runtime scripts according to the current theme.</summary>
+        public static void UpdateIcons()
+        {
+            var cmScriptPaths = GetAllCinemachineRuntimeScripts();
+            foreach (var cmScriptPath in cmScriptPaths)
+            {
+                var monoImporter = AssetImporter.GetAtPath(cmScriptPath) as MonoImporter;
+                if (monoImporter == null)
+                    continue;
+
+                var iconPath = GetIconPathForScript(monoImporter.GetScript());
+                if (iconPath != string.Empty)
+                {
+                    var icon = LoadAssetAtPathCached(iconPath);
+                    monoImporter.SetIcon(icon);
+                    monoImporter.SaveAndReimport();
+                }
+            }
+            
+            // local function
+            static List<string> GetAllCinemachineRuntimeScripts()
+            {
+                var cmRuntimeScripts = new List<string>();
+                var directories = Directory.GetDirectories(ScriptableObjectUtility.CinemachineRelativeInstallPath + "/Runtime");
+                foreach (var directory in directories)
+                    cmRuntimeScripts.AddRange(Directory.GetFiles(directory, "*.cs"));
+                
+                return cmRuntimeScripts;
+            }
+        }
+        
+        static string GetIconPathForScript(MonoScript monoScript)
+        {
+            var scriptClass = monoScript.GetClass();
+            if (scriptClass == null)
+                return string.Empty;
+
+            if (scriptClass.IsSubclassOf(typeof(CinemachineExtension)))
+                return ScriptableObjectUtility.CinemachineRelativeInstallPath +
+                    "/Editor/EditorResources/Icons/CmExtensions/CinemachineExtensions@256.png";
+            if (scriptClass.IsSubclassOf(typeof(CinemachineComponentBase)))
+                return ScriptableObjectUtility.CinemachineRelativeInstallPath +
+                    "/Editor/EditorResources/Icons/CmComponent/CMComponent@256.png";
+
+            if (scriptClass == typeof(CinemachineSplineRoll) || scriptClass == typeof(CinemachineSplineCart))
+                return ScriptableObjectUtility.CinemachineRelativeInstallPath +
+                    "/Editor/EditorResources/Icons/SplineTrack/DollyTrack@256.png";
+
+            if (scriptClass.IsSubclassOf(typeof(CinemachineVirtualCameraBase)) || scriptClass == typeof(CinemachineBrain))
+                return ScriptableObjectUtility.CinemachineRelativeInstallPath +
+                    "/Editor/EditorResources/Icons/CmCamera/VirtualCamera@256.png";
+
+            return string.Empty;
+        }
+    }
+}
