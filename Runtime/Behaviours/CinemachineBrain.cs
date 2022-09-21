09865596b942e411e32dcb9a863401d3a61e1140
Gregory Labute <gregoryl@unity3d.com>
2022-09-21 14:07:27 -0400

    reset camera projection matrix every frame (#573)

--- Runtime/Behaviours/CinemachineBrain.cs
+++ Runtime/Behaviours/CinemachineBrain.cs
@@ -895,6 +895,7 @@ namespace Cinemachine
                 Camera cam = OutputCamera;
                 if (cam != null)
                 {
+                    cam.ResetProjectionMatrix();
                     cam.nearClipPlane = state.Lens.NearClipPlane;
                     cam.farClipPlane = state.Lens.FarClipPlane;
                     cam.orthographicSize = state.Lens.OrthographicSize;
