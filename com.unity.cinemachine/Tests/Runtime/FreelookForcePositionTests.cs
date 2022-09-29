d545d4495f7006b75fc6bd97aa6a140cb475c356
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-09-29 10:19:53 -0400

    Cleanup

--- com.unity.cinemachine/Tests/Runtime/FreelookForcePositionTests.cs
+++ com.unity.cinemachine/Tests/Runtime/FreelookForcePositionTests.cs
@@ -16,10 +16,9 @@ namespace Tests.Runtime
         GameObject m_FollowTargetGo;
         Vector3 m_OriginalPosition;
         Quaternion m_OriginalOrientation;
-        static readonly Vector2 k_AxisRange = new(-180f, 180f);
         static readonly Array k_BindingModes = Enum.GetValues(typeof(BindingMode));
 
-        [SetUp]
+        [OneTimeSetUp]
         public override void SetUp()
         {
             CreateGameObject("MainCamera", typeof(Camera), typeof(CinemachineBrain));
@@ -27,19 +26,7 @@ namespace Tests.Runtime
             var camGo = CreateGameObject("CM Freelook", typeof(CmCamera));
             m_CmCamera = camGo.GetComponent<CmCamera>();
             m_OrbitalFollow = camGo.AddComponent<CinemachineOrbitalFollow>();
-            m_OrbitalFollow.HorizontalAxis.Range = k_AxisRange;
-            m_OrbitalFollow.HorizontalAxis.Center = 0f;
-            m_OrbitalFollow.HorizontalAxis.Wrap = true;
-            m_OrbitalFollow.VerticalAxis.Range = k_AxisRange;
-            m_OrbitalFollow.VerticalAxis.Center = 0f;
-            m_OrbitalFollow.VerticalAxis.Wrap = false;
-            m_OrbitalFollow.RadialAxis.Range = new Vector2(1f, 5f);
-            m_OrbitalFollow.RadialAxis.Center = 1f;
-            m_OrbitalFollow.RadialAxis.Wrap = false;
-            m_OrbitalFollow.RadialAxis.Value = m_OrbitalFollow.RadialAxis.Center;
-            m_OrbitalFollow.TrackerSettings.PositionDamping = Vector3.zero;
-            m_OrbitalFollow.TrackerSettings.RotationDamping = Vector3.zero;
-            m_OrbitalFollow.TrackerSettings.QuaternionDamping = 0;
+            
             camGo.AddComponent<CinemachineHardLookAt>();
             
             m_FollowTargetGo = CreatePrimitive(PrimitiveType.Cube);
@@ -131,26 +118,22 @@ namespace Tests.Runtime
         [UnityTest, TestCaseSource(nameof(RigSetups))]
         public IEnumerator Test_Freelook_ForcePosition_AllBindings(TestData rigSetup)
         {
+            
             m_OrbitalFollow.OrbitStyle = rigSetup.OrbitStyle;
             var floatEqualityComparer = new FloatEqualityComparer(rigSetup.Precision);
-            switch (rigSetup.OrbitStyle)
-            {
-                case CinemachineOrbitalFollow.OrbitStyles.Sphere:
-                    m_OrbitalFollow.Radius = rigSetup.Radius;
-                    break;
-                case CinemachineOrbitalFollow.OrbitStyles.ThreeRing:
-                    m_OrbitalFollow.Orbits = rigSetup.Orbits;
-                    m_OrbitalFollow.Orbits.SplineCurvature = 1f;
-                    break;
-            }
+            var axisRange = Setup(rigSetup.OrbitStyle);
             
             const float step = 5f; // so tests are not too long
             foreach (BindingMode bindingMode in k_BindingModes)
             {
+                if (bindingMode == BindingMode.SimpleFollowWithWorldUp)
+                    continue; // this mode has 0 horizontal axes, so we don't test it
+                
                 m_OrbitalFollow.TrackerSettings.BindingMode = bindingMode;
                 yield return null;
                 
-                for (var axisValue = k_AxisRange.x + 1; axisValue < k_AxisRange.y; axisValue += step)
+                // we don't test the range ends because the axis values can flip there (identical result, but not the same)
+                for (var axisValue = axisRange.x + 1; axisValue < axisRange.y; axisValue += step)
                 {
                     // Set Axis values
                     m_OrbitalFollow.HorizontalAxis.Value = axisValue;
@@ -166,10 +149,63 @@ namespace Tests.Runtime
                     m_CmCamera.ForceCameraPosition(m_OriginalPosition, m_OriginalOrientation);
                     yield return null;
                     
-                    // ignore SimpleFollowWithWorldUp because axis value is 0 always in this case
-                    if (m_OrbitalFollow.TrackerSettings.BindingMode != BindingMode.SimpleFollowWithWorldUp)
-                        Assert.That(m_OrbitalFollow.HorizontalAxis.Value, Is.EqualTo(axisValue).Using(floatEqualityComparer));
+                    Assert.That(m_OrbitalFollow.HorizontalAxis.Value, Is.EqualTo(axisValue).Using(floatEqualityComparer));
                     Assert.That(m_OrbitalFollow.VerticalAxis.Value, Is.EqualTo(axisValue).Using(floatEqualityComparer));
+                }
+            }
+            
+            // local functions
+            Vector2 Setup(CinemachineOrbitalFollow.OrbitStyles style)
+            {
+                switch (style)
+                {
+                    case CinemachineOrbitalFollow.OrbitStyles.Sphere:
+                        m_OrbitalFollow.Radius = rigSetup.Radius;
+                        return SetupSphereAxes();
+                    case CinemachineOrbitalFollow.OrbitStyles.ThreeRing:
+                    default:
+                        m_OrbitalFollow.Orbits = rigSetup.Orbits;
+                        m_OrbitalFollow.Orbits.SplineCurvature = 1f;
+                        return Setup3RigAxes();
+                }
+                
+                // local functions
+                Vector2 SetupSphereAxes()
+                {
+                    // testing only one quater of the hemisphere, to avoid cases where axis values are not the same, but are identical -> difficult to test
+                    var range = new Vector2(0, 90f);
+                    m_OrbitalFollow.HorizontalAxis.Range = range;
+                    m_OrbitalFollow.HorizontalAxis.Center = 0f;
+                    m_OrbitalFollow.HorizontalAxis.Wrap = true;
+                    m_OrbitalFollow.VerticalAxis.Range = range;
+                    m_OrbitalFollow.VerticalAxis.Center = 0f;
+                    m_OrbitalFollow.VerticalAxis.Wrap = false;
+                    m_OrbitalFollow.RadialAxis.Range = new Vector2(1f, 5f);
+                    m_OrbitalFollow.RadialAxis.Center = 1f;
+                    m_OrbitalFollow.RadialAxis.Wrap = false;
+                    m_OrbitalFollow.RadialAxis.Value = m_OrbitalFollow.RadialAxis.Center;
+                    m_OrbitalFollow.TrackerSettings.PositionDamping = Vector3.zero;
+                    m_OrbitalFollow.TrackerSettings.RotationDamping = Vector3.zero;
+                    m_OrbitalFollow.TrackerSettings.QuaternionDamping = 0;
+                    return range;
+                }
+                Vector2 Setup3RigAxes()
+                {
+                    var range = new Vector2(-180f, 180f);
+                    m_OrbitalFollow.HorizontalAxis.Range = range;
+                    m_OrbitalFollow.HorizontalAxis.Center = 0f;
+                    m_OrbitalFollow.HorizontalAxis.Wrap = true;
+                    m_OrbitalFollow.VerticalAxis.Range = range;
+                    m_OrbitalFollow.VerticalAxis.Center = 0f;
+                    m_OrbitalFollow.VerticalAxis.Wrap = false;
+                    m_OrbitalFollow.RadialAxis.Range = new Vector2(1f, 5f);
+                    m_OrbitalFollow.RadialAxis.Center = 1f;
+                    m_OrbitalFollow.RadialAxis.Wrap = false;
+                    m_OrbitalFollow.RadialAxis.Value = m_OrbitalFollow.RadialAxis.Center;
+                    m_OrbitalFollow.TrackerSettings.PositionDamping = Vector3.zero;
+                    m_OrbitalFollow.TrackerSettings.RotationDamping = Vector3.zero;
+                    m_OrbitalFollow.TrackerSettings.QuaternionDamping = 0;
+                    return range;
                 }
             }
         }
