c7955eaf013c8a87d09e587fbef773cc52aeed6f
Krisztian G. Birkas <57672095+gaborkb@users.noreply.github.com>
2022-10-12 12:46:43 -0400

    CMCL-0000: 3rdPersonShooter fixes [CinemachineTestingDojo] (#599)
    * Decoupled reticle from 3rdPersonAim -> this will cause issues to people who upgrade. Could handle case by converter probably.
    
    * 3rdPersonAim enabled check
    
    * Update PositionAimTargetAndReticle.cs
    
    * Prefab transform set to 0,0,0, and instance too
    
    Co-authored-by: Greg Labute <gregoryl@unity3d.com>

---
+++ com.unity.cinemachine/Samples~/Sample Scenes/Shared/Scripts/PositionAimTargetAndReticle.cs
@@ -0,0 +1,49 @@
+using Cinemachine;
+using UnityEngine;
+
+public class PositionAimTargetAndReticle : MonoBehaviour
+{
+    [Tooltip("This canvas will be enabled when there is a 3rdPersoAim camera active")]
+    public Canvas ReticleCanvas;
+
+    [Tooltip("If non-null, this target will pe positioned on the screen over the actual aim target")]
+    public RectTransform AimTargetIndicator;
+
+    // We add a CameraUpdatedEvent listener so that we are guaranteed to update after the
+    // Brain has positioned the camera
+    void OnEnable()
+    {
+        CinemachineCore.CameraUpdatedEvent.AddListener(SetAimTarget);
+    }
+
+    void OnDisable()
+    {
+        CinemachineCore.CameraUpdatedEvent.RemoveListener(SetAimTarget);
+    }
+
+    void SetAimTarget(CinemachineBrain brain)
+    {
+        var enableReticle = false;
+        if (brain == null || brain.OutputCamera == null)
+            CinemachineCore.CameraUpdatedEvent.RemoveListener(SetAimTarget);
+        else
+        {
+            var liveCam = brain.ActiveVirtualCamera as CmCamera;
+            if (liveCam != null)
+            {
+                if (liveCam.TryGetComponent<Cinemachine3rdPersonAim>(out var aim) && aim.enabled)
+                {
+                    // Set the worldspace aim target position so that we can know what gets hit
+                    enableReticle = true;
+                    transform.position = aim.AimTarget;
+
+                    // Set the screen-space hit target indicator position
+                    if (AimTargetIndicator != null)
+                        AimTargetIndicator.position = brain.OutputCamera.WorldToScreenPoint(transform.position);
+                }
+            }
+        }
+        if (ReticleCanvas != null)
+            ReticleCanvas.enabled = enableReticle;
+    }
+}
