d2971065f52378542c46476b11ef7efd1971edcc
Greg Labute <gregoryl@unity3d.com>
2022-10-21 15:00:03 -0400

    UITK implementation of AutoDolly PropertyDrawer

--- com.unity.cinemachine/Editor/PropertyDrawers/SplineAutoDollyPropertyDrawer.cs
+++ com.unity.cinemachine/Editor/PropertyDrawers/SplineAutoDollyPropertyDrawer.cs
@@ -2,50 +2,71 @@ using System;
 using System.Collections.Generic;
 using Cinemachine.Utility;
 using UnityEditor;
-using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
 
 namespace Cinemachine.Editor
 {
     [CustomPropertyDrawer(typeof(AutoDollySelectorAttribute))]
     class SplineAutoDollyPropertyDrawer : PropertyDrawer
     {
-        readonly float vSpace = EditorGUIUtility.standardVerticalSpacing;
-
-        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
-            var value = property.managedReferenceValue;
-            Type type = value == null ? null : value.GetType();
+            var ux = new VisualElement();
 
-            EditorGUI.BeginProperty(rect, label, property);
-            var r = rect; r.height = EditorGUIUtility.singleLineHeight;
-            r = EditorGUI.PrefixLabel(r, label);
-            int selection = EditorGUI.Popup(r, AutoDollyMenuItems.GetTypeIndex(type), AutoDollyMenuItems.s_ItemNames);
-            if (selection >= 0)
+            var value = property.managedReferenceValue;
+            var index = AutoDollyMenuItems.GetTypeIndex(value == null ? null : value.GetType());
+            var dropdown = ux.AddChild(new DropdownField
             {
-                Type newType = AutoDollyMenuItems.s_AllItems[selection];
-                if (type != newType)
+                label = property.name,
+                tooltip = property.tooltip,
+                choices = AutoDollyMenuItems.s_ItemNames,
+                index = index,
+                style = { flexGrow = 1 }
+            });
+            dropdown.AddToClassList(InspectorUtility.kAlignFieldClass);
+            dropdown.RegisterValueChangedCallback((evt) => 
+            {
+                var value = property.managedReferenceValue;
+                var oldIndex = AutoDollyMenuItems.GetTypeIndex(value == null ? null : value.GetType());
+                index = AutoDollyMenuItems.GetTypeIndex(evt.newValue);
+                if (oldIndex != index)
                 {
-                    property.managedReferenceValue = (newType == null) ? null : Activator.CreateInstance(newType);
+                    property.managedReferenceValue = (index == 0) 
+                        ? null : Activator.CreateInstance(AutoDollyMenuItems.s_AllItems[index]);
                     property.serializedObject.ApplyModifiedProperties();
-                    type = newType;
+                    UpdateChildren();
+                }
+            });
+            UpdateChildren();
+
+            void UpdateChildren()
+            {
+                // First delete the existing element
+                const string kElementName = "ActiveAutoDollyContainer";
+                var old = ux.Q(kElementName);
+                if (old != null)
+                    old.RemoveFromHierarchy();
+
+                // Create a new one
+                var type = AutoDollyMenuItems.s_AllItems[index];
+                if (type != null)
+                {
+                    // GML todo: fix indenting hack
+                    var children = ux.AddChild(new VisualElement() 
+                        { name = kElementName, style = { marginLeft = InspectorUtility.SingleLineHeight } });
+                    var childProperty = property.Copy();
+                    var endProperty = childProperty.GetEndProperty();
+                    childProperty.NextVisible(true);
+                    while (!SerializedProperty.EqualContents(childProperty, endProperty))
+                    {
+                        children.Add(new PropertyField(childProperty));
+                        childProperty.NextVisible(false);
+                    }
                 }
             }
-            if (type != null)
-            {
-                rect.y += r.height + vSpace; rect.height -= r.height - vSpace;
-                ++EditorGUI.indentLevel;
-                InspectorUtility.DrawChildProperties(rect, property);
-                --EditorGUI.indentLevel;
-            }
-            EditorGUI.EndProperty();
-        }
 
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-        {
-            var height = EditorGUIUtility.singleLineHeight;
-            if (property.managedReferenceValue != null)
-                height += InspectorUtility.PropertyHeightOfChidren(property) + vSpace;
-            return height;
+            return ux;
         }
 
         [InitializeOnLoad]
@@ -53,15 +74,22 @@ namespace Cinemachine.Editor
         {
             public static int GetTypeIndex(Type type)
             {
-                for (int j = 0; j < s_AllItems.Count; ++j)
-                    if (s_AllItems[j] == type)
-                        return j;
-                return -1;
+                for (int i = 0; i < s_AllItems.Count; ++i)
+                    if (s_AllItems[i] == type)
+                        return i;
+                return 0;
+            }
+            public static int GetTypeIndex(string typeName)
+            {
+                for (int i = 0; i < s_ItemNames.Count; ++i)
+                    if (s_ItemNames[i] == typeName)
+                        return i;
+                return 0;
             }
         
             // These lists are synchronized
-            public static List<Type> s_AllItems = new List<Type>();
-            public static GUIContent[] s_ItemNames = Array.Empty<GUIContent>();
+            public static List<Type> s_AllItems = new ();
+            public static List<string> s_ItemNames = new ();
 
             // This code dynamically discovers eligible classes and builds the menu data 
             static AutoDollyMenuItems()
@@ -75,10 +103,10 @@ namespace Cinemachine.Editor
                 s_AllItems.Add(null);
                 s_AllItems.AddRange(allTypes);
 
-                s_ItemNames = new GUIContent[s_AllItems.Count];
-                s_ItemNames[0] = new GUIContent("None");
+                s_ItemNames.Clear();
+                s_ItemNames.Add("None");
                 for (int i = 1; i < s_AllItems.Count; ++i)
-                    s_ItemNames[i] = new GUIContent(InspectorUtility.NicifyClassName(s_AllItems[i]));
+                    s_ItemNames.Add(InspectorUtility.NicifyClassName(s_AllItems[i]));
             }
         }
     }
