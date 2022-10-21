9d50c4ab81f088bd954662307c7c4f30368a3706
Greg Labute <gregoryl@unity3d.com>
2022-10-21 07:44:11 -0400

    Update FoldoutWithEnabledButtonPropertyDrawer.cs

--- com.unity.cinemachine/Editor/PropertyDrawers/FoldoutWithEnabledButtonPropertyDrawer.cs
+++ com.unity.cinemachine/Editor/PropertyDrawers/FoldoutWithEnabledButtonPropertyDrawer.cs
@@ -27,29 +27,37 @@ namespace Cinemachine.Editor
             if (enabledProp == null)
                 return new PropertyField(property);
 
-            var ux = new VisualElement();
-            ux.Add(new PropertyField(enabledProp, property.displayName) { tooltip = property.tooltip });
-            // GML todo: fix the indenting
-            var children = ux.AddChild(new VisualElement() { style = { marginLeft = InspectorUtility.SingleLineHeight }});
+            // Don't set the text of the Foldout as this will set the Toggle.text,
+            // which is on the right side of the checkbox
+            var foldout = new Foldout(); // { style = { marginTop = 2 }}; // GML this hack would compensate for the current uneven line spacing
+            var foldoutToggle = foldout.Q<Toggle>(className: "unity-foldout__toggle");
 
+            // This is to counter the auto-adding of the "unity-foldout__toggle--inspector" class that
+            // adds -12px margin-left. Not ideal. I raised this as an issue.
+            foldoutToggle.style.marginLeft = 3;
+
+            // This does the magic nested alignment if this Foldout is inside another Foldout
+            foldoutToggle.AddToClassList(Toggle.alignedFieldUssClassName);
+
+            // Change from arrow to checkbox
+            foldoutToggle.RemoveFromClassList("unity-foldout__toggle");
+
+            // Bind toggle to the enabled property while displaying the main property text
+            foldoutToggle.label = property.displayName;
+            foldoutToggle.tooltip = property.tooltip;
+            foldoutToggle.BindProperty(enabledProp);
+
+            // Add children
             var childProperty = property.Copy();
             var endProperty = childProperty.GetEndProperty();
             childProperty.NextVisible(true);
             while (!SerializedProperty.EqualContents(childProperty, endProperty))
             {
                 if (!SerializedProperty.EqualContents(childProperty, enabledProp))
-                    children.Add(new PropertyField(childProperty));
+                    foldout.Add(new PropertyField(childProperty));
                 childProperty.NextVisible(false);
             }
-
-            TrackEnabled(enabledProp);
-            ux.TrackPropertyValue(enabledProp, TrackEnabled);
-
-            void TrackEnabled(SerializedProperty p)
-            {
-                children.SetVisible(p.boolValue);
-            }
-            return ux;
+            return foldout;
         }
     }
 }
