7cfbe1d372ae3afb5458ada5728f3b077fa09932
Gregory Labute <gregoryl@unity3d.com>
2022-11-08 09:35:42 -0500

    Bugfix: could not set main camera to physical mode while a vcam with ModeOverride = None was active (#652)

--- com.unity.cinemachine/Runtime/Behaviours/CinemachineBrain.cs
+++ com.unity.cinemachine/Runtime/Behaviours/CinemachineBrain.cs
@@ -971,7 +971,6 @@ namespace Cinemachine
                 Camera cam = OutputCamera;
                 if (cam != null)
                 {
-                    cam.ResetProjectionMatrix();
                     cam.nearClipPlane = state.Lens.NearClipPlane;
                     cam.farClipPlane = state.Lens.FarClipPlane;
                     cam.orthographicSize = state.Lens.OrthographicSize;
