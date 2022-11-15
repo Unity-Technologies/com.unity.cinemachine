9f7aee49ccdfb0d2a92c19e8a305bba0b0aa1eac
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-11-15 13:26:57 -0500

    2D Samples added

---
+++ com.unity.cinemachine/Samples~/2D Samples/Scripts/CameraMagnetTargetController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Cinemachine.Examples
+{
+    public class CameraMagnetTargetController : MonoBehaviour
+    {
+        public CinemachineTargetGroup TargetGroup;
+
+        int m_PlayerIndex;
+        CameraMagnetProperty[] m_CameraMagnets;
+
+        void Start()
+        {
+            m_CameraMagnets = GetComponentsInChildren<CameraMagnetProperty>();
+            m_PlayerIndex = 0;
+        }
+
+        void Update()
+        {
+            for (int i = 1; i < TargetGroup.Targets.Count; ++i)
+            {
+                float distance = (TargetGroup.Targets[m_PlayerIndex].Object.position -
+                    TargetGroup.Targets[i].Object.position).magnitude;
+                if (distance < m_CameraMagnets[i - 1].Proximity)
+                {
+                    TargetGroup.Targets[i].Weight = m_CameraMagnets[i - 1].MagnetStrength *
+                        (1 - (distance / m_CameraMagnets[i - 1].Proximity));
+                }
+                else
+                {
+                    TargetGroup.Targets[i].Weight = 0;
+                }
+            }
+        }
+    }
+}
---
+++ com.unity.cinemachine/Samples~/2D Samples/Scripts/MouseScrollZoom2D.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+namespace Cinemachine.Examples
+{
+    [RequireComponent(typeof(CmCamera))]
+    [SaveDuringPlay] // Enable SaveDuringPlay for this class
+    public class MouseScrollZoom2D : MonoBehaviour
+    {
+        [Range(0, 10)]
+        public float ZoomMultiplier = 1f;
+        [Range(0, 100)]
+        public float MinZoom = 1f;
+        [Range(0, 100)]
+        public float MaxZoom = 50f;
+
+        CmCamera m_CmCamera;
+        float m_OriginalOrthoSize, m_OriginalFieldOfView;
+
+        void Awake()
+        {
+            m_CmCamera = GetComponent<CmCamera>();
+            m_OriginalOrthoSize = m_CmCamera.Lens.OrthographicSize;
+            m_OriginalFieldOfView = m_CmCamera.Lens.FieldOfView;
+
+#if UNITY_EDITOR
+            // This code shows how to play nicely with the CmCamera's SaveDuringPlay functionality
+            SaveDuringPlay.SaveDuringPlay.OnHotSave -= RestoreLens;
+            SaveDuringPlay.SaveDuringPlay.OnHotSave += RestoreLens;
+#endif
+        }
+
+#if UNITY_EDITOR
+        void OnDestroy()
+        {
+            SaveDuringPlay.SaveDuringPlay.OnHotSave -= RestoreLens;
+        }
+        
+        void RestoreLens()
+        {
+            m_CmCamera.Lens.OrthographicSize = m_OriginalOrthoSize;
+            m_CmCamera.Lens.FieldOfView = m_OriginalFieldOfView;
+        }
+#endif
+
+        void OnValidate()
+        {
+            MaxZoom = Mathf.Max(MinZoom, MaxZoom);
+        }
+
+        void Update()
+        {
+#if ENABLE_LEGACY_INPUT_MANAGER
+            var zoom = m_CmCamera.Lens.OrthographicSize + Input.mouseScrollDelta.y * ZoomMultiplier;
+            if (m_CmCamera.Lens.Orthographic)
+                m_CmCamera.Lens.OrthographicSize = Mathf.Clamp(zoom, MinZoom, MaxZoom);
+            else
+                m_CmCamera.Lens.FieldOfView = Mathf.Clamp(zoom, MinZoom, MaxZoom);
+#else
+            InputSystemHelper.EnableBackendsWarningMessage();
+#endif
+        }
+    }
+}
