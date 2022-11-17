7475ff5b3e1d45cfa14f0c4dd108c34027af4391
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-11-16 21:34:48 -0500

    DampingVector tests

--- com.unity.cinemachine/Tests/Editor/DampingTests.cs
+++ com.unity.cinemachine/Tests/Editor/DampingTests.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System.Collections;
+using Cinemachine.Utility;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools.Utils;
 
 namespace Tests.Editor
 {
@@ -10,29 +14,71 @@ namespace Tests.Editor
         {
             const float dampTime = 10f;
             const float initial = 100f;
-            float[] fixedFactor = new float[3] {0.79f, 0f, 1.07f};
-            for (int f = 0; f < fixedFactor.Length; ++f)
+
+            float t = 0;
+            float r = Damper.Damp(initial, dampTime, t);
+            Assert.That(r, Is.EqualTo(0f));
+            Assert.That(r, Is.LessThan(initial));
+            const int iterations = 10;
+            for (int i = 0; i < iterations; ++i)
             {
-                float t = 0;
-                float r = Cinemachine.Utility.Damper.Damp(initial, dampTime, t);
-                Assert.That(r, Is.EqualTo(0f));
-                Assert.That(r, Is.LessThan(initial));
-                const int iterations = 10;
-                for (int i = 0; i < iterations; ++i)
-                {
-                    t += dampTime / iterations;
-                    float fdt = fixedFactor[f] * t;
-                    string msg = $"i = {i}, t = {t}, fdt = {fdt}";
-                    if (i != iterations - 1)
-                        Assert.That(t, Is.LessThan(dampTime), msg);
-                    else
-                        t = dampTime;
-                    float r2 = Cinemachine.Utility.Damper.Damp(initial, dampTime, t);
-                    Assert.That(r, Is.LessThan(r2), msg);
-                    r = r2;
-                }
-                //Assert.AreEqual(initial * (1 - MathHelpers.kNegligibleResidual), r, "f = " + f);
+                t += dampTime / iterations;
+
+                if (i != iterations - 1)
+                    Assert.That(t, Is.LessThan(dampTime));
+                else
+                    t = dampTime;
+                float r2 = Damper.Damp(initial, dampTime, t);
+                Assert.That(r, Is.LessThan(r2));
+                r = r2;
             }
+        }
+
+        static IEnumerable AxisDriverAccelTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(Vector3.one);
+                yield return new TestCaseData(Vector3.forward);
+                yield return new TestCaseData(Vector3.back);
+                yield return new TestCaseData(Vector3.left);
+                yield return new TestCaseData(Vector3.right);
+                yield return new TestCaseData(Vector3.up);
+                yield return new TestCaseData(Vector3.down);
+            }
+        }
+
+        readonly Vector3EqualityComparer m_Vector3EqualityComparer = new(UnityVectorExtensions.Epsilon);
+        [Test, TestCaseSource(nameof(AxisDriverAccelTestCases))]
+        public void DampVector(Vector3 initial)
+        {
+            var previousDeltaMagnitude = -1f;
+            var previousDelta = Vector3.zero;
+            const float deltaTime = 0.1f;
+            const float dampTime = 1f;
+            int iterationCount;
+            for (iterationCount = 0; true; iterationCount++)
+            {
+                var delta =  Damper.Damp(initial, dampTime, deltaTime);
+                var deltaMagnitude = delta.magnitude;
+
+                if (deltaMagnitude < UnityVectorExtensions.Epsilon)
+                    break; // stop when delta is small enough
+                
+                if (previousDeltaMagnitude >= 0)
+                {
+                    Assert.That(delta.normalized, 
+                        Is.EqualTo(previousDelta.normalized).Using(m_Vector3EqualityComparer)); // monotonic 
+                    Assert.That(deltaMagnitude, Is.LessThan(previousDeltaMagnitude)); // strictly decreasing
+                }
+                
+                previousDeltaMagnitude = deltaMagnitude;
+                previousDelta = delta;
+                initial -= delta;
+            }
+
+            var realDampTime = iterationCount * deltaTime;
+            Debug.Log(realDampTime + "seconds instead of " + dampTime + " seconds.");
         }
     }
 }
