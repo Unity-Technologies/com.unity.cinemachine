30bb356595aada5289df7a5e1b5980f8da8363b5
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-11-25 15:58:59 -0500

    Compressed sample scenes. Fixed prefabs. TODO: once adjust confiner is added I add the zoom feature to the samples

--- com.unity.cinemachine/Samples~/2D Samples/Scripts/MouseScrollZoom2D.cs
+++ com.unity.cinemachine/Samples~/2D Samples/Scripts/MouseScrollZoom2D.cs
@@ -1,69 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Cinemachine.Examples
 {
-    [RequireComponent(typeof(CmCamera))]
-    [SaveDuringPlay] // Enable SaveDuringPlay for this class
-    public class MouseScrollZoom2D : MonoBehaviour
+    [ExecuteAlways]
+    [SaveDuringPlay]
+    public class MouseScrollZoom2D : CinemachineExtension, IInputAxisSource
     {
-        [Range(0, 10)]
-        public float ZoomMultiplier = 1f;
-        [Range(0, 100)]
-        public float MinZoom = 1f;
-        [Range(0, 100)]
-        public float MaxZoom = 50f;
-
-        CmCamera m_CmCamera;
-        float m_OriginalOrthoSize, m_OriginalFieldOfView;
-
-        void Awake()
+        /// <summary>Axis representing the current horizontal rotation.  Value is in degrees
+        /// and represents a rotation about the up vector</summary>
+        [Tooltip("Axis representing the current horizontal rotation.  Value is in degrees "
+            + "and represents a rotation about the Y axis.")]
+        [SerializeReference]
+        public InputAxis ZoomAxis = DefaultZoom;
+        
+        static InputAxis DefaultZoom => new () { Value = 0, Range = new Vector2(20, 60), Wrap = false, Center = 40 };
+        
+        
+        protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
         {
-            m_CmCamera = GetComponent<CmCamera>();
-            m_OriginalOrthoSize = m_CmCamera.Lens.OrthographicSize;
-            m_OriginalFieldOfView = m_CmCamera.Lens.FieldOfView;
-
-#if UNITY_EDITOR
-            // This code shows how to play nicely with the CmCamera's SaveDuringPlay functionality
-            SaveDuringPlay.SaveDuringPlay.OnHotSave -= RestoreLens;
-            SaveDuringPlay.SaveDuringPlay.OnHotSave += RestoreLens;
-#endif
-        }
-
-#if UNITY_EDITOR
-        void OnDestroy()
-        {
-            SaveDuringPlay.SaveDuringPlay.OnHotSave -= RestoreLens;
+            if (stage == CinemachineCore.Stage.Finalize)
+            {
+                //state.Lens
+            }
         }
         
-        void RestoreLens()
+        public void GetInputAxes(List<IInputAxisSource.AxisDescriptor> axes)
         {
-            m_CmCamera.Lens.OrthographicSize = m_OriginalOrthoSize;
-            m_CmCamera.Lens.FieldOfView = m_OriginalFieldOfView;
-        }
-#endif
-
-        void OnValidate()
-        {
-            MaxZoom = Mathf.Max(MinZoom, MaxZoom);
-        }
-
-        void Update()
-        {
-#if ENABLE_LEGACY_INPUT_MANAGER
-            if (m_CmCamera.Lens.Orthographic)
-            {
-                var zoom = m_CmCamera.Lens.OrthographicSize + Input.mouseScrollDelta.y * ZoomMultiplier;
-                m_CmCamera.Lens.OrthographicSize = Mathf.Clamp(zoom, MinZoom, MaxZoom);
-            }
-            else
-            {
-                var zoom = m_CmCamera.Lens.FieldOfView + Input.mouseScrollDelta.y * ZoomMultiplier;
-                m_CmCamera.Lens.FieldOfView = Mathf.Clamp(zoom, MinZoom, MaxZoom);
-            }
-#else
-            InputSystemHelper.EnableBackendsWarningMessage();
-#endif
+            axes.Add(new IInputAxisSource.AxisDescriptor { Axis = ZoomAxis, Name = "Look X (Pan)", AxisIndex = 0 });
         }
     }
 }
