e3f9d0feff18e2e15d7c99b5bb402767592a5783
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-11-25 16:36:50 -0500

    Confiner invalidate not cache on lens chancge. MouseZoom extension (does not change fov in the eyes of cm) - may revert to prev so it is changed - will think about use for this too

--- com.unity.cinemachine/Samples~/2D Samples/Scripts/MouseScrollZoom2D.cs
+++ com.unity.cinemachine/Samples~/2D Samples/Scripts/MouseScrollZoom2D.cs
@@ -12,23 +12,34 @@ namespace Cinemachine.Examples
         /// and represents a rotation about the up vector</summary>
         [Tooltip("Axis representing the current horizontal rotation.  Value is in degrees "
             + "and represents a rotation about the Y axis.")]
-        [SerializeReference]
-        public InputAxis ZoomAxis = DefaultZoom;
+        public InputAxis ZoomAxis = DefaultZoomAxis;
         
-        static InputAxis DefaultZoom => new () { Value = 0, Range = new Vector2(20, 60), Wrap = false, Center = 40 };
-        
+        static InputAxis DefaultZoomAxis => new () { Value = 40, Range = new Vector2(20, 60), Wrap = false, Center = 40 };
         
         protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
         {
             if (stage == CinemachineCore.Stage.Finalize)
             {
-                //state.Lens
+                var zoomValue = ZoomAxis.Value - ZoomAxis.Center;
+                if (state.Lens.Orthographic)
+                {
+                    
+                }
+                else
+                {
+                    state.Lens.FieldOfView += zoomValue;
+                }
             }
         }
         
         public void GetInputAxes(List<IInputAxisSource.AxisDescriptor> axes)
         {
-            axes.Add(new IInputAxisSource.AxisDescriptor { Axis = ZoomAxis, Name = "Look X (Pan)", AxisIndex = 0 });
+            axes.Add(new IInputAxisSource.AxisDescriptor { DrivenAxis = () => ref ZoomAxis, Name = "Zoom", AxisIndex = 2 });
+        }
+
+        void Reset()
+        {
+            ZoomAxis = DefaultZoomAxis;
         }
     }
 }
