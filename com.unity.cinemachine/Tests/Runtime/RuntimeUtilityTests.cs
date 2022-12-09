98acd7cfebbcee3709931a2ab4807ff820a78e86
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-12-09 11:00:19 -0500

    RuntimeUtility minor cleanup. Added RuntimeUtility tests

--- com.unity.cinemachine/Tests/Runtime/RuntimeUtilityTests.cs
+++ com.unity.cinemachine/Tests/Runtime/RuntimeUtilityTests.cs
@@ -12,7 +12,8 @@ namespace Tests.Runtime
     {
         GameObject m_Box;
         const string k_BoxTag = "Player";
-        static readonly Vector3 k_BoxPosition = new (0, 0, 10);
+        int m_LayerMask = -5; // [DefaultValue("Physics.DefaultRaycastLayers")] 
+        BoxCollider m_BoxCollider;
 
         [SetUp]
         public override void SetUp()
@@ -21,26 +22,51 @@ namespace Tests.Runtime
 
             m_Box = CreatePrimitive(PrimitiveType.Cube);
             m_Box.tag = k_BoxTag;
-            m_Box.transform.SetPositionAndRotation(k_BoxPosition, Quaternion.identity);
+            m_Box.transform.SetPositionAndRotation(new Vector3(0, 0, 10), Quaternion.identity);
+            m_BoxCollider = m_Box.GetComponent<BoxCollider>();
+            m_BoxCollider.isTrigger = false;
         }
-        
+
+        [TearDown]
+        public override void TearDown()
+        {
+            base.TearDown();
+        }
+
         [UnityTest]
         public IEnumerator RaycastIgnoreTagTest()
         {
-            yield return WaitForOnePhysicsFrame();
+            yield return WaitForOnePhysicsFrame(); // So that colliders are updated/moved
+
+            var boxPosition = m_Box.transform.position;
+            var ray = new Ray(Vector3.zero, boxPosition);
+            var rayLength = boxPosition.magnitude;
             
-            var ray = new Ray(Vector3.zero, k_BoxPosition);
-            Assert.True(RuntimeUtility.RaycastIgnoreTag(ray, out var hitInfo, k_BoxPosition.sqrMagnitude, LayerMask.NameToLayer("Default"), string.Empty));
-            Debug.Log(hitInfo.distance);
+            // check ray cast hit
+            Assert.True(RuntimeUtility.RaycastIgnoreTag(ray, out var hitInfo, rayLength, m_LayerMask, string.Empty));
+            Assert.That(hitInfo.distance, Is.EqualTo(boxPosition.z - (m_BoxCollider.size.z / 2f)));
             
-            Assert.False(RuntimeUtility.RaycastIgnoreTag(ray, out hitInfo, k_BoxPosition.sqrMagnitude, LayerMask.NameToLayer("Default"), k_BoxTag));
+            // check that ray cast did not hit anything, because it ignores the box's tag
+            Assert.False(RuntimeUtility.RaycastIgnoreTag(ray, out hitInfo, rayLength, m_LayerMask, k_BoxTag));
         }
         
-        // [UnityTest]
-        // public IEnumerator SphereCastIgnoreTagTest()
-        // {
-        //     
-        // }
+        [UnityTest]
+        public IEnumerator SphereCastIgnoreTagTest()
+        {
+            yield return WaitForOnePhysicsFrame(); // So that colliders are updated/moved
+
+            var boxPosition = m_Box.transform.position;
+            var direction = boxPosition.normalized; // shooting from origin
+            var rayLength = boxPosition.magnitude;
+            var sphereRadius = 1f;
+            
+            // check ray cast hit
+            Assert.True(RuntimeUtility.SphereCastIgnoreTag(Vector3.zero, sphereRadius, direction, out var hitInfo, rayLength, m_LayerMask, string.Empty));
+            Assert.That(hitInfo.distance, Is.EqualTo(boxPosition.z - (m_BoxCollider.size.z / 2f) - sphereRadius));
+            
+            // check that ray cast did not hit anything, because it ignores the box's tag
+            Assert.False(RuntimeUtility.SphereCastIgnoreTag(Vector3.zero, sphereRadius, direction, out hitInfo, rayLength, m_LayerMask, k_BoxTag));
+        }
     }
 }
 #endif
