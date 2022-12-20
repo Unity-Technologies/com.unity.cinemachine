3074ef5f071061fe681d2ef245f05337500609b0
Gregory Labute <gregoryl@unity3d.com>
2022-12-20 09:03:19 -0500

    CMCL-0000: cameron animation scales properly with speed (#715)
    * cameron animation scales properly with speed
    
    * hysteresis reduction

--- com.unity.cinemachine/Samples~/Shared Assets/Scripts/SimplePlayerAnimator.cs
+++ com.unity.cinemachine/Samples~/Shared Assets/Scripts/SimplePlayerAnimator.cs
@@ -5,51 +5,90 @@ namespace Cinemachine.Examples
     /// <summary>
     /// Add-on for SimplePlayerController that controls animation for the Cameron character.
     /// </summary>
-    [RequireComponent(typeof(SimplePlayerController))]
     [RequireComponent(typeof(Animator))]
     public class SimplePlayerAnimator : MonoBehaviour
     {
-        // Tune this to the animation in the model: feet should not slide when walking at this speed
-        public float NormalWalkSpeed = 1; 
+        [Tooltip("Tune this to the animation in the model: feet should not slide when walking at this speed")]
+        public float NormalWalkSpeed = 1.7f;
 
-        public float SprintAnimationScale = 1;
-        public float JumpAnimationScale = 1;
+        [Tooltip("Tune this to the animation in the model: feet should not slide when sprinting at this speed")]
+        public float NormalSprintSpeed = 5;
+
+        [Tooltip("Never speed up the sprint animation more than this, to avoid absurdly fast movement")]
+        public float MaxSprintScale = 1.4f;
+
+        [Tooltip("Scale factor for the overall speed of the jump animation")]
+        public float JumpAnimationScale = 0.65f;
 
         Animator m_Animator;
         SimplePlayerController m_Controller;
+        Vector3 m_PreviousPosition; // used if m_Controller == null
+        bool m_WasWalking;
+        bool m_WasRunning;
+        const float k_IdleThreshold = 0.2f;
 
         void Start()
         {
+            m_PreviousPosition = transform.position;
             TryGetComponent(out m_Animator);
             if (TryGetComponent(out m_Controller))
             {
                 m_Controller.StartJump += () => m_Animator.SetTrigger("Jump");
                 m_Controller.EndJump += () => m_Animator.SetTrigger("Land");
-                m_Controller.PostUpdate += UpdateAnimation;
+                m_Controller.PostUpdate += () =>
+                {
+                    var vel = m_Controller.GetPlayerVelocity();
+                    UpdateAnimation(vel);
+                    m_Animator.SetFloat("JumpScale", JumpAnimationScale * (m_Controller.IsSprinting 
+                        ? m_Controller.JumpSpeed / m_Controller.SprintJumpSpeed : 1));
+                };
             }
         }
 
-        void UpdateAnimation()
+        // LateUpdate so we normally don't have to worry about script execution order:
+        // we can assume that the player has already been moved
+        void LateUpdate()
         {
-            var vel = m_Controller.GetPlayerVelocity();
-            var speed = new Vector2(vel.x, vel.z).magnitude;
+            // In no-controller mode, we monitor the player's motion and deduce the appropriate animation.
+            // We don't support jumping in this mode.
+            if (m_Controller == null)
+            {
+                // Get velocity in player-local coords
+                var pos = transform.position;
+                var vel = Quaternion.Inverse(transform.rotation) * (pos - m_PreviousPosition) / Time.deltaTime;
+                m_PreviousPosition = pos;
+                UpdateAnimation(vel);
+            }
+        }
 
-            var speedX = m_Controller.Strafe ? vel.x : 0;
-            var speedZ = m_Controller.Strafe ? vel.z : speed;
+        void UpdateAnimation(Vector3 vel)
+        {
+            // Set animation params for current velocity
+            vel.y = 0; // we don't consider vertical movement
+            var speed = vel.magnitude;
+
+            // Hysteresis reduction
+            bool isRunning = speed > NormalWalkSpeed * 2 + (m_WasRunning ? -0.15f : 0.15f);
+            bool isWalking = !isRunning && speed > k_IdleThreshold + (m_WasWalking ? -0.05f : 0.05f);
+            m_WasWalking = isWalking;
+            m_WasRunning = isRunning;
+
+            // Set the normalized direction of motion and scale the animation speed to match motion speed
+            var dir = speed > k_IdleThreshold ? vel / speed : Vector3.zero;
+            var motionScale = isWalking ? speed / NormalWalkSpeed : 1;
+
+            // We scale the sprint animation speed to loosely match the actual speed, but we cheat
+            // at the high end to avoid making the animation look ridiculous
+            if (isRunning)
+                motionScale = (speed < NormalSprintSpeed) 
+                    ? speed / NormalSprintSpeed
+                    : Mathf.Min(MaxSprintScale, 1 + (speed - NormalSprintSpeed) / (3 * NormalSprintSpeed));
             
-            var animSpeedZ = Mathf.Min(speedZ, m_Controller.Speed) / (m_Controller.Speed * 2);
-            var sprintSpeed = Mathf.Abs(speedZ) - m_Controller.Speed;
-            if (sprintSpeed > 0 && speedZ > m_Controller.Speed)
-                animSpeedZ += Mathf.Sign(speedZ) * sprintSpeed / (m_Controller.SprintSpeed - m_Controller.Speed);
-
-            var animSpeedX = Mathf.Clamp(speedX / m_Controller.Speed, -1, 1);
-            var animSpeedY = Mathf.Clamp(vel.y / (m_Controller.IsSprinting ? m_Controller.SprintJumpSpeed : m_Controller.JumpSpeed), -1, 1);
-
-            m_Animator.SetFloat("SpeedZ", animSpeedZ);
-            m_Animator.SetFloat("SpeedX", animSpeedX);
-            m_Animator.SetFloat("SpeedY", animSpeedY);
-            m_Animator.SetFloat("WalkScale", m_Controller.IsSprinting ? SprintAnimationScale : m_Controller.Speed / NormalWalkSpeed);
-            m_Animator.SetFloat("JumpScale", JumpAnimationScale * (m_Controller.IsSprinting ? m_Controller.JumpSpeed / m_Controller.SprintJumpSpeed : 1));
+            m_Animator.SetFloat("DirX", dir.x);
+            m_Animator.SetFloat("DirZ", dir.z);
+            m_Animator.SetFloat("MotionScale", motionScale);
+            m_Animator.SetBool("Walking", isWalking);
+            m_Animator.SetBool("Running", isRunning);
         }
     }
 }
