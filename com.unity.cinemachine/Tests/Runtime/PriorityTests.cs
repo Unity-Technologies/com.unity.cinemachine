4e5dc3f3cb9a17beaa3d2d852deb1971a67f7d58
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-12-30 19:39:19 -0500

    More cleanup and a bit more test

--- com.unity.cinemachine/Tests/Runtime/PriorityTests.cs
+++ com.unity.cinemachine/Tests/Runtime/PriorityTests.cs
@@ -27,36 +27,48 @@ namespace Tests.Runtime
             base.TearDown();
         }
         
-        static IEnumerable PriorityCases
+        static IEnumerable PriorityValues
         {
+            // Values must be in descending order
             get
             {
                 yield return new TestCaseData(new[] {40, 30, 20, 10, 0, -10, -20, -30, -40}).SetName("Standard").Returns(null);
                 yield return new TestCaseData(new[] {int.MaxValue, int.MaxValue / 2, 0, int.MinValue / 2, int.MinValue}).SetName("Edge-case limits").Returns(null);
             }
         }
-        [UnityTest, TestCaseSource(nameof(PriorityCases))]
+        [UnityTest, TestCaseSource(nameof(PriorityValues))]
         public IEnumerator CheckPriorityOrder(int[] priorities)
         {
-            yield return null;
-            
             // Create vcams and set priorities
             for (var i = 0; i < priorities.Length; ++i)
             {
                 m_CmCameras.Add(CreateGameObject("CM Vcam " + i, typeof(CmCamera)).GetComponent<CmCamera>());
                 m_CmCameras[i].Priority = priorities[i];
             }
+            yield return null;
 
+            CmCamera activeCamera;
             // Check that active vcam is cmCamera. Then disable it and check that the next is now the active one.
             foreach (var cmCamera in m_CmCameras)
             {
-                yield return null;
-                
-                var activeCamera = m_Brain.ActiveVirtualCamera as CmCamera;
+                activeCamera = m_Brain.ActiveVirtualCamera as CmCamera;
                 Assert.NotNull(activeCamera);
                 Assert.That(activeCamera, Is.EqualTo(cmCamera));
                 activeCamera.enabled = false;
+                yield return null;
             }
+
+            // Re-enable all cm cameras
+            foreach (var cmCamera in m_CmCameras)
+            {
+                cmCamera.enabled = true;
+            }
+            yield return null;
+                
+            // Check that the first CmCamera in the list is the active one
+            activeCamera = m_Brain.ActiveVirtualCamera as CmCamera;
+            Assert.NotNull(activeCamera);
+            Assert.That(activeCamera, Is.EqualTo(m_CmCameras[0]));
         }
     }
 }
