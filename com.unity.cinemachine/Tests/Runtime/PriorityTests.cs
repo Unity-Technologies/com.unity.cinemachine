7cb0e636c769b6a646d6afe5cbf332bf35187eae
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-12-30 19:24:55 -0500

    Unit test for priority order and edge cases

---
+++ com.unity.cinemachine/Tests/Runtime/PriorityTests.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Cinemachine;
+
+namespace Tests.Runtime
+{
+    [TestFixture]
+    public class PriorityTests : CinemachineRuntimeFixtureBase
+    {
+        List<CmCamera> m_CmCameras;
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+
+            m_Brain.DefaultBlend.m_Style = CinemachineBlendDefinition.Style.Cut;
+            m_CmCameras = new List<CmCamera>();
+        }
+
+        [TearDown]
+        public override void TearDown()
+        {
+            base.TearDown();
+        }
+        
+        static IEnumerable PriorityCases
+        {
+            get
+            {
+                yield return new TestCaseData(new[] {40, 30, 20, 10, 0, -10, -20, -30, -40}).SetName("Standard").Returns(null);
+                yield return new TestCaseData(new[] {int.MaxValue, int.MaxValue / 2, 0, int.MinValue / 2, int.MinValue}).SetName("Edge-case limits").Returns(null);
+            }
+        }
+        [UnityTest, TestCaseSource(nameof(PriorityCases))]
+        public IEnumerator CheckPriorityOrder(int[] priorities)
+        {
+            // Create vcams and set priorities
+            for (var i = 0; i < priorities.Length; ++i)
+            {
+                m_CmCameras.Add(CreateGameObject("CM Vcam " + i, typeof(CmCamera)).GetComponent<CmCamera>());
+                m_CmCameras[i].PriorityAndChannel.Enabled = true;
+                m_CmCameras[i].Priority = priorities[i];
+            }
+            
+            yield return null;
+
+            // Check that active vcam is the current vcam. Then disable it and check that now the next is the active one.
+            foreach (var cmCamera in m_CmCameras)
+            {
+                var activeCamera = m_Brain.ActiveVirtualCamera as CmCamera;
+                Assert.NotNull(activeCamera);
+                Assert.That(activeCamera, Is.EqualTo(cmCamera));
+                activeCamera.enabled = false;
+                yield return null;
+            }
+        }
+        
+        
+    }
+}
