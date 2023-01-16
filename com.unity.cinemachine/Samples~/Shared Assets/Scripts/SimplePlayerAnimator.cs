0f6471e99adfab80a4d76dc97ffb192b6acbde4e
Krisztian G. Birkas <gaborkb@unity3d.com>
2023-01-16 13:47:39 -0500

    Resolved conflicts

--- com.unity.cinemachine/Samples~/Shared Assets/Scripts/SimplePlayerAnimator.cs
+++ com.unity.cinemachine/Samples~/Shared Assets/Scripts/SimplePlayerAnimator.cs
@@ -25,11 +25,7 @@ namespace Cinemachine.Examples
 
         Animator m_Animator;
         SimplePlayerController m_Controller;
-<<<<<<< HEAD
-        Vector3 m_PreviousPosition; // used if m_Controller == null
-=======
         Vector3 m_PreviousPosition; // used if m_Controller == null or disabled
->>>>>>> main
         bool m_WasWalking;
         bool m_WasRunning;
         const float k_IdleThreshold = 0.2f;
@@ -45,34 +41,20 @@ namespace Cinemachine.Examples
                 m_Controller.EndJump += () => m_Animator.SetTrigger("Land");
                 m_Controller.PostUpdate += () =>
                 {
-<<<<<<< HEAD
-                    var vel = m_Controller.GetPlayerVelocity();
-                    UpdateAnimation(vel);
-=======
                     UpdateAnimation(m_Controller.GetPlayerVelocity());
->>>>>>> main
                     m_Animator.SetFloat("JumpScale", JumpAnimationScale * (m_Controller.IsSprinting 
                         ? m_Controller.JumpSpeed / m_Controller.SprintJumpSpeed : 1));
                 };
             }
         }
-
-<<<<<<< HEAD
-        // LateUpdate so we normally don't have to worry about script execution order:
-        // we can assume that the player has already been moved
-=======
+        
         // We use LateUpdate so we normally don't have to worry about script execution order:
         // we can assume that the player has already been moved.
->>>>>>> main
         void LateUpdate()
         {
             // In no-controller mode, we monitor the player's motion and deduce the appropriate animation.
             // We don't support jumping in this mode.
-<<<<<<< HEAD
-            if (m_Controller == null)
-=======
             if (m_Controller == null || !m_Controller.enabled)
->>>>>>> main
             {
                 // Get velocity in player-local coords
                 var pos = transform.position;
@@ -81,16 +63,10 @@ namespace Cinemachine.Examples
                 UpdateAnimation(vel);
             }
         }
-
-<<<<<<< HEAD
-        void UpdateAnimation(Vector3 vel)
-        {
-            // Set animation params for current velocity
-=======
+        
         // Set animation params for current velocity
         void UpdateAnimation(Vector3 vel)
         {
->>>>>>> main
             vel.y = 0; // we don't consider vertical movement
             var speed = vel.magnitude;
 
