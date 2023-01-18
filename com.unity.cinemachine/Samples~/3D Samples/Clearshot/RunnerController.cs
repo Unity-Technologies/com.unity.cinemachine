4b5421e479fa61dd7cbae0dbd6cbea8c553a914f
Greg Labute <gregoryl@unity3d.com>
2023-01-18 12:09:37 -0500

    Update samples for Enabled flag

--- com.unity.cinemachine/Samples~/3D Samples/Clearshot/RunnerController.cs
+++ com.unity.cinemachine/Samples~/3D Samples/Clearshot/RunnerController.cs
@@ -7,22 +7,21 @@ namespace Cinemachine.Examples
     public class RunnerController : MonoBehaviour
     {
         CinemachineSplineCart m_Cart;
-
-        void Start() => m_Cart = GetComponent<CinemachineSplineCart>();
-
         static bool s_LeaderWasSlowed;
         bool m_IsTired;
 
+        void Start() => m_Cart = GetComponent<CinemachineSplineCart>();
+
         void Update()
         {
-            // Slow down leader to improve chances of at least 1 take over in one run
+            // Slow down leader to improve chances of at least 1 takeover per run
             if (!s_LeaderWasSlowed)
             {
                 if (m_Cart.Spline.Spline.ConvertIndexUnit(
                     m_Cart.SplinePosition, m_Cart.PositionUnits, PathIndexUnit.Normalized) > 0.5f)
                 {
                     s_LeaderWasSlowed = true;
-                    if (m_Cart.AutomaticDolly.Implementation is RandomizedDollySpeed speedControl)
+                    if (m_Cart.AutomaticDolly.Method is RandomizedDollySpeed speedControl)
                     {
                         // Leader is tired!
                         speedControl.MinSpeed /= 2;
@@ -36,10 +35,13 @@ namespace Cinemachine.Examples
         // This is called by the "Restart Race" UX button.
         public void ResetRace()
         {
+            // Reset position to start
             m_Cart.SplinePosition = 0;
-            if (m_Cart.AutomaticDolly.Implementation is RandomizedDollySpeed speedControl)
+
+            // Reset slowdown mechanism
+            if (m_Cart.AutomaticDolly.Method is RandomizedDollySpeed speedControl)
             {
-                m_Cart.AutomaticDolly.Implementation.Reset();
+                m_Cart.AutomaticDolly.Method.Reset();
                 if (m_IsTired)
                 {
                     // Restore the speed, leader is no longer tired
