6afa08901242b9e54ae9e26b07c7fa8f6cb1d730
Greg Labute <gregoryl@unity3d.com>
2023-01-28 10:05:31 -0500

    InputAxisControllerEditor in UITK

--- com.unity.cinemachine/Editor/Editors/InputAxisControllerEditor.cs
+++ com.unity.cinemachine/Editor/Editors/InputAxisControllerEditor.cs
@@ -1,9 +1,6 @@
-using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Cinemachine.Editor
@@ -11,92 +8,70 @@ namespace Cinemachine.Editor
     [CustomEditor(typeof(InputAxisController))]
     class InputAxisControllerEditor : UnityEditor.Editor
     {
-        public override void OnInspectorGUI()
+        InputAxisController Target => target as InputAxisController;
+
+        VisualElement m_ListIsEmptyMessage;
+        int m_ListCount;
+
+        void OnDisable()
         {
-            var Target = target as InputAxisController;
+            EditorApplication.update -= UpdateControllersStatus;
+        }
+
+        void UpdateControllersStatus()
+        {
             if (Target != null && !Target.ConrollersAreValid())
             {
                 Undo.RecordObject(Target, "SynchronizeControllers");
                 Target.SynchronizeControllers();
             }
-
-            serializedObject.Update();
-            var controllers = serializedObject.FindProperty("Controllers");
-
-            EditorGUI.BeginChangeCheck();
+        }
+        
+        public override VisualElement CreateInspectorGUI()
+        {
+            EditorApplication.update -= UpdateControllersStatus;
+            EditorApplication.update += UpdateControllersStatus;
+            
+            var ux = new VisualElement();
 
 #if CINEMACHINE_UNITY_INPUTSYSTEM
-            EditorGUILayout.PropertyField(serializedObject.FindProperty("PlayerIndex"));
-            EditorGUILayout.PropertyField(serializedObject.FindProperty("AutoEnableInputs"));
+            ux.Add(new PropertyField(serializedObject.FindProperty(() => Target.PlayerIndex)));
+            ux.Add(new PropertyField(serializedObject.FindProperty(() => Target.AutoEnableInputs)));
 #endif
-
-            int numElements = controllers.arraySize;
-            if (numElements == 0)
-                EditorGUILayout.HelpBox("No applicable CM components found.  Must have one of: "
-                    + InspectorUtility.GetAssignableBehaviourNames(typeof(IInputAxisSource)), 
-                    MessageType.Warning);
-            else for (int i = 0; i < numElements; ++i)
+            ux.AddHeader("Driven Axes");
+            var list = ux.AddChild(new ListView()
             {
-                var element = controllers.GetArrayElementAtIndex(i);
+                reorderable = false,
+                showAddRemoveFooter = false,
+                showBorder = false,
+                showBoundCollectionSize = false,
+                showFoldoutHeader = false,
+                virtualizationMethod = CollectionVirtualizationMethod.DynamicHeight
+            });
+            var controllersProperty = serializedObject.FindProperty(() => Target.Controllers);
+            list.BindProperty(controllersProperty);
 
-                var rect = EditorGUILayout.GetControlRect();
-                float height = EditorGUIUtility.singleLineHeight;
-                rect.height = height;
+            m_ListIsEmptyMessage = ux.AddChild(new HelpBox("No applicable CM components found.  Must have one of: "
+                    + InspectorUtility.GetAssignableBehaviourNames(typeof(IInputAxisSource)), HelpBoxMessageType.Warning));
 
-                element.isExpanded = EditorGUI.Foldout(
-                    new Rect(rect.x, rect.y, EditorGUIUtility.labelWidth - height, rect.height),
-                    element.isExpanded, 
-                    new GUIContent(element.displayName, element.tooltip), true);
+            m_ListCount = controllersProperty.arraySize;
+            TrackControllerCount(controllersProperty);
+            list.TrackPropertyValue(controllersProperty, TrackControllerCount);
 
-                if (element.isExpanded)
-                {
-                    ++EditorGUI.indentLevel;
-                    rect = EditorGUILayout.GetControlRect(true, InspectorUtility.PropertyHeightOfChidren(element));
-                    InspectorUtility.DrawChildProperties(rect, element);
-                    --EditorGUI.indentLevel;
-                }
-                else
-                {
-                    // Draw the input value on the same line as the foldout, for convenience
-                    SerializedProperty actionProperty = null;
-                    SerializedProperty legacyProperty = null;
-                    int numFields = 0;
-#if CINEMACHINE_UNITY_INPUTSYSTEM
-                    actionProperty = element.FindPropertyRelative("InputAction");
-                    ++numFields;
-#endif
-#if ENABLE_LEGACY_INPUT_MANAGER
-                    legacyProperty = element.FindPropertyRelative("LegacyInput");
-                    ++numFields;
-#endif
-                    if (numFields > 0)
-                    {
-                        var blankLabelWidth = height / numFields;
-                        rect.x += EditorGUIUtility.labelWidth - blankLabelWidth;
-                        rect.width -= EditorGUIUtility.labelWidth - blankLabelWidth;
-                        rect.width /= numFields;
+            return ux;
+        }
 
-                        int oldIndent = EditorGUI.indentLevel;
-                        float oldLabelWidth = EditorGUIUtility.labelWidth;
+        void TrackControllerCount(SerializedProperty property)
+        {
+            m_ListIsEmptyMessage.SetVisible(property.arraySize == 0);
 
-                        EditorGUI.indentLevel = 0;
-                        EditorGUIUtility.labelWidth = blankLabelWidth;
-
-                        if (actionProperty != null)
-                        {
-                            EditorGUI.PropertyField(rect, actionProperty, new GUIContent(" ", actionProperty.tooltip));
-                            rect.x += rect.width;
-                        }
-                        if (legacyProperty != null)
-                            EditorGUI.PropertyField(rect, legacyProperty, new GUIContent(" ", legacyProperty.tooltip));
-
-                        EditorGUI.indentLevel = oldIndent;
-                        EditorGUIUtility.labelWidth = oldLabelWidth;
-                    }
-                }
+            // New elements should be collapsed.  GML todo: how to do this less intrusively?
+            if (property.arraySize != m_ListCount)
+            {
+                m_ListCount = property.arraySize;
+                for (int i = 0; i < m_ListCount; ++i)
+                    property.GetArrayElementAtIndex(i).isExpanded = false;
             }
-            if (EditorGUI.EndChangeCheck())
-                serializedObject.ApplyModifiedProperties();
         }
 
         [InitializeOnLoad]
@@ -165,54 +140,26 @@ namespace Cinemachine.Editor
         }
     }
 
-
-
-#if false // GML incomplete code.  This is not working yet in UITK - stay in IMGUI for now
-        public override VisualElement CreateInspectorGUI()
-        {
-            var Target = target as InputAxisController;
-
-            var ux = new VisualElement();
-
-            ux.Add(new PropertyField(serializedObject.FindProperty("PlayerIndex")));
-#if CINEMACHINE_UNITY_INPUTSYSTEM
-            ux.Add(new PropertyField(serializedObject.FindProperty("AutoEnableInputs"));
-#endif
-            ux.AddSpace();
-
-            //var list = new PropertyField(serializedObject.FindProperty("Controllers"))
-            var list = new ListView()
-            {
-                reorderable = false,
-                showAddRemoveFooter = false,
-                showBorder = false,
-                showBoundCollectionSize = false,
-                showFoldoutHeader = false,
-                virtualizationMethod = CollectionVirtualizationMethod.DynamicHeight
-            };
-            list.BindProperty(serializedObject.FindProperty("Controllers"));
-            ux.Add(list);
-
-            return ux;
-        }
-    }
-
     [CustomPropertyDrawer(typeof(InputAxisController.Controller))]
     sealed class InputAxisControllerItemPropertyDrawer : PropertyDrawer
     {
-        InputAxisController.Controller m_def = new InputAxisController.Controller();
+        InputAxisController.Controller m_def = new ();
 
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
-            // Draw the input value on the same line as the foldout, for convenience
-            SerializedProperty inputProp = null;
-#if CINEMACHINE_UNITY_INPUTSYSTEM
-            inputProp = property.FindPropertyRelative(() => m_def.InputAction);
-#elif ENABLE_LEGACY_INPUT_MANAGER
-            inputProp = property.FindPropertyRelative(() => m_def.InputName);
-#endif
-            var overlay = new PropertyField(inputProp, "") { style = {flexGrow = 1}};
+            var overlay = new VisualElement { style = { flexDirection = FlexDirection.Row, flexGrow = 1 }};
+            overlay.Add(new PropertyField(property.FindPropertyRelative(() => m_def.Enabled), "") 
+                { style = {flexGrow = 0, flexBasis = InspectorUtility.SingleLineHeight, alignSelf = Align.Center}} );
 
+            // Draw the input value on the same line as the foldout, for convenience
+#if CINEMACHINE_UNITY_INPUTSYSTEM
+            overlay.Add(new PropertyField(property.FindPropertyRelative(() => m_def.InputAction), "") 
+                { style = {flexGrow = 1, flexBasis = 5 * InspectorUtility.SingleLineHeight}} );
+#endif
+#if ENABLE_LEGACY_INPUT_MANAGER
+            overlay.Add(new PropertyField(property.FindPropertyRelative(() => m_def.LegacyInput), "") 
+                { style = {flexGrow = 1, flexBasis = 5 * InspectorUtility.SingleLineHeight, marginLeft = 4}} );
+#endif
             var foldout = new Foldout()
             {
                 text = property.displayName,
@@ -229,8 +176,5 @@ namespace Cinemachine.Editor
             }
             return new InspectorUtility.FoldoutWithOverlay(foldout, overlay, null);
         }
-
-        //VisualElement CreateInputAxisNameControl(
     }
-#endif
 }
