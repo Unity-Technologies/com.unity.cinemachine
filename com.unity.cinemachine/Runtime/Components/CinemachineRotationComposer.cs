71ca1d222c4e77b1628025b734a727ccf72badda
Antoine Charton <antoine.charton@gmail.com>
2023-02-07 14:19:57 -0500

    Reverted rotation composer comments removal

--- com.unity.cinemachine/Runtime/Components/CinemachineRotationComposer.cs
+++ com.unity.cinemachine/Runtime/Components/CinemachineRotationComposer.cs
@@ -187,7 +187,7 @@ namespace Cinemachine
             {
                 if (deltaTime >= 0 && VirtualCamera.PreviousStateIsValid)
                     curState.RawOrientation = m_CameraOrientationPrevFrame;
-                return;
+                return; // navel-gazing, get outa here
             }
 
             // Expensive FOV calculations
