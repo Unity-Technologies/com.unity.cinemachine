d9d675a1acab4d2d0657e123284ed40cc1f7781b
Antoine Charton <antoine.charton@gmail.com>
2023-02-21 17:44:37 -0500

    Update assembly infos with new assemblies names

--- com.unity.cinemachine/Runtime/AssemblyInfo.cs
+++ com.unity.cinemachine/Runtime/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Unity.Cinemachine.Editor")]
-[assembly: InternalsVisibleTo("com.unity.cinemachine.shared.tests")]
-[assembly: InternalsVisibleTo("com.unity.cinemachine.editor.tests")]
-[assembly: InternalsVisibleTo("com.unity.cinemachine.runtime.tests")]
+[assembly: InternalsVisibleTo("Unity.Cinemachine.Shared.Tests")]
+[assembly: InternalsVisibleTo("Unity.Cinemachine.Editor.Tests")]
+[assembly: InternalsVisibleTo("Unity.Cinemachine.Runtime.Tests")]
