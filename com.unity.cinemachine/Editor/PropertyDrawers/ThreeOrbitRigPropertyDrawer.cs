65c0335211143fae0edb0f89e24b83e1abe88393
Greg Labute <gregoryl@unity3d.com>
2023-02-26 07:58:18 -0500

    Update ThreeOrbitRigPropertyDrawer.cs

--- com.unity.cinemachine/Editor/PropertyDrawers/ThreeOrbitRigPropertyDrawer.cs
+++ com.unity.cinemachine/Editor/PropertyDrawers/ThreeOrbitRigPropertyDrawer.cs
@@ -6,10 +6,9 @@ namespace Cinemachine.Editor
     [CustomPropertyDrawer(typeof(Cinemachine3OrbitRig.Orbit))]
     class ThreeOrbitRigPropertyDrawer : PropertyDrawer
     {
-        Cinemachine3OrbitRig.Orbit def = new Cinemachine3OrbitRig.Orbit();
-
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
+            Cinemachine3OrbitRig.Orbit def = new ();
             var row = new InspectorUtility.LeftRightRow();
             row.Left.Add(new Label(property.displayName) { tooltip = property.tooltip });
             row.Right.AddChild(new InspectorUtility.CompactPropertyField(
