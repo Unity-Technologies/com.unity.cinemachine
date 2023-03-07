ef8ed599627d28fa81d18a787afe6eea6e073f9c
Krisztian G. Birkas <gaborkb@unity3d.com>
2023-03-07 15:32:11 -0500

    Previous commit continued. Added namespaces for UI scripts

--- com.unity.cinemachine/Samples~/Shared Assets/UI/SampleHelpUI.cs
+++ com.unity.cinemachine/Samples~/Shared Assets/UI/SampleHelpUI.cs
@@ -2,51 +2,54 @@ using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-/// <summary>
-/// Displays a button in the game view that will bring up a window with scrollable text.
-/// </summary>
-public class SampleHelpUI : MonoBehaviour
+namespace Unity.Cinemachine.Samples
 {
-    public bool IsVisible;
-    public string HelpTitle;
-    [TextArea(minLines: 10, maxLines: 50)]
-    public string HelpText;
-
-    VisualElement m_HelpBox;
-    Toggle m_HelpToggle;
-    Button m_CloseButton;
-
-    void OnEnable()
+    /// <summary>
+    /// Displays a button in the game view that will bring up a window with scrollable text.
+    /// </summary>
+    public class SampleHelpUI : MonoBehaviour
     {
-        var uiDocument = GetComponent<UIDocument>();
+        public bool IsVisible;
+        public string HelpTitle;
+        [TextArea(minLines: 10, maxLines: 50)]
+        public string HelpText;
 
-        m_HelpToggle = uiDocument.rootVisualElement.Q("HelpToggle") as Toggle;
-        m_HelpBox = uiDocument.rootVisualElement.Q("HelpBox");
-        if (uiDocument.rootVisualElement.Q("HelpTitle") is Label helpTitle)
-            helpTitle.text = HelpTitle;
-        if (uiDocument.rootVisualElement.Q("HelpTextField") is TextField helpText)
-            helpText.value = HelpText;
-        m_CloseButton = uiDocument.rootVisualElement.Q("CloseButton") as Button;
+        VisualElement m_HelpBox;
+        Toggle m_HelpToggle;
+        Button m_CloseButton;
 
-        m_HelpToggle.RegisterValueChangedCallback(HelpToggleChanged);
-        m_CloseButton.RegisterCallback<ClickEvent>(CloseHelpBox);
-        
-        m_HelpToggle.value = m_HelpBox.visible = IsVisible;
-    }
+        void OnEnable()
+        {
+            var uiDocument = GetComponent<UIDocument>();
 
-    void OnDisable()
-    {
-        m_CloseButton.UnregisterCallback<ClickEvent>(CloseHelpBox);
-        m_HelpToggle.UnregisterValueChangedCallback(HelpToggleChanged);
-    }
-    
-    void OnValidate()
-    {
-        if (m_HelpToggle != null && m_HelpBox != null)
+            m_HelpToggle = uiDocument.rootVisualElement.Q("HelpToggle") as Toggle;
+            m_HelpBox = uiDocument.rootVisualElement.Q("HelpBox");
+            if (uiDocument.rootVisualElement.Q("HelpTitle") is Label helpTitle)
+                helpTitle.text = HelpTitle;
+            if (uiDocument.rootVisualElement.Q("HelpTextField") is TextField helpText)
+                helpText.value = HelpText;
+            m_CloseButton = uiDocument.rootVisualElement.Q("CloseButton") as Button;
+
+            m_HelpToggle.RegisterValueChangedCallback(HelpToggleChanged);
+            m_CloseButton.RegisterCallback<ClickEvent>(CloseHelpBox);
+
             m_HelpToggle.value = m_HelpBox.visible = IsVisible;
+        }
+
+        void OnDisable()
+        {
+            m_CloseButton.UnregisterCallback<ClickEvent>(CloseHelpBox);
+            m_HelpToggle.UnregisterValueChangedCallback(HelpToggleChanged);
+        }
+
+        void OnValidate()
+        {
+            if (m_HelpToggle != null && m_HelpBox != null)
+                m_HelpToggle.value = m_HelpBox.visible = IsVisible;
+        }
+
+        void HelpToggleChanged(ChangeEvent<bool> evt) => m_HelpBox.visible = IsVisible = evt.newValue;
+
+        void CloseHelpBox(ClickEvent evt) => m_HelpToggle.value = false;
     }
-
-    void HelpToggleChanged(ChangeEvent<bool> evt) => m_HelpBox.visible = IsVisible = evt.newValue;
-
-    void CloseHelpBox(ClickEvent evt) => m_HelpToggle.value = false;
 }
--- com.unity.cinemachine/Samples~/Shared Assets/UI/SamplesDynamicUI.cs
+++ com.unity.cinemachine/Samples~/Shared Assets/UI/SamplesDynamicUI.cs
@@ -4,83 +4,87 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UIElements;
 
-public class SamplesDynamicUI : MonoBehaviour
+namespace Unity.Cinemachine.Samples
 {
-    [Serializable]
-    public class Item
+    public class SamplesDynamicUI : MonoBehaviour
     {
-        [Tooltip("Text displayed on button")]
-        public string Name = "Text";
+        [Serializable]
+        public class Item
+        {
+            [Tooltip("Text displayed on button")]
+            public string Name = "Text";
 
-        [Tooltip("Set this to true to create a toggle button")]
-        public bool IsToggle;
+            [Tooltip("Set this to true to create a toggle button")]
+            public bool IsToggle;
 
-        [Tooltip("Value of the toggle")]
-        public bool ToggleValue;
+            [Tooltip("Value of the toggle")]
+            public bool ToggleValue;
 
-        [Tooltip("Event sent when button is clicked")]
-        public UnityEvent OnClick = new();
+            [Tooltip("Event sent when button is clicked")]
+            public UnityEvent OnClick = new();
 
-        [Tooltip("Event sent when toggle button is clicked")]
-        public UnityEvent<bool> OnValueChanged = new();
-    }
-    
-    [Tooltip("The buttons to be displayed")]
-    public List<Item> Buttons = new ();
+            [Tooltip("Event sent when toggle button is clicked")]
+            public UnityEvent<bool> OnValueChanged = new();
+        }
 
-    Color m_UnityLightGray = new Color(196, 196, 196, 255);
-    VisualElement m_DynamicBox;
-    List<VisualElement> m_DynamicELements;
-    void OnEnable()
-    {
-        var uiDocument = GetComponent<UIDocument>();
-        
-        m_DynamicBox = uiDocument.rootVisualElement.Q("TogglesAndButtons");
-        m_DynamicELements = new List<VisualElement>(Buttons.Count);
-        foreach (var item in Buttons)
-            if (item.IsToggle)
-            {
-                var toggle = new Toggle
+        [Tooltip("The buttons to be displayed")]
+        public List<Item> Buttons = new();
+
+        Color m_UnityLightGray = new Color(196, 196, 196, 255);
+        VisualElement m_DynamicBox;
+        List<VisualElement> m_DynamicELements;
+
+        void OnEnable()
+        {
+            var uiDocument = GetComponent<UIDocument>();
+
+            m_DynamicBox = uiDocument.rootVisualElement.Q("TogglesAndButtons");
+            m_DynamicELements = new List<VisualElement>(Buttons.Count);
+            foreach (var item in Buttons)
+                if (item.IsToggle)
                 {
-                    label = item.Name,
-                    value = item.ToggleValue,
-                    focusable = false,
-                    style =
+                    var toggle = new Toggle
                     {
-                        flexDirection = new StyleEnum<FlexDirection>(FlexDirection.RowReverse),
-                        alignSelf = new StyleEnum<Align>(Align.FlexStart),
-                        unityFontStyleAndWeight = new StyleEnum<FontStyle>(FontStyle.Bold),
-                    }
-                };
-                toggle.RegisterValueChangedCallback(e => item.OnValueChanged.Invoke(e.newValue));
-                m_DynamicBox.Add(toggle);
-                m_DynamicELements.Add(toggle);
-            }
-            else
-            {
-                var button = new Button
+                        label = item.Name,
+                        value = item.ToggleValue,
+                        focusable = false,
+                        style =
+                        {
+                            flexDirection = new StyleEnum<FlexDirection>(FlexDirection.RowReverse),
+                            alignSelf = new StyleEnum<Align>(Align.FlexStart),
+                            unityFontStyleAndWeight = new StyleEnum<FontStyle>(FontStyle.Bold),
+                        }
+                    };
+                    toggle.RegisterValueChangedCallback(e => item.OnValueChanged.Invoke(e.newValue));
+                    m_DynamicBox.Add(toggle);
+                    m_DynamicELements.Add(toggle);
+                }
+                else
                 {
-                    text = item.Name,
-                    focusable = false,
-                    style =
+                    var button = new Button
                     {
-                        backgroundColor = new StyleColor(m_UnityLightGray),
-                        alignSelf = new StyleEnum<Align>(Align.FlexStart),
-                        unityFontStyleAndWeight = new StyleEnum<FontStyle>(FontStyle.Bold),
-                    }
-                };
-                button.clickable.clicked += item.OnClick.Invoke;
-                m_DynamicBox.Add(button);
-                m_DynamicELements.Add(button);
-            }
+                        text = item.Name,
+                        focusable = false,
+                        style =
+                        {
+                            backgroundColor = new StyleColor(m_UnityLightGray),
+                            alignSelf = new StyleEnum<Align>(Align.FlexStart),
+                            unityFontStyleAndWeight = new StyleEnum<FontStyle>(FontStyle.Bold),
+                        }
+                    };
+                    button.clickable.clicked += item.OnClick.Invoke;
+                    m_DynamicBox.Add(button);
+                    m_DynamicELements.Add(button);
+                }
 
-        m_DynamicBox.visible = Buttons.Count > 0;
-    }
+            m_DynamicBox.visible = Buttons.Count > 0;
+        }
 
-    void OnDisable()
-    {
-        foreach (var element in m_DynamicELements) 
-            m_DynamicBox.Remove(element);
-        m_DynamicELements.Clear();
+        void OnDisable()
+        {
+            foreach (var element in m_DynamicELements)
+                m_DynamicBox.Remove(element);
+            m_DynamicELements.Clear();
+        }
     }
 }
