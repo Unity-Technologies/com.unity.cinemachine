acc401711f199b1a0c3160452d42b32b4dd0fe6a
Antoine Charton <antoine.charton@gmail.com>
2023-03-15 13:37:24 -0400

    Add test that checks the brain setup without a camera attached

---
+++ com.unity.cinemachine/Tests/Runtime/CinemachineBrainSetupTest.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Unity.Cinemachine.Tests.Editor
+{
+    [TestFixture]
+    public class CinemachineBrainSetup
+    {
+        CinemachineCamera m_Vcam;
+        CinemachineBrain m_Brain;
+        GameObject m_FollowObject;
+
+        [SetUp]
+        public void Setup()
+        {
+            m_Brain = new GameObject("Brain").AddComponent<CinemachineBrain>();
+            m_Vcam = new GameObject("CM Vcam").AddComponent<CinemachineCamera>();
+            m_Vcam.Priority.Value = 100;
+            m_FollowObject = new GameObject("Follow");
+            m_Vcam.Target.TrackingTarget = m_FollowObject.transform;
+            m_Vcam.gameObject.AddComponent<CinemachineOrbitalFollow>();
+            m_Vcam.gameObject.AddComponent<CinemachineRotationComposer>();
+            m_Vcam.gameObject.AddComponent<CinemachineFreeLookModifier>();
+            m_Vcam.gameObject.AddComponent<InputController1AllAxis>();
+        }
+
+        [UnityTest]
+        public IEnumerator SelectBrainWithoutCameraComponentDoesNotCauseErrors()
+        {
+            Selection.activeGameObject = m_Vcam.gameObject;
+            yield return null;
+            yield return null;
+            Selection.activeGameObject = null;
+        }
+    }
+}
