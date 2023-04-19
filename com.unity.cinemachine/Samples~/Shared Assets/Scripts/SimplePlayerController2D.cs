36c6c775147c3b41f2d1ea534f4d8a6d33540573
Greg Labute <gregoryl@unity3d.com>
2023-04-19 12:54:00 -0400

    PR fixes

--- com.unity.cinemachine/Samples~/Shared Assets/Scripts/SimplePlayerController2D.cs
+++ com.unity.cinemachine/Samples~/Shared Assets/Scripts/SimplePlayerController2D.cs
@@ -5,6 +5,9 @@ namespace Unity.Cinemachine.Samples
     [RequireComponent(typeof(Rigidbody2D))]
     public class SimplePlayerController2D : SimplePlayerControllerBase
     {
+        public Transform PlayerGeometry;
+        public bool MotionControlWhileInAir;
+
         bool m_IsSprinting;
         bool m_IsGrounded;
         Rigidbody2D m_Rigidbody2D;
@@ -20,19 +23,19 @@ namespace Unity.Cinemachine.Samples
             m_IsSprinting = false;
         }
 
-        void Update()
+        void FixedUpdate()
         {
             Cursor.lockState = LockCursor ? CursorLockMode.Locked : CursorLockMode.None;
             PreUpdate?.Invoke();
             var vel = m_Rigidbody2D.velocity;
 
             // Compute the new velocity and move the player, but only if not mid-jump
-            if (m_IsGrounded)
+            if (m_IsGrounded || MotionControlWhileInAir)
             {
                 // Read the input from the user
                 m_IsSprinting = Sprint.Value > 0.5f;
                 var desiredVel = new Vector2(MoveX.Value * (m_IsSprinting ? SprintSpeed : Speed), 0);
-                if (Mathf.Max(MoveZ.Value, Jump.Value) > 0.01f)
+                if (!IsJumping && Mathf.Max(MoveZ.Value, Jump.Value) > 0.01f)
                 {
                     desiredVel.y = m_IsSprinting ? SprintJumpSpeed : JumpSpeed;
                     vel.y = m_IsSprinting ? SprintJumpSpeed : JumpSpeed;
@@ -41,15 +44,17 @@ namespace Unity.Cinemachine.Samples
             }
 
             // Rotate the player to face movement direction
-            if (vel.x > Speed * 0.5f)
-                transform.rotation = Quaternion.identity;
-            if (vel.x < -Speed * 0.5f)
-                transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
-
+            if (PlayerGeometry.rotation != null)
+            {
+                if (vel.x > Speed * 0.5f)
+                    PlayerGeometry.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+                if (vel.x < -Speed * 0.5f)
+                    PlayerGeometry.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+            }                
             m_Rigidbody2D.velocity = vel;
 
             PostUpdate?.Invoke(
-                Quaternion.Inverse(transform.rotation) * new Vector3(0, vel.y, vel.x), 
+                new Vector3(0, vel.y, Mathf.Abs(vel.x)), 
                 m_IsSprinting ? JumpSpeed / SprintJumpSpeed : 1);
         }
 
