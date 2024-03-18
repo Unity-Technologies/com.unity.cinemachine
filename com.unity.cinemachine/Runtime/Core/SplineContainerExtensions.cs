cb7b235a72ff1f2aab6dd8eb6a89c9bb5d82d4d4
Gregory Labute <gregoryl@unity3d.com>
2024-03-18 17:55:26 -0400

    Bugfix: Spline roll was being calculated incorrectly when spline was rotated.

--- com.unity.cinemachine/Runtime/Core/SplineContainerExtensions.cs
+++ com.unity.cinemachine/Runtime/Core/SplineContainerExtensions.cs
@@ -88,7 +88,7 @@ namespace Unity.Cinemachine
         {
             var result = LocalEvaluateSplineWithRoll(spline, roll, defaultRotation, tNormalized, out position, out rotation);
             position = spline.transform.TransformPoint(position);
-            rotation = rotation * spline.transform.rotation;
+            rotation = spline.transform.rotation * rotation;
             return result;
         }
         
