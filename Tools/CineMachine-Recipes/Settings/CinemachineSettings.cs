a57a5cb0a27bd06bd3bf673912145e93ce53b332
Simon Schossleitner <simons@unity3d.com>
2024-04-17 08:04:58 +0200

    Initial onboarding with Wrench 0.8.16 Migrate Pack Job Dependency

---
+++ Tools/CineMachine-Recipes/Settings/CinemachineSettings.cs
@@ -0,0 +1,27 @@
+﻿using RecipeEngine.Modules.Wrench.Settings;
+
+namespace Cinemachine.Cookbook.Settings;
+
+public class CinemachineSettings
+{
+    // Path from the root of the repository where packages are located.
+    private static string PackagesRootPath = ".";
+
+    // update this to list all packages in this repo that you want to release.
+    private static List<string> PackagesToRelease = new()
+    {
+        "com.unity.cinemachine",
+    };
+    ISet<string> PvPprofilesToCheck = new HashSet<string>() { "PVP-20-1" };
+    public CinemachineSettings()
+    {
+        Settings = new WrenchSettings(
+            PackagesRootPath,
+            PackagesToRelease
+            
+        );
+        Settings.PvpProfilesToCheck = PvPprofilesToCheck;
+    }
+
+    public WrenchSettings Settings { get; set; }
+}
