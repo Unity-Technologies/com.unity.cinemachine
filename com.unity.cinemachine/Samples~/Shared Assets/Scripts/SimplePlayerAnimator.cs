dba73e4aa1bc388a62859e2f233843e9d0793e53
Gregory Labute <gregoryl@unity3d.com>
2024-07-05 13:13:59 -0400

    update samples

--- com.unity.cinemachine/Samples~/Shared Assets/Scripts/SimplePlayerAnimator.cs
+++ com.unity.cinemachine/Samples~/Shared Assets/Scripts/SimplePlayerAnimator.cs
@@ -19,7 +19,6 @@ namespace Unity.Cinemachine.Samples
     /// and expects to be driven by the SimplePlayerControllerBase using the STartJump, EndJump, 
     /// and PostUpdate callbacks.
     /// </summary>
-    [RequireComponent(typeof(Animator))]
     public class SimplePlayerAnimator : MonoBehaviour
     {
         [Tooltip("Tune this to the animation in the model: feet should not slide when walking at this speed")]
@@ -34,23 +33,39 @@ namespace Unity.Cinemachine.Samples
         [Tooltip("Scale factor for the overall speed of the jump animation")]
         public float JumpAnimationScale = 0.65f;
 
-        Animator m_Animator;
         SimplePlayerControllerBase m_Controller;
         Vector3 m_PreviousPosition; // used if m_Controller == null or disabled
-        bool m_WasWalking;
-        bool m_WasRunning;
+        bool m_IsWalking;
+        bool m_IsRunning;
+        bool m_IsJumping;
+        bool m_LandTriggered;
+        bool m_JumpTriggered;
         const float k_IdleThreshold = 0.2f;
+
+        public enum States { Idle, Walk, Run, Jump, RunJump }
+
+        /// <summary>Current state of the player</summary>
+        public States State 
+        { 
+            get 
+            {
+                if (m_IsJumping)
+                    return m_IsRunning ? States.RunJump : States.Jump;
+                if (m_IsRunning)
+                    return States.Run;
+                return m_IsWalking ? States.Walk : States.Idle;
+            }
+        }
 
         void Start()
         {
             m_PreviousPosition = transform.position;
-            TryGetComponent(out m_Animator);
             m_Controller = GetComponentInParent<SimplePlayerControllerBase>();
             if (m_Controller != null)
             {
                 // Install our callbacks to handle jump and animation based on velocity
-                m_Controller.StartJump += () => OnJump(true);
-                m_Controller.EndJump += () => OnJump(false);
+                m_Controller.StartJump += () => m_JumpTriggered = true;
+                m_Controller.EndJump += () => m_LandTriggered = true;
                 m_Controller.PostUpdate += (vel, jumpAnimationScale) => UpdateAnimation(vel, jumpAnimationScale);
             }
         }
@@ -74,13 +89,6 @@ namespace Unity.Cinemachine.Samples
         }
 
         /// <summary>
-        /// Called by the SimplePlayerControllerBase when the player starts or ends a jump.
-        /// Override this to interact appropriately with your animation controller.
-        /// </summary>
-        /// <param name="jumping">True when jump starts, false when it ends.</param>
-        virtual protected void OnJump(bool jumping) => m_Animator.SetTrigger(jumping ? "Jump" : "Land");
-        
-        /// <summary>
         /// Update the animation based on the player's velocity.
         /// Override this to interact appropriately with your animation controller.
         /// </summary>
@@ -89,14 +97,20 @@ namespace Unity.Cinemachine.Samples
         /// It can be used to slow down the jump animation for longer jumps.</param>
         virtual protected void UpdateAnimation(Vector3 vel, float jumpAnimationScale)
         {
+            if (!TryGetComponent(out Animator animator))
+            {
+                Debug.LogError("SimplePlayerAnimator: An Animator component is required");
+                return;
+            }
+
             vel.y = 0; // we don't consider vertical movement
             var speed = vel.magnitude;
 
             // Hysteresis reduction
-            bool isRunning = speed > NormalWalkSpeed * 2 + (m_WasRunning ? -0.15f : 0.15f);
-            bool isWalking = !isRunning && speed > k_IdleThreshold + (m_WasWalking ? -0.05f : 0.05f);
-            m_WasWalking = isWalking;
-            m_WasRunning = isRunning;
+            bool isRunning = speed > NormalWalkSpeed * 2 + (m_IsRunning ? -0.15f : 0.15f);
+            bool isWalking = !isRunning && speed > k_IdleThreshold + (m_IsWalking ? -0.05f : 0.05f);
+            m_IsWalking = isWalking;
+            m_IsRunning = isRunning;
 
             // Set the normalized direction of motion and scale the animation speed to match motion speed
             var dir = speed > k_IdleThreshold ? vel / speed : Vector3.zero;
@@ -109,12 +123,25 @@ namespace Unity.Cinemachine.Samples
                     ? speed / NormalSprintSpeed
                     : Mathf.Min(MaxSprintScale, 1 + (speed - NormalSprintSpeed) / (3 * NormalSprintSpeed));
             
-            m_Animator.SetFloat("DirX", dir.x);
-            m_Animator.SetFloat("DirZ", dir.z);
-            m_Animator.SetFloat("MotionScale", motionScale);
-            m_Animator.SetBool("Walking", isWalking);
-            m_Animator.SetBool("Running", isRunning);
-            m_Animator.SetFloat("JumpScale", JumpAnimationScale * jumpAnimationScale);
+            animator.SetFloat("DirX", dir.x);
+            animator.SetFloat("DirZ", dir.z);
+            animator.SetFloat("MotionScale", motionScale);
+            animator.SetBool("Walking", isWalking);
+            animator.SetBool("Running", isRunning);
+            animator.SetFloat("JumpScale", JumpAnimationScale * jumpAnimationScale);
+
+            if (m_JumpTriggered)
+            {
+                animator.SetTrigger("Jump");
+                m_JumpTriggered = false;
+                m_IsJumping = true;
+            }
+            if (m_LandTriggered)
+            {
+                animator.SetTrigger("Land");
+                m_LandTriggered = false;
+                m_IsJumping = false;
+            }
         }
     }
 }
