d98d1a85bde3fdc3cd128ca03ea4d473b9e39a3a
Simon Schossleitner <simons@unity3d.com>
2024-07-08 14:48:40 +0200

    Rebase onto master and upgrade to Wrench 0.10.2

--- Tools/CineMachine-Recipes/Settings/CinemachineSettings.cs
+++ Tools/CineMachine-Recipes/Settings/CinemachineSettings.cs
@@ -1,30 +1,40 @@
-﻿using RecipeEngine.Modules.Wrench.Settings;
+﻿using RecipeEngine.Modules.Wrench.Models;
+using RecipeEngine.Modules.Wrench.Settings;
 
 namespace Cinemachine.Cookbook.Settings;
 
 public class CinemachineSettings
 {
     // Path from the root of the repository where packages are located.
-    private static string PackagesRootPath = ".";
+    string[] PackagesRootPath = { "." };
 
     // update this to list all packages in this repo that you want to release.
-    private static List<string> PackagesToRelease = new()
+    Dictionary<string, PackageOptions> PackageOptions = new()
     {
-        "com.unity.cinemachine",
+        {
+            "com.unity.cinemachine",
+            new PackageOptions()
+            {
+                ReleaseOptions = new ReleaseOptions() { IsReleasing = true }, // Will generate jobs for this packages.
+            }
+        },
     };
-    
+
     public readonly string ProjectsDir = "Projects";
-    public readonly string[] ProjectNames = new[] { "HDRP", "HDRPInputSystem", "Standalone", "StandaloneInputSystem", "URP", "URPInputSystem" };
-    
+
+    public readonly string[] ProjectNames = new[]
+        { "HDRP", "HDRPInputSystem", "Standalone", "StandaloneInputSystem", "URP", "URPInputSystem" };
+
     ISet<string> PvPprofilesToCheck = new HashSet<string>() { "PVP-20-1" };
+
     public CinemachineSettings()
     {
         Wrench = new WrenchSettings(
             PackagesRootPath,
-            PackagesToRelease,
-            useLocalPvpExemptions:true
+            PackageOptions,
+            useLocalPvpExemptions: true
         );
-        
+
         Wrench.PvpProfilesToCheck = PvPprofilesToCheck;
     }
 
