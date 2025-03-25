cc5b82b3ceae3f4572251873a4fcf328ddeab9de
Gregory Labute <gregoryl@unity3d.com>
2025-03-25 09:35:23 -0400

    fix test again

--- com.unity.cinemachine/Tests/Runtime/ManualUpdateTests.cs
+++ com.unity.cinemachine/Tests/Runtime/ManualUpdateTests.cs
@@ -31,7 +31,8 @@ namespace Unity.Cinemachine.Tests
         public IEnumerator ManualUpdateRespectsFrameCount()
         {
             // Check that source vcam is active
-            yield return UpdateCinemachine();
+            int currentFrame = 0;
+            yield return UpdateCinemachine(currentFrame++);
             Assert.That(ReferenceEquals(m_Brain.ActiveVirtualCamera, m_vcam));
 
             // Update should do something if we call it exactly once per frame
@@ -39,7 +40,7 @@ namespace Unity.Cinemachine.Tests
             for (int i = 0; i < 5; ++i)
             {
                 Assert.AreEqual(i, m_TestCounter.UpdateCount, "Single call to ManualUpdate per frame, update count does not match");
-                yield return UpdateCinemachine(i);
+                yield return UpdateCinemachine(currentFrame++);
             }
 
             // Multiple calls to ManualUpdate within the same frame should have no effect (other than to waste time)
@@ -47,8 +48,8 @@ namespace Unity.Cinemachine.Tests
             for (int i = 0; i < 5; ++i)
             {
                 Assert.AreEqual(i, m_TestCounter.UpdateCount, "Multiple calls to ManualUpdate per frame, update count does not match");
-                m_Brain.ManualUpdate(i, CinemachineCore.UniformDeltaTimeOverride); // spurious call to ManualUpdate
-                yield return UpdateCinemachine(i);
+                m_Brain.ManualUpdate(currentFrame, CinemachineCore.UniformDeltaTimeOverride); // spurious call to ManualUpdate
+                yield return UpdateCinemachine(currentFrame++);
             }
         }
     }
