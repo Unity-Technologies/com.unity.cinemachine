ede08fc7c68864868f5f64db8a3ce7660a7358d7
Gregory Labute <gregoryl@unity3d.com>
2025-06-05 16:28:21 -0400

    Update CinemachineImpulseListenerEditor.cs

--- com.unity.cinemachine/Editor/Impulse/CinemachineImpulseListenerEditor.cs
+++ com.unity.cinemachine/Editor/Impulse/CinemachineImpulseListenerEditor.cs
@@ -12,9 +12,20 @@ namespace Unity.Cinemachine.Editor
             var ux = new VisualElement();
 
             this.AddMissingCmCameraHelpBox(ux);
-            ux.Add(new HelpBox(
-                "The Impulse Listener will respond to signals broadcast by any CinemachineImpulseSource.", 
-                HelpBoxMessageType.Info));
+
+            if (!(target as CinemachineImpulseListener).TryGetComponent<CinemachineVirtualCameraBase>(out _))
+            {
+                ux.Add(new HelpBox(
+                    "To add impulse listening capabilities to GameObjects that are not CinemachineCameras, "
+                        + "you can use the <b>Cinemachine External Impulse Listener</b> behaviour instead.",
+                    HelpBoxMessageType.Info));
+            }
+            else
+            {
+                ux.Add(new HelpBox(
+                    "The Impulse Listener will respond to signals broadcast by any CinemachineImpulseSource.", 
+                    HelpBoxMessageType.Info));
+            }
             var prop = serializedObject.GetIterator();
             if (prop.NextVisible(true))
                 InspectorUtility.AddRemainingProperties(ux, prop);
