44a5d10bfe7bf03d5bd310e066e6b1da00996884
Gregory Labute <gregoryl@unity3d.com>
2025-09-12 10:27:06 -0400

    Move Recentering button to Value field

--- com.unity.cinemachine/Editor/PropertyDrawers/InputAxisPropertyDrawer.cs
+++ com.unity.cinemachine/Editor/PropertyDrawers/InputAxisPropertyDrawer.cs
@@ -15,30 +15,27 @@ namespace Unity.Cinemachine.Editor
             foldout.BindProperty(property);
 
             var valueProp = property.FindPropertyRelative(nameof(InputAxis.Value));
+            var centerProp = property.FindPropertyRelative(nameof(InputAxis.Center));
+
             var valueLabel = new Label(" ") { style = { minWidth = InspectorUtility.SingleLineHeight * 2 }};
             var valueField =  new PropertyField(valueProp, "") { style = { flexGrow = 1, marginLeft = 2 }};
-            valueField.OnInitialGeometry(() => valueField.SafeSetIsDelayed());
+            var recenterButton1 = CreateRecenterButton(valueProp, centerProp);
+            valueField.OnInitialGeometry(() => 
+            {
+                valueField.SafeSetIsDelayed();
+                valueField.Q<FloatField>().Add(recenterButton1);
+            });
             valueLabel.AddDelayedFriendlyPropertyDragger(valueProp, valueField, (d) => d.CancelDelayedWhenDragging = true);
 
             var ux = new InspectorUtility.FoldoutWithOverlay(foldout, valueField, valueLabel);
 
             // We want dynamic dragging on the value, even if isDelayed is set. PropertyRow puts a delayed-friendly dragger.
-            foldout.AddChild(InspectorUtility.PropertyRow(valueProp, out var valueField2));
+            var valueRow = foldout.AddChild(InspectorUtility.PropertyRow(valueProp, out var valueField2));
             valueField2.OnInitialGeometry(() => valueField2.SafeSetIsDelayed());
+            var recenterButton2 = valueRow.Contents.AddChild(CreateRecenterButton(valueProp, centerProp));
 
-            var centerProp = property.FindPropertyRelative(nameof(InputAxis.Center));
-            var centerRow = foldout.AddChild(InspectorUtility.PropertyRow(centerProp, out var centerField));
+            foldout.AddChild(InspectorUtility.PropertyRow(centerProp, out var centerField));
             centerField.OnInitialGeometry(() => centerField.SafeSetIsDelayed());
-            centerRow.Contents.Add(new Button(() =>
-            {
-                valueProp.floatValue = centerProp.floatValue;
-                valueProp.serializedObject.ApplyModifiedProperties();
-            })
-            { 
-                text = "Recenter", 
-                tooltip = "Reset the axis value to the center",
-                style = { marginLeft = 4, marginRight = 0, marginTop = 0, marginBottom = 0 }
-            });
 
             var rangeContainer = foldout.AddChild(new VisualElement() { style = { flexDirection = FlexDirection.Row }});
             rangeContainer.Add(new PropertyField(property.FindPropertyRelative(nameof(InputAxis.Range))) { style = { flexGrow = 1 }});
@@ -58,12 +55,29 @@ namespace Unity.Cinemachine.Editor
                 var rangeDisabled = (flags & (int)InputAxis.RestrictionFlags.RangeIsDriven) != 0;
                 centerField.SetEnabled(!rangeDisabled);
                 rangeContainer.SetEnabled(!rangeDisabled);
-                recentering.SetVisible((flags & (int)(InputAxis.RestrictionFlags.NoRecentering | InputAxis.RestrictionFlags.Momentary)) == 0);
+                var hideRecentering = (flags & (int)(InputAxis.RestrictionFlags.NoRecentering | InputAxis.RestrictionFlags.Momentary)) == 0;
+                recentering.SetVisible(hideRecentering);
+                recenterButton1.SetVisible(hideRecentering);
+                recenterButton2.SetVisible(hideRecentering);
                 wrap.SetVisible((flags & (int)InputAxis.RestrictionFlags.Momentary) == 0);
                 wrapLabel.SetVisible((flags & (int)InputAxis.RestrictionFlags.Momentary) == 0);
             });
 
             return ux;
+        }
+
+        Button CreateRecenterButton(SerializedProperty valueProp, SerializedProperty centerProp)
+        {
+            return new Button(() =>
+            {
+                valueProp.floatValue = centerProp.floatValue;
+                valueProp.serializedObject.ApplyModifiedProperties();
+            })
+            { 
+                text = "Recenter", 
+                tooltip = "Reset the axis value to the center vlaue",
+                style = { marginLeft = 4, marginRight = 0, marginTop = 0, marginBottom = 0 }
+            };
         }
     }
 }
