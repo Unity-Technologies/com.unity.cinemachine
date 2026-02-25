7d14851817641d5adc9ebb2a9235f5dcf1da7661
sim-bz <sim.bz@unity3d.com>
2026-02-25 10:20:52 -0500

    Fixed compilation.

--- com.unity.cinemachine/Editor/Windows/CinemachineCorePrefs.cs
+++ com.unity.cinemachine/Editor/Windows/CinemachineCorePrefs.cs
@@ -29,7 +29,9 @@ namespace Unity.Cinemachine.Editor
         private static void InitializeModule()
         {
             CinemachineDebug.GameViewGuidesEnabled = ShowInGameGuides.Value;
+#if CINEMACHINE_UGUI
             CinemachineStoryboard.s_StoryboardGlobalMute = StoryboardGlobalMute.Value;
+#endif
         }
 
         static CinemachineSettings.BoolItem s_SettingsFoldedOut = new("CNMCN_Core_Folded", true);
